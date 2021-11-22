7775294b360ccf689d2502bad154395626d0c482
aymeric-mavryk <aymeric@tezosdynamics.com>
2021-11-23 00:22:23 +0100

    Implement IPFS upload

--- src/frontend/src/pages/BecomeSatellite/BecomeSatellite.style.tsx
+++ src/frontend/src/pages/BecomeSatellite/BecomeSatellite.style.tsx
@@ -1,12 +1,34 @@
 import styled from 'styled-components/macro'
-import { Card, containerColor, upColor, downColor } from 'styles'
+import { containerColor, upColor, downColor, Card } from 'styles'
 
 export const BecomeSatelliteStyled = styled.div`
   background-color: ${containerColor};
 `
 
-export const BecomeSatelliteForm = styled(Card)``
+export const BecomeSatelliteForm = styled(Card)`
+  padding-bottom: 80px;
+
+  > p {
+    margin-top: 30px;
+  }
+
+  > button {
+    width: 300px;
+    float: right;
+  }
+`
 
 export const BecomeSatelliteFormBalanceCheck = styled.div<{ balanceOk: boolean }>`
   color: ${(props) => (props.balanceOk ? upColor : downColor)};
 `
+
+export const UploaderFileSelector = styled.div`
+  margin: 15px 0;
+  cursor: pointer;
+
+  > input {
+    width: 86px;
+  }
+`
+
+export const BecomeSatelliteProfilePic = styled.div``
