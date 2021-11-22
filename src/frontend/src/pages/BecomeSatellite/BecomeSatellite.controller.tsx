640fe5fc0333e2b1bd070529c689da414fe5a7a7
aymeric-mavryk <aymeric@tezosdynamics.com>
2021-11-23 00:22:23 +0100

    Implement IPFS upload

--- src/frontend/src/pages/BecomeSatellite/BecomeSatellite.controller.tsx
+++ src/frontend/src/pages/BecomeSatellite/BecomeSatellite.controller.tsx
@@ -1,36 +1,33 @@
-import { SatellitesHeader } from 'pages/Satellites/SatellitesHeader/SatellitesHeader.controller'
-import * as React from 'react'
+import { getVMvkTokenStorage } from 'pages/Doorman/Doorman.actions'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { State } from 'reducers'
-import { Message, Page } from 'styles'
 
-import { getMvkTokenStorage, getVMvkTokenStorage, stake, unstake } from './BecomeSatellite.actions'
+import { registerAsSatellite, RegisterAsSatelliteForm } from './BecomeSatellite.actions'
 import { BecomeSatelliteView } from './BecomeSatellite.view'
 
 export const BecomeSatellite = () => {
   const dispatch = useDispatch()
-  const loading = useSelector((state: State) => state.loading)
-  const { wallet, ready, tezos, accountPkh } = useSelector((state: State) => state.wallet)
-  const { mvkTokenStorage, myMvkTokenBalance } = useSelector((state: State) => state.mvkToken)
-  const { vMvkTokenStorage, myVMvkTokenBalance } = useSelector((state: State) => state.vMvkToken)
+  const { accountPkh } = useSelector((state: State) => state.wallet)
+  const { myVMvkTokenBalance } = useSelector((state: State) => state.vMvkToken)
 
   useEffect(() => {
     if (accountPkh) {
-      dispatch(getMvkTokenStorage(accountPkh))
       dispatch(getVMvkTokenStorage(accountPkh))
     }
   }, [dispatch, accountPkh])
 
   // useOnBlock(tezos, loadStorage)
 
-  // const stakeCallback = (amount: number) => {
-  //   dispatch(stake(amount))
-  // }
+  const registerCallback = (form: RegisterAsSatelliteForm) => {
+    dispatch(registerAsSatellite(form))
+  }
 
-  // const unstakeCallback = (amount: number) => {
-  //   dispatch(showExitFeeModal(amount))
-  // }
-
-  return <BecomeSatelliteView />
+  return (
+    <BecomeSatelliteView
+      registerCallback={registerCallback}
+      accountPkh={accountPkh}
+      myVMvkTokenBalance={myVMvkTokenBalance}
+    />
+  )
 }
