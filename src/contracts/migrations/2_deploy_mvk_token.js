d70d62250a0afd7b2e067495a852f5fc9273dc2c
Sam Morrow <smorrow32@gmail.com>
2021-12-08 15:09:29 +0200

    1. Connect to registerAsSatellite 2. Upped initial MVK supply in 2_deploy_mvk_token.j

--- src/contracts/migrations/2_deploy_mvk_token.js
+++ src/contracts/migrations/2_deploy_mvk_token.js
@@ -6,7 +6,8 @@ const { alice, bob, eve, mallory } = require('../scripts/sandbox/accounts')
 const saveContractAddress = require('../helpers/saveContractAddress')
 const doormanAddress = require('../deployments/doormanAddress')
 
-const initialSupply = '1000000000' // 1,000 MVK Tokens in mu (10^6)
+
+const initialSupply = '100000000000' // 1000 MVK Tokens in mu (10^6)
 
 const metadata = MichelsonMap.fromLiteral({
   '': Buffer('tezos-storage:data', 'ascii').toString('hex'),
