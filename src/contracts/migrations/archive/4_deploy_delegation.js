522a47cdfb2223f6a630c4dd1c452297ce18a2d1
Sam Morrow <smorrow32@gmail.com>
2022-02-09 14:09:54 +0200

    Copying missing/wrongly placed files from origin/governance into staging

---
+++ src/contracts/migrations/archive/4_deploy_delegation.js
@@ -0,0 +1,54 @@
+const delegationContract = artifacts.require('delegation')
+const vMvkTokenContract = artifacts.require('vMvkToken')
+const doormanContract = artifacts.require('doorman')
+
+const { MichelsonMap } = require('@taquito/michelson-encoder')
+
+const { alice } = require('../scripts/sandbox/accounts')
+const saveContractAddress = require('../helpers/saveContractAddress')
+const vMvkTokenAddress = require('../deployments/vMvkTokenAddress')
+const doormanAddress = require('../deployments/doormanAddress')
+
+const delegateLedger   = MichelsonMap.fromLiteral({});
+const satelliteLedger  = MichelsonMap.fromLiteral({});
+const adminAddress     = alice.pkh;
+const configType        = {
+    minimumStakedMvkBalance: 100_000_000_000,  // 100,000 vMVK in mu (10^6)
+    delegationRatio: 10_000,                   // 10%
+    maxSatellites: 100                         // max number of satellites
+};
+const breakGlassConfigType = {
+    delegateToSatelliteIsPaused           : false,
+    undelegateFromSatelliteIsPaused         : false,
+    registerAsSatelliteIsPaused    : false,
+    unregisterAsSatelliteIsPaused  : false,
+    updateSatelliteRecordIsPaused  : false
+}
+
+const initialStorage = {
+  admin : adminAddress,
+  config : configType,
+  breakGlassConfig: breakGlassConfigType,
+  delegateLedger : delegateLedger,
+  satelliteLedger : satelliteLedger,
+  // vMvkTokenAddress : vMvkTokenAddress,
+  doormanAddress: doormanAddress,
+  governanceAddress : vMvkTokenAddress // update on governance deployment
+}
+
+module.exports = async (deployer, network, accounts) => {
+  await deployer.deploy(delegationContract, initialStorage)
+  const deployedDelegationContract = await delegationContract.deployed()
+
+  //   Set delegation address in vMVK
+  // const deployedVMvkToken = await vMvkTokenContract.deployed()
+  // await deployedVMvkToken.setDelegationTokenAddress(deployedDelegationContract.address)
+
+  // Set delegation address in Doorman
+  const deployedDoorman = await doormanContract.deployed()
+  await deployedDoorman.setDelegationAddress(deployedDelegationContract.address)
+
+  await saveContractAddress('delegationAddress', deployedDelegationContract.address)
+}
+
+module.exports.initial_storage = initialStorage
