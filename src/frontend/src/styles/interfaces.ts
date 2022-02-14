608539ca1c50d868fa1cc8c8cd994c1ae1e107f6
Sam Morrow <smorrow32@gmail.com>
2022-02-14 18:45:42 +0200

    Basic Dark mode implementation

---
+++ src/frontend/src/styles/interfaces.ts
@@ -0,0 +1,16 @@
+export interface MavrykTheme {
+  backgroundColor: string
+  containerColor: string
+  borderColor: string
+  textColor: string
+  subTextColor: string
+  backgroundTextColor: string
+  placeholderColor: string
+  primaryColor: string
+  secondaryColor: string
+  upColor: string
+  downColor: string
+  warningColor: string
+  infoColor: string
+  selectedColor: string
+}
