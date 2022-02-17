930da021056dbf68777b6696fbafee34daf1e2a7
Sam Morrow <smorrow32@gmail.com>
2022-02-17 14:29:53 +0200

    1. Global Button radius value 2. Continued theme adjustments for Dark mode 3. New styling for Navigation links in Menu 4. Adjusted ConnectWallet button component

--- src/frontend/src/app/App.components/Menu/Menu.style.tsx
+++ src/frontend/src/app/App.components/Menu/Menu.style.tsx
@@ -5,7 +5,6 @@ import { MavrykTheme } from '../../../styles/interfaces'
 export const MenuStyled = styled.div`
   position: relative;
   text-align: center;
-  height: 100vh;
   width: 270px;
   align-items: center;
   display: flex;
@@ -22,35 +21,6 @@ export const MenuLogo = styled.img`
   width: 175px;
 `
 
-export const MenuButton = styled.div<{ theme: MavrykTheme }>`
-  margin: 0 auto;
-  width: 160px;
-  height: 50px;
-  cursor: pointer;
-  background: ${({ theme }) => theme.backgroundTextColor};
-  border-radius: 10px;
-  color: ${({ theme }) => theme.subTextColor};
-  text-align: center;
-  font-weight: bold;
-  line-height: 50px;
-  font-size: 12px;
-
-  > svg {
-    display: inline-block;
-    width: 24px;
-    height: 24px;
-    margin: 14px 9px 13px 8px;
-    stroke: ${({ theme }) => theme.subTextColor};
-    vertical-align: top;
-  }
-
-  > div {
-    display: inline-block;
-    margin-right: 9px;
-    font-weight: 600;
-  }
-`
-
 export const MenuGrid = styled.div`
   //display: grid;
   //grid-template-columns: 50px 50px;
@@ -60,82 +30,6 @@ export const MenuGrid = styled.div`
   flex-direction: column;
   justify-content: space-evenly;
   width: 100%;
-`
-
-export const MenuIcon = styled.div<{ selected: boolean; theme: MavrykTheme }>`
-  margin: 0 auto 25px auto;
-  width: 50px;
-  height: 50px;
-  cursor: pointer;
-  background: ${({ theme }) => theme.backgroundTextColor};
-  border-radius: 10px;
-  color: ${({ theme }) => theme.subTextColor};
-  text-align: center;
-  font-weight: bold;
-  line-height: 50px;
-
-  > div {
-    font-size: 11px;
-    line-height: 31px;
-    font-weight: 600;
-    color: ${({ theme }) => theme.subTextColor};
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-  }
-
-  > svg {
-    display: inline-block;
-    width: 24px;
-    height: 24px;
-    margin: 14px 9px 13px 8px;
-    stroke: ${({ theme }) => theme.subTextColor};
-    vertical-align: top;
-  }
-
-  ${(props) =>
-    props.selected &&
-    css`
-      background: ${({ theme }) => theme.primaryColor};
-      color: ${({ theme }) => theme.backgroundTextColor};
-      box-shadow: 2px 4px 4px rgba(112, 104, 170, 0.3);
-
-      > div {
-        color: ${({ theme }) => theme.textColor};
-      }
-
-      > svg {
-        stroke: ${({ theme }) => theme.backgroundTextColor};
-      }
-    `}
-`
-
-export const MenuConnected = styled.div<{ theme: MavrykTheme }>`
-  text-align: center;
-  font-weight: 600;
-  margin: 10px auto 33px auto;
-
-  > p {
-    font-size: 11px;
-    line-height: 11px;
-    margin: 3px;
-    color: ${({ theme }) => theme.textColor};
-  }
-
-  > div {
-    font-size: 18px;
-    line-height: 18px;
-    color: ${({ theme }) => theme.primaryColor};
-  }
-
-  svg {
-    cursor: pointer;
-    height: 12px;
-    margin-left: 10px;
-    width: 20px;
-    vertical-align: bottom;
-    stroke: ${({ theme }) => theme.primaryColor};
-  }
 `
 
 export const MenuBanner = styled.img`
--- src/frontend/src/app/App.components/Menu/Menu.view.tsx
+++ src/frontend/src/app/App.components/Menu/Menu.view.tsx
@@ -1,14 +1,10 @@
 import { Link, useLocation } from 'react-router-dom'
 import Toggle from 'react-toggle'
-import { CommaNumber } from '../CommaNumber/CommaNumber.controller'
 // prettier-ignore
 import {
   MenuBanner,
-  MenuButton,
-  MenuConnected,
   MenuFooter,
   MenuGrid,
-  MenuIcon,
   MenuLogo,
   MenuStyled,
   ThemeToggleIcon,
@@ -19,7 +15,6 @@ import { MainNavigationLink } from '../../../styles/interfaces'
 import { NavigationLink } from './NavigationLink/NavigationLink.controller'
 import { useState } from 'react'
 import { ConnectWallet } from '../ConnectWallet/ConnectWallet.controller'
-import { ConnectWalletView } from '../ConnectWallet/ConnectWallet.view'
 
 type MenuViewProps = {
   loading: boolean
@@ -69,6 +64,7 @@ export const MenuView = ({
         {mainNavigationLinks.map((navigationLink: MainNavigationLink, index: number) => {
           return (
             <NavigationLink
+              key={index}
               handleToggle={handleToggle}
               isExpanded={navigationLink.id === isExpanded}
               location={location}
@@ -76,83 +72,6 @@ export const MenuView = ({
             />
           )
         })}
-        {/*<Link to="/dashboard">*/}
-        {/*  <MenuIcon selected={location.pathname === '/dashboard'}>*/}
-        {/*    <svg>*/}
-        {/*      <use xlinkHref="/icons/sprites.svg#grid" />*/}
-        {/*    </svg>*/}
-        {/*    <div>Dashboard</div>*/}
-        {/*  </MenuIcon>*/}
-        {/*</Link>*/}
-
-        {/*<Link to="/">*/}
-        {/*  <MenuIcon selected={location.pathname === '/'}>*/}
-        {/*    <svg>*/}
-        {/*      <use xlinkHref="/icons/sprites.svg#coins" />*/}
-        {/*    </svg>*/}
-        {/*    <div>Staking</div>*/}
-        {/*  </MenuIcon>*/}
-        {/*</Link>*/}
-
-        {/*<Link to="/governance">*/}
-        {/*  <MenuIcon selected={location.pathname === '/governance'}>*/}
-        {/*    <svg>*/}
-        {/*      <use xlinkHref="/icons/sprites.svg#hammer" />*/}
-        {/*    </svg>*/}
-        {/*    <div>Governance</div>*/}
-        {/*  </MenuIcon>*/}
-        {/*</Link>*/}
-
-        {/*<Link to="/yield-farms">*/}
-        {/*  <MenuIcon selected={location.pathname === '/yield-farms'}>*/}
-        {/*    <svg>*/}
-        {/*      <use xlinkHref="/icons/sprites.svg#plant" />*/}
-        {/*    </svg>*/}
-        {/*    <div>Farms</div>*/}
-        {/*  </MenuIcon>*/}
-        {/*</Link>*/}
-
-        {/*<Link to="/treasury">*/}
-        {/*  <MenuIcon selected={location.pathname === '/treasury'}>*/}
-        {/*    <svg>*/}
-        {/*      <use xlinkHref="/icons/sprites.svg#treasury" />*/}
-        {/*    </svg>*/}
-        {/*    <div>Treasury</div>*/}
-        {/*  </MenuIcon>*/}
-        {/*</Link>*/}
-
-        {/*<Link to="/loans">*/}
-        {/*  <MenuIcon selected={location.pathname === '/loans'}>*/}
-        {/*    <svg>*/}
-        {/*      <use xlinkHref="/icons/sprites.svg#bank" />*/}
-        {/*    </svg>*/}
-        {/*    <div>Loans</div>*/}
-        {/*  </MenuIcon>*/}
-        {/*</Link>*/}
-
-        {/*<Link to="/vaults">*/}
-        {/*  <MenuIcon selected={location.pathname === '/vaults'}>*/}
-        {/*    <svg>*/}
-        {/*      <use xlinkHref="/icons/sprites.svg#shop" />*/}
-        {/*    </svg>*/}
-        {/*    <div>Vaults</div>*/}
-        {/*  </MenuIcon>*/}
-        {/*</Link>*/}
-
-        {/*<Link to="/satellites">*/}
-        {/*  <MenuIcon*/}
-        {/*    selected={*/}
-        {/*      location.pathname === '/satellites' ||*/}
-        {/*      location.pathname.startsWith('/satellite-details') ||*/}
-        {/*      location.pathname === '/become-satellite'*/}
-        {/*    }*/}
-        {/*  >*/}
-        {/*    <svg>*/}
-        {/*      <use xlinkHref="/icons/sprites.svg#satellite" />*/}
-        {/*    </svg>*/}
-        {/*    <div>Satellites</div>*/}
-        {/*  </MenuIcon>*/}
-        {/*</Link>*/}
       </MenuGrid>
 
       <MenuBanner src="/images/buy-mvk.svg" alt="buy" />
