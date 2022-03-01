c7a1bd9347391cae057c3bc4dacb8b9ea2f2738c
Tristan ALLAIRE <tristan.allaire@alarch.fr>
2022-03-01 17:30:19 +0100

    Created handlers for Doorman Contract + Bug spotted for Hasura when testing with docker-compose

---
+++ src/indexer/mavryk/handlers/on_mvk_update_operators.py
@@ -0,0 +1,46 @@
+
+from mavryk.types.mvk.storage import MvkStorage
+from dipdup.models import Transaction
+from mavryk.types.mvk.parameter.update_operators import UpdateOperatorsParameter
+from dipdup.context import HandlerContext
+import mavryk.models as models
+
+async def on_mvk_update_operators(
+    ctx: HandlerContext,
+    update_operators: Transaction[UpdateOperatorsParameter, MvkStorage],
+) -> None:
+    update_operators = update_operators.parameter.__root__
+    for update_operator in update_operators:
+        if hasattr(update_operator,'add_operator'):
+            # Get addresses
+            owner = update_operator.add_operator.owner
+            operator = update_operator.add_operator.operator
+
+            # Get or create users
+            user_owner, _ = await models.User.get_or_create(
+                address=owner
+            )
+            user_operator, _ = await models.User.get_or_create(
+                address=operator
+            )
+            await user_owner.fetch_related()
+            await user_owner.operators.add(user_operator)
+            
+            await user_owner.save()
+        elif hasattr(update_operator,'remove_operator'):
+            # Get addresses
+            owner = update_operator.remove_operator.owner
+            operator = update_operator.remove_operator.operator
+
+            # Get or create users
+            user_owner, _ = await models.User.get_or_create(
+                address=owner
+            )
+            user_operator, _ = await models.User.get_or_create(
+                address=operator
+            )
+            await user_owner.fetch_related()
+            if user_operator in await user_owner.operators:
+                user_owner.operators.remove(user_operator)
+            
+            await user_owner.save()
--- src/indexer/mavryk/models.py
+++ src/indexer/mavryk/models.py
@@ -1,6 +1,13 @@
 from tortoise import Model, fields
+from enum import IntEnum
 
+# Enumerators
+class StakeType(IntEnum):
+    STAKE       = 0
+    UNSTAKE     = 1
+    FARM_CLAIM  = 2
 
+# Unique contract instances
 class MVKToken(Model):
     address                         = fields.CharField(pk=True, max_length=36)
     maximum_supply                  = fields.BigIntField(default=0)
@@ -9,17 +16,34 @@ class MVKToken(Model):
     class Meta:
         table = 'mvk_token'
 
+class Doorman(Model):
+    address                         = fields.CharField(pk=True, max_length=36)
+    smvk_total_supply               = fields.BigIntField(default=0)
+    min_mvk_amount                  = fields.BigIntField(default=0)
+    unclaimed_rewards               = fields.BigIntField(default=0)
+    accumulated_fees_per_share      = fields.FloatField(default=0)
+    stake_paused                    = fields.BooleanField(default=False)
+    unstake_paused                  = fields.BooleanField(default=False)
+    compound_paused                 = fields.BooleanField(default=False)
+
+    class Meta:
+        table = 'doorman'
+
+# Users
 class User(Model):
     address                         = fields.CharField(pk=True, max_length=36)
     mvk_balance                     = fields.BigIntField(default=0)
     smvk_balance                    = fields.BigIntField(default=0)
-    participation_fees_per_share    = fields.BigIntField(default=0)
+    participation_fees_per_share    = fields.FloatField(default=0)
+    operators                       = fields.ManyToManyField('models.User', related_name='operates', null=True)
+    doorman                         = fields.ForeignKeyField('models.Doorman', related_name='stake_accounts', null=True)
 
     class Meta:
         table = 'user'
 
+# Time-based records
 class TransferRecord(Model):
-    id = fields.BigIntField(pk=True)
+    id                              = fields.BigIntField(pk=True)
     timestamp                       = fields.DatetimeField()
     token_address                   = fields.ForeignKeyField('models.MVKToken', related_name='transfer_records')
     from_                           = fields.ForeignKeyField('models.User', related_name='transfer_sender')
@@ -28,3 +52,17 @@ class TransferRecord(Model):
 
     class Meta:
         table = 'transfer_record'
+
+class StakeRecord(Model):
+    id                              = fields.BigIntField(pk=True)
+    timestamp                       = fields.DatetimeField()
+    type                            = fields.IntEnumField(enum_type=StakeType)
+    mvk_loyalty_index               = fields.FloatField(default=0.0)
+    exit_fee                        = fields.BigIntField(default=0)
+    from_                           = fields.ForeignKeyField('models.User', related_name='stake_records')
+    desired_amount                  = fields.BigIntField(default=0)
+    final_amount                    = fields.BigIntField(default=0)
+    doorman                         = fields.ForeignKeyField('models.Doorman', related_name='stake_records')
+
+    class Meta:
+        table = 'stake_record'
