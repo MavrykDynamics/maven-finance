ccdc0eeaa395b77d9a6b042cb1a3dfaf83830d20
Sam Morrow <55789228+Sam-M-Israel@users.noreply.github.com>
2022-03-03 13:21:16 +0200

    1. Tabs initial setup on ProposalSubmission page for the 3 stages 2. Fixing issue in ProtectedRoute.tsx

---
+++ src/frontend/src/pages/ProposalSubmission/StageOneForm/StageOneForm.controller.tsx
@@ -0,0 +1,9 @@
+import { StageOneFormView } from './StageOneForm.view'
+
+type StageOneFormProps = {
+  id: number
+  label: string
+}
+export const StageOneForm = ({}: StageOneFormProps) => {
+  return <StageOneFormView />
+}
---
+++ src/frontend/src/pages/ProposalSubmission/StageThreeForm/StageThreeForm.controller.tsx
@@ -0,0 +1,9 @@
+import { StageThreeFormView } from './StageThreeForm.view'
+
+type StageThreeFormProps = {
+  id: number
+  label: string
+}
+export const StageThreeForm = ({}: StageThreeFormProps) => {
+  return <StageThreeFormView />
+}
