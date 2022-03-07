bac1e3750ab8b50687d514344002fdc8c87e2d19
Sam Morrow <55789228+Sam-M-Israel@users.noreply.github.com>
2022-03-07 11:52:10 +0200

    Commented out routes and pages not needed for demo

--- src/frontend/src/app/App.components/AppRoutes/AppRoutes.controller.tsx
+++ src/frontend/src/app/App.components/AppRoutes/AppRoutes.controller.tsx
@@ -26,15 +26,15 @@ export const AppRoutes = () => {
       <Route exact path="/">
         <Doorman />
       </Route>
-      <Route exact path="/dashboard">
-        <Dashboard />
-      </Route>
-      <Route exact path="/dashboard-personal">
-        <Dashboard />
-      </Route>
-      <Route exact path="/your-vesting">
-        <Dashboard />
-      </Route>
+      {/*<Route exact path="/dashboard">*/}
+      {/*  <Dashboard />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/dashboard-personal">*/}
+      {/*  <Dashboard />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/your-vesting">*/}
+      {/*  <Dashboard />*/}
+      {/*</Route>*/}
       <Route exact path="/stake">
         <Doorman />
       </Route>
@@ -47,44 +47,44 @@ export const AppRoutes = () => {
       <Route exact path="/satellite-details/:satelliteId">
         <SatelliteDetails />
       </Route>
-      <Route exact path="/governance">
-        <Governance />
-      </Route>
-      <Route exact path="/proposal-history">
-        <Governance />
-      </Route>
-      <Route exact path="/break-glass">
-        <BreakGlass />
-      </Route>
-      <Route exact path="/emergency-governance">
-        <BreakGlass />
-      </Route>
-      <Route exact path="/mavryk-council">
-        <Governance />
-      </Route>
-      <ProtectedRoute
-        path="/submit-proposal"
-        component={ProposalSubmission}
-        accountPkh={accountPkh}
-        arrayToFilterThrough={satelliteLedger}
-        authenticationPath={'/'}
-        redirectPath={'/submit-proposal'}
-      />
-      <Route exact path="/treasury">
-        <Treasury />
-      </Route>
-      <Route exact path="/loans">
-        <Loans />
-      </Route>
-      <Route exact path="/yield-farms">
-        <Farms />
-      </Route>
-      <Route exact path="/vaults">
-        <Vaults />
-      </Route>
-      <Route exact path="/admin">
-        <Admin />
-      </Route>
+      {/*<Route exact path="/governance">*/}
+      {/*  <Governance />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/proposal-history">*/}
+      {/*  <Governance />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/break-glass">*/}
+      {/*  <BreakGlass />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/emergency-governance">*/}
+      {/*  <BreakGlass />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/mavryk-council">*/}
+      {/*  <Governance />*/}
+      {/*</Route>*/}
+      {/*<ProtectedRoute*/}
+      {/*  path="/submit-proposal"*/}
+      {/*  component={ProposalSubmission}*/}
+      {/*  accountPkh={accountPkh}*/}
+      {/*  arrayToFilterThrough={satelliteLedger}*/}
+      {/*  authenticationPath={'/'}*/}
+      {/*  redirectPath={'/submit-proposal'}*/}
+      {/*/>*/}
+      {/*<Route exact path="/treasury">*/}
+      {/*  <Treasury />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/loans">*/}
+      {/*  <Loans />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/yield-farms">*/}
+      {/*  <Farms />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/vaults">*/}
+      {/*  <Vaults />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/admin">*/}
+      {/*  <Admin />*/}
+      {/*</Route>*/}
       <Route exact path="/404">
         {/*TODO: Replace later on with actual 404 page*/}
         <Doorman />
--- src/frontend/src/app/App.components/Menu/NavigationLink/MainNavigationLinks.ts
+++ src/frontend/src/app/App.components/Menu/NavigationLink/MainNavigationLinks.ts
@@ -1,25 +1,25 @@
 import { MainNavigationRoute } from '../../../../styles/interfaces'
 
 export const mainNavigationLinks: MainNavigationRoute[] = [
-  {
-    title: 'Dashboard',
-    id: 1,
-    path: '/dashboard',
-    icon: 'grid',
-    protectedRoute: false,
-    subPages: [
-      { subTitle: 'Overview', subPath: '/dashboard', protectedRoute: false },
-      { subTitle: 'Personal', subPath: '/dashboard-personal', protectedRoute: false },
-      {
-        subTitle: 'Vestee Info',
-        subPath: '/your-vesting',
-        protectedRoute: true,
-        requires: {
-          isVestee: true,
-        },
-      },
-    ],
-  },
+  // {
+  //   title: 'Dashboard',
+  //   id: 1,
+  //   path: '/dashboard',
+  //   icon: 'grid',
+  //   protectedRoute: false,
+  //   subPages: [
+  //     { subTitle: 'Overview', subPath: '/dashboard', protectedRoute: false },
+  //     { subTitle: 'Personal', subPath: '/dashboard-personal', protectedRoute: false },
+  //     {
+  //       subTitle: 'Vestee Info',
+  //       subPath: '/your-vesting',
+  //       protectedRoute: true,
+  //       requires: {
+  //         isVestee: true,
+  //       },
+  //     },
+  //   ],
+  // },
   {
     title: 'Staking',
     id: 2,
@@ -38,69 +38,69 @@ export const mainNavigationLinks: MainNavigationRoute[] = [
       { subTitle: 'Become A Satellite', subPath: '/become-satellite', protectedRoute: false },
     ],
   },
-  {
-    title: 'Governance',
-    id: 4,
-    path: '/governance',
-    icon: 'hammer',
-    protectedRoute: false,
-    subPages: [
-      { subTitle: 'Proposals', subPath: '/governance', protectedRoute: false },
-      { subTitle: 'Proposal History', subPath: '/proposal-history', protectedRoute: false },
-      { subTitle: 'Break Glass', subPath: '/break-glass', protectedRoute: false },
-      {
-        subTitle: 'Mavryk Council',
-        subPath: '/mavryk-council',
-        protectedRoute: false,
-      },
-      {
-        subTitle: 'Submit Proposal',
-        subPath: '/submit-proposal',
-        protectedRoute: true,
-        requires: {
-          isSatellite: true,
-        },
-      },
-      {
-        subTitle: 'Emergency Governance',
-        subPath: '/emergency-governance',
-        protectedRoute: true,
-      },
-    ],
-  },
-  {
-    title: 'Farms',
-    id: 5,
-    path: '/yield-farms',
-    icon: 'plant',
-    protectedRoute: false,
-  },
-  {
-    title: 'Treasury',
-    id: 6,
-    path: '/treasury',
-    icon: 'treasury',
-    protectedRoute: false,
-  },
-  {
-    title: 'Loans',
-    id: 7,
-    path: '/loans',
-    icon: 'bank',
-    protectedRoute: false,
-  },
-  {
-    title: 'Vaults',
-    id: 8,
-    path: '/vaults',
-    icon: 'shop',
-    protectedRoute: false,
-  },
-  {
-    title: 'Admin',
-    id: 9,
-    path: '/admin',
-    icon: 'shop',
-    protectedRoute: false,
-  },
+  // {
+  //   title: 'Governance',
+  //   id: 4,
+  //   path: '/governance',
+  //   icon: 'hammer',
+  //   protectedRoute: false,
+  //   subPages: [
+  //     { subTitle: 'Proposals', subPath: '/governance', protectedRoute: false },
+  //     { subTitle: 'Proposal History', subPath: '/proposal-history', protectedRoute: false },
+  //     { subTitle: 'Break Glass', subPath: '/break-glass', protectedRoute: false },
+  //     {
+  //       subTitle: 'Mavryk Council',
+  //       subPath: '/mavryk-council',
+  //       protectedRoute: false,
+  //     },
+  //     {
+  //       subTitle: 'Submit Proposal',
+  //       subPath: '/submit-proposal',
+  //       protectedRoute: true,
+  //       requires: {
+  //         isSatellite: true,
+  //       },
+  //     },
+  //     {
+  //       subTitle: 'Emergency Governance',
+  //       subPath: '/emergency-governance',
+  //       protectedRoute: true,
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: 'Farms',
+  //   id: 5,
+  //   path: '/yield-farms',
+  //   icon: 'plant',
+  //   protectedRoute: false,
+  // },
+  // {
+  //   title: 'Treasury',
+  //   id: 6,
+  //   path: '/treasury',
+  //   icon: 'treasury',
+  //   protectedRoute: false,
+  // },
+  // {
+  //   title: 'Loans',
+  //   id: 7,
+  //   path: '/loans',
+  //   icon: 'bank',
+  //   protectedRoute: false,
+  // },
+  // {
+  //   title: 'Vaults',
+  //   id: 8,
+  //   path: '/vaults',
+  //   icon: 'shop',
+  //   protectedRoute: false,
+  // },
+  // {
+  //   title: 'Admin',
+  //   id: 9,
+  //   path: '/admin',
+  //   icon: 'shop',
+  //   protectedRoute: false,
+  // },
 ]
