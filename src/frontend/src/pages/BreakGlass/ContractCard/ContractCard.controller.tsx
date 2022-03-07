b3954dbd851b3a3bbc644faa04ccbe16088bcc9b
Sam Morrow <55789228+Sam-M-Israel@users.noreply.github.com>
2022-03-07 16:58:37 +0200

    1. Menu bug fix 2. Initial Break glass pages with mock data

---
+++ src/frontend/src/pages/BreakGlass/ContractCard/ContractCard.controller.tsx
@@ -0,0 +1,65 @@
+import { useState, useEffect, useRef } from 'react'
+import {
+  CardTopSection,
+  ContractCardStyled,
+  DropDownContainer,
+  EntrypointNameWithStatus,
+  TitleStatusContainer,
+} from './ContractCard.style'
+import { BGTextTitle } from '../BreakGlass.style'
+import { StatusFlag } from '../../../app/App.components/StatusFlag/StatusFlag.controller'
+import * as React from 'react'
+import { ContractBreakGlass } from '../mockContracts'
+import { ProposalStatus } from '../../../utils/TypesAndInterfaces/Governance'
+import { TzAddress } from '../../../app/App.components/TzAddress/TzAddress.view'
+
+type ContractCardProps = {
+  contract: ContractBreakGlass
+}
+export const ContractCard = ({ contract }: ContractCardProps) => {
+  const { entrypoints } = contract
+  const [expanded, setExpanded] = useState(false)
+  const [accordionHeight, setAccordionHeight] = useState(0)
+  const ref = useRef(null)
+  const conStatus = contract.status === 'LIVE' ? ProposalStatus.EXECUTED : ProposalStatus.DEFEATED
+
+  const open = () => setExpanded(!expanded)
+
+  useEffect(() => {
+    // @ts-ignore
+    const getHeight = ref.current.scrollHeight
+    setAccordionHeight(getHeight)
+  }, [expanded])
+
+  return (
+    <ContractCardStyled key={contract.address}>
+      <CardTopSection>
+        <TitleStatusContainer>
+          <BGTextTitle>{contract.name}</BGTextTitle>
+          <StatusFlag text={contract.status} status={conStatus} />
+        </TitleStatusContainer>
+        <TzAddress tzAddress={contract.address} hasIcon={false} />
+      </CardTopSection>
+      <DropDownContainer onClick={open} className={expanded ? 'show' : 'hide'} height={accordionHeight} ref={ref}>
+        <span>
+          Entrypoints{' '}
+          {expanded ? (
+            <svg>
+              <use xlinkHref={`/icons/sprites.svg#arrow-up`} />
+            </svg>
+          ) : (
+            <svg>
+              <use xlinkHref={`/icons/sprites.svg#arrow-down`} />
+            </svg>
+          )}
+        </span>
+        <div className={'accordion ' + `${expanded}`} ref={ref}>
+          {entrypoints.map((item, index) => {
+            const entryStatus = item.status === 'LIVE'
+            return <EntrypointNameWithStatus status={entryStatus}>%{item.name}</EntrypointNameWithStatus>
+          })}
+        </div>
+      </DropDownContainer>
+    </ContractCardStyled>
+  )
+}
---
+++ src/frontend/src/pages/BreakGlass/ContractCard/ContractCard.style.tsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components/macro'
+import { MavrykTheme } from '../../../styles/interfaces'
+
+export const ContractCardStyled = styled.div<{ theme: MavrykTheme }>`
+  background-color: ${({ theme }) => theme.backgroundColor};
+  width: 300px;
+  border-radius: 10px;
+  padding-top: 20px;
+  margin: 15px;
+  height: 125px;
+`
+export const CardTopSection = styled.div<{ theme: MavrykTheme }>`
+  padding: 0 20px;
+  margin-bottom: 20px;
+  height: 50px;
+`
+
+export const TitleStatusContainer = styled.div<{ theme: MavrykTheme }>`
+  width: 100%;
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const DropDownContainer = styled.div<{ height: number; theme: MavrykTheme }>`
+  background-color: ${({ theme }) => theme.connectWalletBackgroundColor};
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  height: 35px; /* changed */
+  display: flex;
+  flex-direction: column;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  cursor: pointer;
+  padding: 0 10px;
+  transition: all 0.3s ease-in-out; /* added */
+  overflow: hidden;
+
+  span {
+    padding: 12px 0;
+    > svg {
+      height: 8px;
+      width: 13px;
+      stroke: ${({ theme }) => theme.primaryColor};
+      stroke-width: 5px;
+      fill: none;
+    }
+  }
+
+  .accordion {
+    padding: 10px 15px 15px; /* changed */
+  }
+
+  &.show {
+    height: ${({ height }) => height}px;
+  }
+  &.hide {
+    height: 35px; /* changed */
+  }
+`
+export const EntrypointNameWithStatus = styled.p<{ status: boolean; theme: MavrykTheme }>`
+  font-weight: 500;
+  font-size: 16px;
+  color: ${({ status, theme }) => (status ? theme.upColor : theme.downColor)};
+  margin: 6px 0;
+  padding: 3px 0;
+`
