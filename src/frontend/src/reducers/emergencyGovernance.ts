3127fc53e05da247bd2e2be5248eaffc42c9c001
Sam Morrow <55789228+Sam-M-Israel@users.noreply.github.com>
2022-03-08 19:14:40 +0200

    1. Created reusable TextArea and IPFSUploader (not fully implemented) components 2. Initial addition of Modal for submitting emergency governance proposals

--- src/frontend/src/reducers/emergencyGovernance.ts
+++ src/frontend/src/reducers/emergencyGovernance.ts
@@ -3,6 +3,9 @@ import {
   GET_EMERGENCY_GOVERNANCE_STORAGE,
   SET_EMERGENCY_GOVERNANCE_ACTIVE,
   SET_HAS_ACKNOWLEDGED_EMERGENCY_GOV,
+  SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_ERROR,
+  SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_REQUEST,
+  SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_RESULT,
 } from '../pages/EmergencyGovernance/EmergencyGovernance.actions'
 
 export interface EmergencyGovernanceStorage {
@@ -19,9 +22,12 @@ export interface EmergencyGovernanceStorage {
   nextEmergencyGovernanceProposalId: number
 }
 export interface EmergencyGovernanceState {
+  type?: typeof GET_EMERGENCY_GOVERNANCE_STORAGE | typeof SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_REQUEST
   emergencyGovernanceStorage: EmergencyGovernanceStorage | any
   emergencyGovActive: boolean
   hasAcknowledgeEmergencyGovernance: boolean
+  emergencyGovernanceProposal?: any
+  error?: any
 }
 
 const emergencyGovernanceDefaultState: EmergencyGovernanceState = {
@@ -35,6 +41,7 @@ export function emergencyGovernance(state = emergencyGovernanceDefaultState, act
     case GET_EMERGENCY_GOVERNANCE_STORAGE:
       return {
         ...state,
+        type: GET_EMERGENCY_GOVERNANCE_STORAGE,
         emergencyGovernanceStorage: action.emergencyGovernanceStorage,
       }
     case SET_EMERGENCY_GOVERNANCE_ACTIVE:
@@ -46,6 +53,23 @@ export function emergencyGovernance(state = emergencyGovernanceDefaultState, act
       return {
         ...state,
         hasAcknowledgeEmergencyGovernance: action.hasAcknowledgeEmergencyGovernance,
+      }
+    case SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_REQUEST:
+      return {
+        ...state,
+        type: SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_REQUEST,
+        emergencyGovernanceProposal: action.form,
+      }
+    case SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_RESULT:
+      return {
+        ...state,
+        type: SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_REQUEST,
+      }
+    case SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_ERROR:
+      return {
+        ...state,
+        type: SUBMIT_EMERGENCY_GOVERNANCE_PROPOSAL_REQUEST,
+        error: action.error,
       }
     default:
       return state
