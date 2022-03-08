cfb537da431c3ea1ada75a6edd7b30d26ad61269
Sam Morrow <55789228+Sam-M-Israel@users.noreply.github.com>
2022-03-08 19:14:40 +0200

    1. Created reusable TextArea and IPFSUploader (not fully implemented) components 2. Initial addition of Modal for submitting emergency governance proposals

---
+++ src/frontend/src/app/App.components/TextArea/TextArea.style.tsx
@@ -0,0 +1,143 @@
+import styled, { keyframes } from 'styled-components/macro'
+import { MavrykTheme } from '../../../styles/interfaces'
+
+export const TextAreaStyled = styled.div`
+  position: relative;
+  margin-bottom: 30px;
+`
+
+export const TextAreaComponent = styled.textarea<{ theme: MavrykTheme }>`
+  min-width: 100%;
+  width: 100%;
+  height: 250px;
+  background-color: ${({ theme }) => theme.placeholderColor};
+  font-weight: 600;
+  border: none;
+  margin: 10px 0;
+  color: ${({ theme }) => theme.subTextColor};
+  -webkit-appearance: none;
+  appearance: none;
+  display: block;
+  position: relative;
+  padding: 12px 16px 12px 16px;
+  border-radius: 6px;
+  transition: border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+  will-change: border-color, box-shadow;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.subTextColor};
+  }
+  &:disabled {
+    background: ${({ theme }) => theme.backgroundTextColor};
+    color: ${({ theme }) => theme.subTextColor};
+  }
+
+  &:hover {
+    border-color: ${({ theme }) => theme.primaryColor}7F;
+  }
+
+  &:focus {
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.primaryColor}19;
+    border-color: ${({ theme }) => theme.primaryColor}7F;
+  }
+
+  &.error {
+    border-color: ${({ theme }) => theme.downColor};
+
+    &:focus {
+      box-shadow: 0 0 0 2px ${({ theme }) => theme.downColor}7F;
+    }
+  }
+
+  &.success {
+    border-color: ${({ theme }) => theme.upColor};
+
+    &:focus {
+      box-shadow: 0 0 0 2px ${({ theme }) => theme.upColor}7F;
+    }
+  }
+`
+const zoomIn = keyframes`
+  from {
+    transform:scale(.2);
+    opacity:0
+  }
+  to {
+    transform:scale(1);
+    opacity:1
+  }
+`
+
+export const TextAreaStatus = styled.div`
+  display: block;
+  position: absolute;
+  top: 25px;
+  right: 10px;
+  z-index: 1;
+  margin-top: -10px;
+  line-height: 13px;
+  text-align: center;
+  visibility: visible;
+  pointer-events: none;
+  will-change: transform, opacity;
+
+  &.error {
+    background-image: url('/icons/input-error.svg');
+    animation: ${zoomIn} 0.3s cubic-bezier(0.12, 0.4, 0.29, 1.46);
+    height: 15px;
+    width: 15px;
+  }
+
+  &.success {
+    background-image: url('/icons/input-success.svg');
+    animation: ${zoomIn} 0.3s cubic-bezier(0.12, 0.4, 0.29, 1.46);
+    height: 12px;
+    width: 17px;
+  }
+`
+
+export const TextAreaIcon = styled.svg<{ theme: MavrykTheme }>`
+  display: block;
+  position: absolute;
+  top: 20px;
+  left: 10px;
+  z-index: 1;
+  width: 20px;
+  height: 20px;
+  margin-top: -10px;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  visibility: visible;
+  pointer-events: none;
+  stroke: ${({ theme }) => theme.backgroundTextColor};
+`
+
+const slideDown = keyframes`
+  from {
+    transform: translate3d(0, -10px, 0);
+    opacity:0
+  }
+  to {
+    transform: translate3d(0, 0px, 0);
+    opacity:1
+  }
+`
+
+export const TextAreaErrorMessage = styled.div<{ theme: MavrykTheme }>`
+  color: ${({ theme }) => theme.downColor};
+  line-height: 24px;
+  will-change: transform, opacity;
+  animation: ${slideDown} 0.3s cubic-bezier(0.12, 0.4, 0.29, 1.46);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  &:first-letter {
+    text-transform: uppercase;
+  }
+`
+
+export const TextAreaSpacer = styled.div`
+  height: 10px;
+`
---
+++ src/frontend/src/app/App.components/TextArea/TextArea.view.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import * as PropTypes from 'prop-types'
+
+import { TextAreaStyled, TextAreaComponent, TextAreaStatus, TextAreaIcon, TextAreaErrorMessage } from './TextArea.style'
+import { TextAreaStatusType } from './TextArea.controller'
+
+type TextAreaViewProps = {
+  icon?: string
+  placeholder: string
+  name?: string
+  value?: string | number
+  onChange: any
+  onBlur: any
+  textAreaStatus?: TextAreaStatusType
+  errorMessage?: string
+  disabled?: boolean
+}
+
+export const TextAreaView = ({
+  icon,
+  placeholder,
+  name,
+  value,
+  onChange,
+  onBlur,
+  textAreaStatus,
+  errorMessage,
+  disabled,
+}: TextAreaViewProps) => {
+  let status = textAreaStatus !== undefined ? textAreaStatus : 'none'
+  return (
+    <TextAreaStyled>
+      {icon && (
+        <TextAreaIcon>
+          <use xlinkHref={`/icons/sprites.svg#${icon}`} />
+        </TextAreaIcon>
+      )}
+      <TextAreaComponent
+        name={name}
+        className={status}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        autoComplete={name}
+        disabled={disabled}
+      />
+      <TextAreaStatus className={status} />
+      {errorMessage && <TextAreaErrorMessage>{errorMessage}</TextAreaErrorMessage>}
+    </TextAreaStyled>
+  )
+}
+
+TextAreaView.propTypes = {
+  icon: PropTypes.string,
+  placeholder: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  inputStatus: PropTypes.string,
+  errorMessage: PropTypes.string,
+  disabled: PropTypes.bool,
+}
+
+TextAreaView.defaultProps = {
+  icon: undefined,
+  placeholder: undefined,
+  name: undefined,
+  value: undefined,
+  inputStatus: undefined,
+}
