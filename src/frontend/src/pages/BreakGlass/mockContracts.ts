99419962860bd1d1af9fe5a12a1871503b1c10ce
Sam Morrow <55789228+Sam-M-Israel@users.noreply.github.com>
2022-03-08 14:55:40 +0200

    1. Fixed font size issue in EGov history cards 2. Fixed grid card layout issue for Break Glass Contract cards 3. Removed previous proptypes for connect wallet

--- src/frontend/src/pages/BreakGlass/mockContracts.ts
+++ src/frontend/src/pages/BreakGlass/mockContracts.ts
@@ -106,4 +106,82 @@ export const MOCK_CONTRACTS: ContractBreakGlass[] = [
       },
     ],
   },
+  {
+    name: 'MVK Token',
+    address: 'KT1Cn6kVR7F9yscbNBx4uRnrra19pcuHniuH',
+    status: 'PAUSED',
+    entrypoints: [
+      {
+        name: 'transfer',
+        status: 'LIVE',
+      },
+      {
+        name: 'balance_of',
+        status: 'LIVE',
+      },
+      {
+        name: 'update_operators',
+        status: 'PAUSED',
+      },
+      {
+        name: 'assertMetadata',
+        status: 'LIVE',
+      },
+      {
+        name: 'mint',
+        status: 'PAUSED',
+      },
+      {
+        name: 'burn',
+        status: 'PAUSED',
+      },
+      {
+        name: 'onStakeChange',
+        status: 'PAUSED',
+      },
+      {
+        name: 'updateGeneralContracts',
+        status: 'PAUSED',
+      },
+      {
+        name: 'getTotalSupply',
+        status: 'LIVE',
+      },
+    ],
+  },
+  {
+    name: 'Governance',
+    address: 'KT1Wq5wTqkr47SGsMha3UvDmZQYZimXdBBJQ',
+    status: 'PAUSED',
+    entrypoints: [
+      {
+        name: 'delegateToSatellite',
+        status: 'PAUSED',
+      },
+      {
+        name: 'undelegateFromSatellite',
+        status: 'PAUSED',
+      },
+      {
+        name: 'registerAsSatellite',
+        status: 'PAUSED',
+      },
+      {
+        name: 'unregisterAsSatellite',
+        status: 'PAUSED',
+      },
+      {
+        name: 'updateSatelliteRecord',
+        status: 'PAUSED',
+      },
+      {
+        name: 'setAdmin',
+        status: 'PAUSED',
+      },
+      {
+        name: 'setGovernanceAddress',
+        status: 'PAUSED',
+      },
+    ],
+  },
 ]
