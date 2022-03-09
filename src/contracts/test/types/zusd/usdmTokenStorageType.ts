326363b841c8bba05963a3b9cccaf88d786e593e
Tim <tyong.espoir@gmail.com>
2022-03-09 21:01:03 +0800

    rename zUSD to USDM

--- src/contracts/test/types/zusd/usdmTokenStorageType.ts
+++ src/contracts/test/types/zusd/usdmTokenStorageType.ts
@@ -1,7 +1,7 @@
 import { MichelsonMap, MichelsonMapKey } from "@taquito/michelson-encoder";
 import { BigNumber } from "bignumber.js";
 
-export type lpTokenZUsdXtzStorageType = {
+export type usdmTokenStorageType = {
   admin: string;
 
   metadata: MichelsonMap<MichelsonMapKey, unknown>;
