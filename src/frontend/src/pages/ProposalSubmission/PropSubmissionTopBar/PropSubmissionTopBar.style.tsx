1d584e0987a0b6b246cbc8e7aef3ac381cfcde7c
Sam Morrow <55789228+Sam-M-Israel@users.noreply.github.com>
2022-03-09 13:24:39 +0200

    fixed proposal submission top bar issues

--- src/frontend/src/pages/ProposalSubmission/PropSubmissionTopBar/PropSubmissionTopBar.style.tsx
+++ src/frontend/src/pages/ProposalSubmission/PropSubmissionTopBar/PropSubmissionTopBar.style.tsx
@@ -6,7 +6,6 @@ export const PropSubmissionTopBarStyled = styled.div<{ theme: MavrykTheme }>`
   margin: 15px auto;
   display: flex;
   width: 100%;
-  height: 100%;
   flex-direction: row;
   border-radius: 10px;
   padding: 10px 20px;
