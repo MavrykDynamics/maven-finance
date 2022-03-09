529f907e32f632dbc819d16ab57e86a7e805b2d5
Tim <>
2022-03-09 14:21:29 +0800

    Add zUsdToken and lpTokenZUsdXtz with storage and storage types

---
+++ src/contracts/test/types/zusd/lpTokenZUsdXtzStorageType.ts
@@ -0,0 +1,14 @@
+import { MichelsonMap, MichelsonMapKey } from "@taquito/michelson-encoder";
+import { BigNumber } from "bignumber.js";
+
+export type lpTokenZUsdXtzStorageType = {
+  admin: string;
+
+  metadata: MichelsonMap<MichelsonMapKey, unknown>;
+  token_metadata: MichelsonMap<MichelsonMapKey, unknown>;
+
+  totalSupply: BigNumber;
+
+  ledger: MichelsonMap<MichelsonMapKey, unknown>;
+  operators: MichelsonMap<MichelsonMapKey, unknown>;
+};
---
+++ src/contracts/test/types/zusd/zUsdTokenStorageType.ts
@@ -0,0 +1,14 @@
+import { MichelsonMap, MichelsonMapKey } from "@taquito/michelson-encoder";
+import { BigNumber } from "bignumber.js";
+
+export type zUsdTokenStorageType = {
+  admin: string;
+
+  metadata: MichelsonMap<MichelsonMapKey, unknown>;
+  token_metadata: MichelsonMap<MichelsonMapKey, unknown>;
+
+  totalSupply: BigNumber;
+
+  ledger: MichelsonMap<MichelsonMapKey, unknown>;
+  operators: MichelsonMap<MichelsonMapKey, unknown>;
+};
