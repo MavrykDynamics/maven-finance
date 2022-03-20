0c6bce55a7a787a792ad02a069e80f3acf761527
Sam Morrow <55789228+Sam-M-Israel@users.noreply.github.com>
2022-03-20 11:09:04 +0200

    tempo commit before switching to demo branch

--- src/frontend/src/reducers/wallet.ts
+++ src/frontend/src/reducers/wallet.ts
@@ -1,19 +1,28 @@
-import { TezosToolkit } from "@taquito/taquito"
-import { TempleWallet } from "@temple-wallet/dapp"
-import { CONNECT, SET_WALLET } from "app/App.components/Menu/Menu.actions"
+import { TezosToolkit } from '@taquito/taquito'
+import { TempleWallet } from '@temple-wallet/dapp'
+import {
+  CONNECT,
+  SET_WALLET,
+  UPDATE_OPERATORS_ERROR,
+  UPDATE_OPERATORS_REQUEST,
+  UPDATE_OPERATORS_RESULT,
+} from 'app/App.components/Menu/Menu.actions'
 
 export interface WalletState {
-  wallet?: TempleWallet,
-  tezos?: TezosToolkit,
-  accountPkh?: string,
+  wallet?: TempleWallet
+  tezos?: TezosToolkit
+  accountPkh?: string
   ready: boolean
+  contractPermissionsMap: Map<string, boolean>
+  error?: any
 }
-
+const defaultContractPermissionsMap = new Map<string, boolean>()
 const walletDefaultState: WalletState = {
   wallet: undefined,
   tezos: undefined,
   accountPkh: undefined,
   ready: false,
+  contractPermissionsMap: defaultContractPermissionsMap,
 }
 
 export function wallet(state = walletDefaultState, action: any): WalletState {
@@ -30,6 +39,19 @@ export function wallet(state = walletDefaultState, action: any): WalletState {
         ready: action.ready,
         accountPkh: action.accountPkh,
       }
+    case UPDATE_OPERATORS_REQUEST:
+      const stateContractPermissionMap = new Map<string, boolean>(state.contractPermissionsMap.entries())
+      stateContractPermissionMap.set(action.contract, action.isAuthorized)
+      return {
+        ...state,
+        contractPermissionsMap: stateContractPermissionMap,
+      }
+    case UPDATE_OPERATORS_ERROR:
+      return {
+        ...state,
+        error: action.error,
+      }
+    case UPDATE_OPERATORS_RESULT:
     default:
       return state
   }
