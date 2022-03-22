9f11465b496a24f6b074b5bb19314e01090ea11b
Tim <>
2022-03-22 18:21:11 +0800

    USDM Token Controller minor changes and storage tests types

---
+++ src/contracts/storage/usdmTokenControllerStorage.ts
@@ -0,0 +1,33 @@
+import { MichelsonMap } from "@taquito/michelson-encoder";
+
+import { BigNumber } from "bignumber.js";
+
+const { alice } = require('../scripts/sandbox/accounts')
+
+import { zeroAddress } from "../test/helpers/Utils";
+
+import { usdmTokenControllerStorageType } from "../test/types/usdmTokenControllerStorageType";
+
+const config = {
+    collateralRatio           : 2000,    // collateral ratio (%)
+    liquidationRatio          : 1500,    // liquidation ratio (%)
+    decimals                  : 3,       // decimals 
+}
+
+export const usdmTokenControllerStorage: usdmTokenControllerStorageType = {
+  admin: alice.pkh,
+  config: config,
+
+  whitelistTokenContracts : MichelsonMap.fromLiteral({}),
+  vaults: MichelsonMap.fromLiteral({}),
+
+  targetLedger: MichelsonMap.fromLiteral({}),
+  driftLedger: MichelsonMap.fromLiteral({}),
+  lastDriftUpdateLedger: MichelsonMap.fromLiteral({}),
+  collateralTokenLedger: MichelsonMap.fromLiteral({}),
+  priceLedger: MichelsonMap.fromLiteral({}),
+
+  usdmTokenAddress : zeroAddress,
+  cfmmAddresses: MichelsonMap.fromLiteral({}),
+
+};
