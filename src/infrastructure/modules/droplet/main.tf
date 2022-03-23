713242377ca5b3f70f6e496ea5bdc6bb08c34301
Tristan ALLAIRE <tristan.allaire@alarch.fr>
2022-03-23 15:46:50 +0100

    Oracle droplet module + infra as IAC

---
+++ src/infrastructure/modules/droplet/main.tf
@@ -0,0 +1,8 @@
+resource "digitalocean_droplet" "this" {
+  image     = var.image
+  name      = var.name
+  region    = var.region
+  size      = var.size
+  vpc_uuid  = var.vpc_id
+  tags      = var.tags
+}
