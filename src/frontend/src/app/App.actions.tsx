3a0767ae7fd57ca12b04cbf71e57d40abc803d30
Sam Morrow <smorrow32@gmail.com>
2022-03-23 18:13:38 +0200

    Adding getEmergencyGovernanceStorage from Indexer and implementation

--- src/frontend/src/app/App.actions.tsx
+++ src/frontend/src/app/App.actions.tsx
@@ -3,6 +3,10 @@ import { GET_DOORMAN_STORAGE, GET_MVK_TOKEN_STORAGE } from '../pages/Doorman/Doo
 import storageToTypeConverter from '../utils/storageToTypeConverter'
 import { GET_DELEGATION_STORAGE } from '../pages/Satellites/Satellites.actions'
 import { GET_FARM_FACTORY_STORAGE, GET_FARM_STORAGE } from '../pages/Farms/Farms.actions'
+import {
+  GET_EMERGENCY_GOVERNANCE_STORAGE,
+  SET_EMERGENCY_GOVERNANCE_ACTIVE,
+} from '../pages/EmergencyGovernance/EmergencyGovernance.actions'
 
 export const RECAPTCHA_REQUEST = 'RECAPTCHA_REQUEST'
 export const recaptchaRequest = () => (dispatch: any) => {
@@ -22,6 +26,13 @@ export const onStart = () => async (dispatch: any, getState: any) => {
   const delegationStorage = storageToTypeConverter('delegation', res[3].delegation[0])
   const farmStorage = storageToTypeConverter('farm', res[4].farm)
   const farmFactoryStorage = storageToTypeConverter('farmFactory', res[4].farm_factory[0])
+  const emergencyGovernanceStorage = storageToTypeConverter('emergencyGovernance', res[5].emergency_governance[0])
+
+  const currentEmergencyGovernanceId = emergencyGovernanceStorage.currentEmergencyGovernanceId
+  dispatch({
+    type: SET_EMERGENCY_GOVERNANCE_ACTIVE,
+    emergencyGovActive: currentEmergencyGovernanceId.toNumber() !== 0,
+  })
 
   dispatch({
     type: GET_DOORMAN_STORAGE,
@@ -43,5 +54,9 @@ export const onStart = () => async (dispatch: any, getState: any) => {
   dispatch({
     type: GET_FARM_FACTORY_STORAGE,
     farmFactoryStorage: farmFactoryStorage,
+  })
+  dispatch({
+    type: GET_EMERGENCY_GOVERNANCE_STORAGE,
+    emergencyGovernanceStorage: emergencyGovernanceStorage,
   })
 }
--- src/frontend/src/utils/TypesAndInterfaces/EmergencyGovernance.ts
+++ src/frontend/src/utils/TypesAndInterfaces/EmergencyGovernance.ts
@@ -1,15 +1,36 @@
-import { MichelsonMap } from '@taquito/taquito'
-
+export interface EmergencyGovProposalVoter {
+  emergencyGovernanceRecordId: number
+  id: number
+  sMvkAmount: number
+  timestamp: Date
+  voterId: string
+}
+export interface EmergencyGovernanceProposalRecord {
+  description: string
+  dropped: string
+  emergencyGovernanceId: number
+  executed: boolean
+  executedTimestamp: Date
+  expirationTimestamp: Date
+  id: number
+  proposerId: string
+  sMvkPercentageRequired: number
+  sMvkRequiredForTrigger: number
+  startTimestamp: Date
+  status: boolean
+  title: string
+  voters: EmergencyGovProposalVoter[]
+}
 export interface EmergencyGovernanceStorage {
-  admin: string
+  address: string
+  admin?: string
   config: {
     voteDuration: number
     minStakedMvkPercentageForTrigger: number
     requiredFee: number
+    sMvkPercentageRequired: number
   }
-  generalContracts: MichelsonMap<string, unknown>
-  emergencyGovernanceLedger: MichelsonMap<string, unknown>
-  tempMvkTotalSupply: number
+  emergencyGovernanceLedger: EmergencyGovernanceProposalRecord[]
   currentEmergencyGovernanceId: number
   nextEmergencyGovernanceProposalId: number
 }
