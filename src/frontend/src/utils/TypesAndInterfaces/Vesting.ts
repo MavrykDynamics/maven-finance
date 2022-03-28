6d2e18f03831772d98499f66688cc141d5bacd5b
Sam Morrow <smorrow32@gmail.com>
2022-03-28 14:40:19 +0300

    1. Adding Treasury, vesting, and governance storage gql queries. 2. Implentations of previous 3. Changes in governance pages to accomdate for new governance proposal types

--- src/frontend/src/utils/TypesAndInterfaces/Vesting.ts
+++ src/frontend/src/utils/TypesAndInterfaces/Vesting.ts
@@ -1,20 +1,11 @@
-import { MichelsonMap } from '@taquito/taquito'
-
 type VestingConfig = {
   defaultCliffPeriod: number
   defaultCooldownPeriod: number
-  newBlockTimeLevel: number
-  newBlocksPerMinute: number
-  blocksPerMinute: number
-  blocksPerMonth: number
 }
 export interface VestingStorage {
-  admin: string
+  address: string
   config: VestingConfig
-  whitelistContracts: MichelsonMap<string, unknown>
-  generalContracts: MichelsonMap<string, unknown>
-  claimLedger: MichelsonMap<string, unknown>
-  vesteeLedger: MichelsonMap<string, unknown>
   totalVestedAmount: number
-  tempBlockLevel: number
+  sumAmountClaimed: number
+  sumRemainingVested: number
 }
