f7e66a0e7996c90af81efaae7aebe770d69615d7
Sam Morrow <smorrow32@gmail.com>
2022-03-28 15:24:47 +0300

    1. Updating contract addresses 2. Fixed missed changes for governance and farms gql implementations 3. Adding more contracts to contract addresses query

--- src/frontend/src/pages/Farms/Farms.actions.tsx
+++ src/frontend/src/pages/Farms/Farms.actions.tsx
@@ -2,6 +2,9 @@ import { State } from '../../reducers'
 import farmAddress from '../../deployments/farmAddress.json'
 import farmFactoryAddress from '../../deployments/farmFactoryAddress.json'
 import { TezosToolkit } from '@taquito/taquito'
+import { fetchFromIndexer } from '../../gql/fetchGraphQL'
+import { FARM_STORAGE_QUERY, FARM_STORAGE_QUERY_NAME, FARM_STORAGE_QUERY_VARIABLE } from '../../gql/queries'
+import storageToTypeConverter from '../../utils/storageToTypeConverter'
 
 export const GET_FARM_STORAGE = 'GET_FARM_STORAGE'
 export const getFarmStorage = (accountPkh?: string) => async (dispatch: any, getState: any) => {
@@ -11,19 +14,24 @@ export const getFarmStorage = (accountPkh?: string) => async (dispatch: any, get
   //   dispatch(showToaster(ERROR, 'Public address not found', 'Make sure your wallet is connected'))
   //   return
   // }
-  // TODO: Change address used to that of the Farm address when possible
-  const contract = accountPkh
-    ? await state.wallet.tezos?.wallet.at(farmAddress.address)
-    : await new TezosToolkit(
-        (process.env.REACT_APP_RPC_PROVIDER as any) || 'https://hangzhounet.api.tez.ie/',
-      ).contract.at(farmAddress.address)
-
-  const storage = await (contract as any).storage()
-  console.log('Printing out Farm storage:\n', storage)
-
+  // const contract = accountPkh
+  //   ? await state.wallet.tezos?.wallet.at(farmAddress.address)
+  //   : await new TezosToolkit(
+  //       (process.env.REACT_APP_RPC_PROVIDER as any) || 'https://hangzhounet.api.tez.ie/',
+  //     ).contract.at(farmAddress.address)
+  //
+  // const storage = await (contract as any).storage()
+  // console.log('Printing out Farm storage:\n', storage)
+  const storage = await fetchFromIndexer(FARM_STORAGE_QUERY, FARM_STORAGE_QUERY_NAME, FARM_STORAGE_QUERY_VARIABLE)
+  const convertedFarmStorage = storageToTypeConverter('farm', storage.farm)
+  const convertedFarmFactoryStorage = storageToTypeConverter('farmFactory', storage.farm_factory[0])
   dispatch({
     type: GET_FARM_STORAGE,
-    farmStorage: storage,
+    farmStorage: convertedFarmStorage,
+  })
+  dispatch({
+    type: GET_FARM_FACTORY_STORAGE,
+    farmFactoryStorage: convertedFarmFactoryStorage,
   })
 }
 
