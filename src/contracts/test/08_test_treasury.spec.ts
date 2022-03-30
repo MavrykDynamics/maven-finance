dc8467b78e02cc01e22492a8a58ebab7a742146c
Tim <>
2022-03-30 17:40:24 +0800

    1) Treasury Contract updates - remove Config (minXtzAmount, maxXtzAmount) 2) Treasury Tests

--- src/contracts/test/08_test_treasury.spec.ts
+++ src/contracts/test/08_test_treasury.spec.ts
@@ -97,8 +97,6 @@ describe("Treasury tests", async () => {
                 const updatedTreasuryStorage   = await treasuryInstance.storage();            
                 assert.equal(updatedTreasuryStorage.admin, eve.pkh);
 
-                console.log(updatedTreasuryStorage);
-
                 // reset treasury admin to bob
                 await signerFactory(eve.sk);
                 const resetAdminOperation = await treasuryInstance.methods.setAdmin(bob.pkh).send();
@@ -113,16 +111,16 @@ describe("Treasury tests", async () => {
 
         });    
 
-    });
+    }); // end test: Treasury Housekeeping tests
 
     describe('test: Treasury deposit tests', function() {
 
         it('test: any user (alice) can deposit tez into treasury', async () => {
             try{        
                 
-                // Alice transfers 250 XTZ to Treasury
-                const depositAmount = 250;
-                const depositAmountMutez = 250000000;
+                // Alice transfers 20 XTZ to Treasury
+                const depositAmount = 20;
+                const depositAmountMutez = 20000000;
                 
                 await signerFactory(alice.sk)
                 const aliceTransferTezToTreasuryOperation = await utils.tezos.contract.transfer({ to: treasuryAddress.address, amount: depositAmount});
@@ -136,82 +134,385 @@ describe("Treasury tests", async () => {
             } 
         });
 
-        it('test: any user can deposit mock FA12 Tokens into treasury', async () => {
+        it('test: any user (alice) can deposit mock FA12 Tokens into treasury', async () => {
             try{        
                 
-                console.log('test');
+                // Alice transfers 20 Mock FA12 Tokens to Treasury
+                const depositAmount = 20000000;
+        
+                await signerFactory(alice.sk)
+                const aliceTransferMockFa12ToTreasuryOperation = await mockFa12TokenInstance.methods.transfer(
+                    alice.pkh, 
+                    treasuryAddress.address, 
+                    depositAmount
+                    ).send();
+                await aliceTransferMockFa12ToTreasuryOperation.confirmation();
 
-    //         // Alice transfers 20 Mock FA12 Tokens to Council
-//         const aliceTransferMockFa12ToCouncilOperation = await mockFa12TokenInstance.methods.transfer(alice.pkh, councilContractAddress, 250000000).send();
-//         await aliceTransferMockFa12ToCouncilOperation.confirmation();
+                const updatedMockFa12TokenStorage       = await mockFa12TokenInstance.storage();
+                const treasuryMockFa12TokenBalance      = await updatedMockFa12TokenStorage.ledger.get(treasuryAddress.address);
 
+                assert.equal(treasuryMockFa12TokenBalance.balance, depositAmount);
 
             } catch(e){
                 console.log(e);
             } 
         });
 
-        it('test: any user can deposit mock FA2 Tokens into treasury', async () => {
+        it('test: any user (alice) can deposit mock FA2 Tokens into treasury', async () => {
             try{        
-                console.log('test');
+                
+                // Alice transfers 20 Mock FA2 Tokens to Treasury
+                const depositAmount = 20000000;
+        
+                await signerFactory(alice.sk)
+                const aliceTransferMockFa2ToTreasuryOperation = await mockFa2TokenInstance.methods.transfer([
+                        {
+                            from_: alice.pkh,
+                            txs: [
+                                {
+                                    to_: treasuryAddress.address,
+                                    token_id: 0,
+                                    amount: depositAmount
+                                }
+                            ]
+                        }
+                    ]).send();
+                await aliceTransferMockFa2ToTreasuryOperation.confirmation();
+
+                const updatedMockFa2TokenStorage       = await mockFa2TokenInstance.storage();
+                const treasuryMockFa2TokenBalance      = await updatedMockFa2TokenStorage.ledger.get(treasuryAddress.address);
+
+                assert.equal(treasuryMockFa2TokenBalance, depositAmount);
+
             } catch(e){
                 console.log(e);
             } 
         });
 
-    });
+    }); // end test: Treasury deposit tests
 
-    describe('test: Treasury transfer tests', function() {
 
-        it('test: user cannot transfer tez from treasury', async () => {
+
+    describe('test: Treasury transfer and mintMvkAndTransfer tests', function() {
+
+        it('test: user (alice) cannot transfer tez from treasury', async () => {
             try{        
-                console.log('test');
+                
+                const from_  = treasuryAddress.address;
+                const to_    = alice.pkh;
+                const amount = 10000000;
+                const token  = "tez"
+
+                await signerFactory(alice.sk);
+                const failTransferTezOperation = await treasuryInstance.methods.transfer(
+                     from_,
+                     to_,
+                     amount,
+                     token
+                );
+                await chai.expect(failTransferTezOperation.send()).to.be.eventually.rejected;
+                
             } catch(e){
                 console.log(e);
             } 
         });
 
-        it('test: user cannot transfer mock FA12 Tokens from treasury', async () => {
+        it('test: user (alice) cannot transfer mock FA12 Tokens from treasury', async () => {
             try{        
-                console.log('test');
+                
+                const from_      = treasuryAddress.address;
+                const to_        = alice.pkh;
+                const amount     = 10000000;
+                const tokenType  = "fa12";
+                const token      = mockFa12TokenAddress.address;
+
+                await signerFactory(alice.sk);
+                const failTransferMockFa12TokenOperation = await treasuryInstance.methods.transfer(
+                     from_,
+                     to_,
+                     amount,
+                     tokenType,
+                     token
+                );
+                await chai.expect(failTransferMockFa12TokenOperation.send()).to.be.eventually.rejected;
+
             } catch(e){
                 console.log(e);
             } 
         });
 
-        it('test: user cannot transfer mock FA2 Tokens from treasury', async () => {
+        it('test: user (alice) cannot transfer mock FA2 Tokens from treasury', async () => {
             try{        
-                console.log('test');
+                
+                const from_      = treasuryAddress.address;
+                const to_        = alice.pkh;
+                const amount     = 10000000;
+                const tokenType  = "fa2";
+                const token      = mockFa12TokenAddress.address;
+                const tokenId    = 0;
+
+                await signerFactory(alice.sk);
+                const failTransferMockFa2TokenOperation = await treasuryInstance.methods.transfer(
+                     from_,
+                     to_,
+                     amount,
+                     tokenType,
+                     token,
+                     tokenId
+                );
+                await chai.expect(failTransferMockFa2TokenOperation.send()).to.be.eventually.rejected;
+
             } catch(e){
                 console.log(e);
             } 
         });
 
-        it('test: admin can transfer tez from treasury', async () => {
+        it('test: user (alice) cannot access treasury mintMvkAndTransfer entrypoint', async () => {
             try{        
-                console.log('test');
+                
+                const to_        = alice.pkh;
+                const amount     = 10000000;
+
+                await signerFactory(alice.sk);
+                const failMintMvkAndTransferOperation = await treasuryInstance.methods.mintMvkAndTransfer(
+                     to_,
+                     amount,
+                );
+                await chai.expect(failMintMvkAndTransferOperation.send()).to.be.eventually.rejected;
+
             } catch(e){
                 console.log(e);
             } 
         });
 
-        it('test: admin can transfer mock FA12 Tokens from treasury', async () => {
+        it('test: admin (bob) cannot transfer tez from treasury', async () => {
             try{        
-                console.log('test');
+                
+                const from_  = treasuryAddress.address;
+                const to_    = bob.pkh;
+                const amount = 10000000;
+                const token  = "tez"
+
+                await signerFactory(bob.sk);
+                const failTransferTezOperation = await treasuryInstance.methods.transfer(
+                     from_,
+                     to_,
+                     amount,
+                     token
+                );
+                await chai.expect(failTransferTezOperation.send()).to.be.eventually.rejected;
+                
             } catch(e){
                 console.log(e);
             } 
         });
 
-        it('test: admin can transfer mock FA2 Tokens from treasury', async () => {
+        it('test: admin (bob) cannot transfer mock FA12 Tokens from treasury', async () => {
             try{        
-                console.log('test');
+                
+                const from_      = treasuryAddress.address;
+                const to_        = bob.pkh;
+                const amount     = 10000000;
+                const tokenType  = "fa12";
+                const token      = mockFa12TokenAddress.address;
+
+                await signerFactory(bob.sk);
+                const failTransferMockFa12TokenOperation = await treasuryInstance.methods.transfer(
+                     from_,
+                     to_,
+                     amount,
+                     tokenType,
+                     token
+                );
+                await chai.expect(failTransferMockFa12TokenOperation.send()).to.be.eventually.rejected;
+
             } catch(e){
                 console.log(e);
             } 
         });
 
-    });
+        it('test: admin (bob) cannot transfer mock FA2 Tokens from treasury', async () => {
+            try{        
+                
+                const from_      = treasuryAddress.address;
+                const to_        = bob.pkh;
+                const amount     = 10000000;
+                const tokenType  = "fa2";
+                const token      = mockFa12TokenAddress.address;
+                const tokenId    = 0;
+
+                await signerFactory(bob.sk);
+                const failTransferMockFa2TokenOperation = await treasuryInstance.methods.transfer(
+                     from_,
+                     to_,
+                     amount,
+                     tokenType,
+                     token,
+                     tokenId
+                );
+                await chai.expect(failTransferMockFa2TokenOperation.send()).to.be.eventually.rejected;
+
+            } catch(e){
+                console.log(e);
+            } 
+        });
+
+        it('test: admin (bob) cannot access treasury mintMvkAndTransfer entrypoint', async () => {
+            try{        
+                
+                const to_        = bob.pkh;
+                const amount     = 10000000;
+
+                await signerFactory(bob.sk);
+                const failMintMvkAndTransferOperation = await treasuryInstance.methods.mintMvkAndTransfer(
+                     to_,
+                     amount,
+                );
+                await chai.expect(failMintMvkAndTransferOperation.send()).to.be.eventually.rejected;
+
+            } catch(e){
+                console.log(e);
+            } 
+        });
+
+
+        it('test: admin (bob) can update whitelist contract address map', async () => {
+            try{        
+
+                await signerFactory(bob.sk);
+                const adminUpdateWhitelistContractsOperation = await treasuryInstance.methods.updateWhitelistContracts(
+                     "admin",
+                     bob.pkh
+                ).send();
+                await adminUpdateWhitelistContractsOperation.confirmation();
+
+                const treasuryStorage            = await treasuryInstance.storage();
+                const treasuryWhitelistContracts = await treasuryStorage.whitelistContracts.get("admin");
+                assert.equal(treasuryWhitelistContracts, bob.pkh);
+
+            } catch(e){
+                console.log(e);
+            } 
+        });
+
+        it('test: whitelisted addresses (bob) can transfer tez from treasury', async () => {
+            try{        
+                
+                const from_  = treasuryAddress.address;
+                const to_    = bob.pkh;
+                const amount = 10000000;
+                const token  = "tez"
+
+                await signerFactory(bob.sk);
+                const adminTransferTezOperation = await treasuryInstance.methods.transfer(
+                     from_,
+                     to_,
+                     amount,
+                     token
+                ).send();
+                await adminTransferTezOperation.confirmation();
+
+                const finalTezBalance    = 10000000;
+                const treasuryTezBalance = await utils.tezos.tz.getBalance(treasuryAddress.address);
+                assert.equal(treasuryTezBalance, finalTezBalance);
+
+            } catch(e){
+                console.log(e);
+            } 
+        });
+
+        it('test: whitelisted addresses (bob) can transfer mock FA12 Tokens from treasury', async () => {
+            try{        
+                
+                const from_      = treasuryAddress.address;
+                const to_        = bob.pkh;
+                const amount     = 10000000;
+                const tokenType  = "fa12";
+                const token      = mockFa12TokenAddress.address;
+
+                await signerFactory(bob.sk);
+                const adminTransferMockFa12TokenOperation = await treasuryInstance.methods.transfer(
+                     from_,
+                     to_,
+                     amount,
+                     tokenType,
+                     token
+                ).send();
+                await adminTransferMockFa12TokenOperation.confirmation();
+
+                const finalMockFa12TokenBalance      = 10000000;
+                const updatedMockFa12TokenStorage    = await mockFa12TokenInstance.storage();
+                const treasuryMockFa12TokenBalance   = await updatedMockFa12TokenStorage.ledger.get(treasuryAddress.address);
+
+                assert.equal(treasuryMockFa12TokenBalance.balance, finalMockFa12TokenBalance);
+
+            } catch(e){
+                console.log(e);
+            } 
+        });
+
+        it('test: whitelisted addresses (bob) can transfer mock FA2 Tokens from treasury', async () => {
+            try{        
+
+                const from_      = treasuryAddress.address;
+                const to_        = bob.pkh;
+                const amount     = 10000000;
+                const tokenType  = "fa2";
+                const token      = mockFa2TokenAddress.address;
+                const tokenId    = 0;
+
+                await signerFactory(bob.sk);
+                const adminTransferMockFa2TokenOperation = await treasuryInstance.methods.transfer(
+                     from_,
+                     to_,
+                     amount,
+                     tokenType,
+                     token,
+                     tokenId
+                ).send();
+                await adminTransferMockFa2TokenOperation.confirmation();
+
+                const finalMockFa2TokenBalance      = 10000000;
+                const updatedMockFa2TokenStorage    = await mockFa2TokenInstance.storage();
+                const treasuryMockFa2TokenBalance   = await updatedMockFa2TokenStorage.ledger.get(treasuryAddress.address);
+
+                assert.equal(treasuryMockFa2TokenBalance, finalMockFa2TokenBalance);
+
+            } catch(e){
+                console.log(e);
+            } 
+        });
+
+        it('test: whitelisted addresses (bob) can access treasury mintMvkAndTransfer entrypoint', async () => {
+            try{        
+                
+                const to_        = bob.pkh;
+                const amount     = 10000000;
+
+                const mvkTokenStorage           = await mvkTokenInstance.storage();
+                const initialBobMvkTokenBalance = await mvkTokenStorage.ledger.get(bob.pkh);
+
+
+                await signerFactory(bob.sk);
+                const mintMvkAndTransferOperation = await treasuryInstance.methods.mintMvkAndTransfer(
+                     to_,
+                     amount,
+                );
+                await mintMvkAndTransferOperation.confirmation();
+
+                const updatedMvkTokenStorage     = await mvkTokenInstance.storage();
+                const updatedBobMvkTokenBalance  = await updatedMvkTokenStorage.ledger.get(bob.pkh);
+
+                assert.equal(updatedBobMvkTokenBalance, initialBobMvkTokenBalance + amount);
+                
+
+            } catch(e){
+                console.log(e);
+            } 
+        });
+
+    }); // end test: Treasury transfer tests
+
 
 });
 
