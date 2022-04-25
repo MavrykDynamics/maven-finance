3dcc3048d92603c3d875998e4542ac3d4743b89a
Tim <tyong.espoir@gmail.com>
2022-04-25 16:19:38 +0800

    1) Token Sale Contract - addToWhitelist, RemoveFromWhitelist, BuyTokens 2) Fix bug in governanceProxyStorage

---
+++ src/contracts/test/types/tokenSaleStorageType.ts
@@ -0,0 +1,21 @@
+import { MichelsonMap, MichelsonMapKey } from '@taquito/michelson-encoder'
+import { BigNumber } from 'bignumber.js'
+
+export type tokenSaleStorageType = {
+  
+  admin                     : string;
+  metadata                  : MichelsonMap<MichelsonMapKey, unknown>;
+  config                    : {};
+
+  governanceAddress         : string;
+  treasuryAddress           : string;
+  mvkTokenAddress           : string;
+
+  tokenSaleLedger           : MichelsonMap<MichelsonMapKey, unknown>;
+  whitelistedAddresses      : MichelsonMap<MichelsonMapKey, unknown>;
+
+  tokenSaleHasStarted       : boolean;
+  whitelistAmountTotal      : BigNumber;
+  overallAmountTotal        : BigNumber;
+  
+}
