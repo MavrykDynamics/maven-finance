db581139e3b856cb85b181137f5739bee2a7280c
Tristan Allaire <tristan.allaire@alarch.fr>
2022-04-25 18:32:14 +0200

    Sandbox node updated

--- src/contracts/sandbox-init.sh
+++ src/contracts/sandbox-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-MAIN_SANDBOX_V="901d451a"
-APPLE_SANDBOX_V="cc4f24db"
+MAIN_SANDBOX_V="36434188"
+APPLE_SANDBOX_V="36434188"
 TZ_NODE_VERSION=$(docker exec -it mavryk-sandbox tezos-node --version)
 NODE_BOOTSTRAPPED=$(docker exec -it mavryk-sandbox tezos-client bootstrapped)
 until [[ "$TZ_NODE_VERSION" == *"$MAIN_SANDBOX_V"* && "$NODE_BOOTSTRAPPED" == *"Node is bootstrapped."* ]] || [[ "$TZ_NODE_VERSION" == *"$APPLE_SANDBOX_V"* && "$NODE_BOOTSTRAPPED" == *"Node is bootstrapped."* ]];
