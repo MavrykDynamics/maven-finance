e524bf13c3b284671cdf3aca2ded0704af8aa280
Tristan Allaire <tristan.allaire@alarch.fr>
2022-04-27 18:26:54 +0200

    Pre-merge fixes

--- src/contracts/test/types/governanceStorageType.ts
+++ src/contracts/test/types/governanceStorageType.ts
@@ -39,7 +39,6 @@ export type governanceStorageType = {
   snapshotMvkTotalSupply             : BigNumber;
   snapshotStakedMvkTotalSupply       : BigNumber;
 
-  proxyLambdaLedger                  : MichelsonMap<MichelsonMapKey, unknown>;
   lambdaLedger                       : MichelsonMap<MichelsonMapKey, unknown>;
 
   financialRequestLedger             : MichelsonMap<MichelsonMapKey, unknown>;
