ef6b91d790a9543f82e76baa95dac056741f1d6e
Jacques <jacques.weber@noos.fr>
2022-05-09 12:27:12 +0200

    quick fix

--- src/contracts/test/types/aggregatorFactoryStorageType.ts
+++ src/contracts/test/types/aggregatorFactoryStorageType.ts
@@ -3,14 +3,15 @@ import { MichelsonMap, MichelsonMapKey } from "@taquito/michelson-encoder";
 export type aggregatorFactoryStorageType = {
   
   admin                     : string;
+  metadata                  : MichelsonMap<MichelsonMapKey, unknown>;
+
   mvkTokenAddress           : string;
   delegationAddress         : string;
   
-  metadata                  : MichelsonMap<MichelsonMapKey, unknown>;
 
   trackedAggregators        : MichelsonMap<MichelsonMapKey, unknown>;
   trackedSatellites         : Array<unknown>;
 
   lambdaLedger              : MichelsonMap<MichelsonMapKey, unknown>;
-  aggregatorlambdaLedger    : MichelsonMap<MichelsonMapKey, unknown>;
+  aggregatorLambdaLedger    : MichelsonMap<MichelsonMapKey, unknown>;
 };
