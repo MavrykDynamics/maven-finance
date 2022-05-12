d53d38af601cfbf51c57fae31ca4e5dd3288e482
Tim <tyong.espoir@gmail.com>
2022-05-13 00:00:39 +0200

    feat: clientAggregator added + testing

---
+++ src/contracts/test/types/clientAggregatorStorageType.ts
@@ -0,0 +1,9 @@
+import { BigNumber } from "bignumber.js";
+
+export type clientAggregatorStorageType = {
+    decimals: BigNumber;
+    round: BigNumber;
+    price: BigNumber;
+    percentOracleResponse: BigNumber;
+    priceDateTime: BigNumber;
+};
