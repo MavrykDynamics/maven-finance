f821ded170c7a03bd588bec75bc02e16168418d4
Kucher Maxym <max.kucher98@gmail.com>
2022-05-13 11:36:08 +0300

    Added responsive mobile plug

---
+++ src/frontend/src/app/App.components/Mobile/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as DiscordIcon } from './Discord';
+export { default as TwitterIcon } from './Twitter';
+export { default as TelegramIcon } from './Telegram';
+export { default as MediumIcon } from './Medium';
+export { default as GitHubIcon } from './GitHub';
