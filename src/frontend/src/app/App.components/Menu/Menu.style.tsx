98ddf8e7fe0704917a1f49d520c09b0af455f711
Kucher Maxym <max.kucher98@gmail.com>
2022-05-19 15:43:24 +0300

    MAV-23: need to make smth with burger on opened menu

--- src/frontend/src/app/App.components/Menu/Menu.style.tsx
+++ src/frontend/src/app/App.components/Menu/Menu.style.tsx
@@ -1,6 +1,6 @@
-import styled, { keyframes } from 'styled-components/macro'
+import styled, { keyframes } from 'styled-components/macro';
 
-import { MavrykTheme } from '../../../styles/interfaces'
+import { MavrykTheme } from '../../../styles/interfaces';
 
 export const moveDown = keyframes`
   from {
@@ -9,37 +9,101 @@ export const moveDown = keyframes`
   to {
     transform: translateY(0rem);
   }
-`
+`;
 export const MenuStyled = styled.div<{ theme: MavrykTheme }>`
-  position: relative;
-  text-align: center;
   width: 270px;
-  min-height: 100vh;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  background-color: ${({ theme }) => theme.containerColor};
+  transition: all 0.25s;
+  .mobile-logo {
+    display: none;
+  }
+
+  @media screen and (max-width: 1445px) {
+    &:not(.menu-expanded) {
+      width: 72px;
+      .desctop-logo,
+      a .navLinkSubTitle,
+      a .navLinkTitle {
+        display: none !important;
+      }
+
+      .mobile-logo {
+        display: block;
+        width: 50px;
+        margin: 27px auto;
+        height: fit-content;
+      }
+
+      #connectWalletButton {
+        display: none;
+      }
+    }
+
+    &.menu-expanded {
+      width: 100vw;
+      display: flex;
+      display: flex;
+      background: rgba(255, 255, 255, 0.3);
+      z-index: 10;
+      align-items: flex-start;
+      .burger-menu {
+        width: 50%;
+      }
+
+      .menu-backdrop {
+        display: block;
+      }
+    }
+  }
+
   > div {
     width: 100%;
     max-width: 270px;
   }
-`
+`;
+
+export const MenuMobileBurger = styled.div<{ theme: MavrykTheme }>`
+  display: none;
+  width: 24px;
+  margin: 0 auto;
+  margin-top: 21px;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  @media screen and (max-width: 1445px) {
+    display: flex;
+  }
+  > div {
+    width: 100%;
+    height: 2px;
+    border-radius: 3px;
+    background-color: #503eaa;
+    margin: 3px 0;
+  }
+`;
 
 export const MenuTopSection = styled.div`
   display: flex;
   flex-direction: column;
   top: 0;
   left: 0;
-  z-index: 1;
-  justify-content: space-between;
-`
+  z-index: 10;
+  position: relative;
+  text-align: center;
+  width: 100%;
+  max-width: 270px;
+  min-height: 100vh;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.containerColor};
+`;
 export const MenuLogo = styled.img`
   margin: 44px auto 20px auto;
   z-index: 1;
   width: 218px;
   height: 43px;
-`
+`;
 
 export const MenuGrid = styled.div`
   display: flex;
@@ -47,7 +111,7 @@ export const MenuGrid = styled.div`
   flex-direction: column;
   justify-content: space-evenly;
   width: 100%;
-`
+`;
 
 export const MenuBottomSection = styled.div`
   display: flex;
@@ -55,29 +119,37 @@ export const MenuBottomSection = styled.div`
   justify-content: space-evenly;
   align-items: center;
   margin-top: 40px;
-`
+`;
 
 export const MenuBanner = styled.img`
   margin: 0 auto 15px auto;
-`
+`;
 
 export const MenuFooter = styled.div<{ theme: MavrykTheme }>`
-  margin: 15px auto 15px auto;
   font-size: 11px;
   color: ${({ theme }) => theme.footerColor};
   font-weight: 600;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+
+  @media screen and (max-width: 1445px) {
+    padding: 0 10px;
+    font-size: 10px;
+  }
 
   > p {
     display: inline-block;
     font-weight: 500;
   }
-`
+`;
 
 export const ThemeToggleIcon = styled.svg`
   width: inherit;
   height: inherit;
-`
+`;
 
 export const MenuSpacerDiv = styled.div<{ height: number }>`
   height: ${({ height }) => height}px;
-`
+`;
