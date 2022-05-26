d40527cfca45b3f3f327b28e5c2f92f2422aabc0
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-05-26 22:12:59 +0300

    Update colors

--- src/frontend/src/pages/Treasury/Treasury.style.tsx
+++ src/frontend/src/pages/Treasury/Treasury.style.tsx
@@ -1,20 +1,63 @@
 import styled from 'styled-components/macro'
 import { MavrykTheme } from '../../styles/interfaces'
 
-import { Card } from 'styles'
+import { Card, cyanColor, skyColor, headerColor, whiteColor } from 'styles'
 
 export const TreasuryViewStyle = styled(Card)`
   display: grid;
   grid-template-columns: auto 254px 184px;
   gap: 50px;
 
+  header {
+    display: flex;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  var {
+    color: ${cyanColor};
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 18px;
+  }
+
   .assets-block {
     display: grid;
     grid-template-columns: 100px 150px auto;
     gap: 30px;
+
+    h5 {
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 12px;
+      color: ${headerColor};
+    }
   }
 
   .right-text {
     text-align: right;
+  }
+
+  .asset-name {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 16px;
+    color: ${skyColor};
+  }
+
+  .asset-value {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 16px;
+    color: ${cyanColor};
+  }
+
+  .asset-lable {
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 18px;
+    color: ${whiteColor};
   }
 ` //TreasuryViewStyle
