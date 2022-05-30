a8e57fbcca3735117e6d4085bca4988ec2d7d6d7
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-05-30 09:59:43 +0300

    Update env

--- src/frontend/src/gql/fetchGraphQL.ts
+++ src/frontend/src/gql/fetchGraphQL.ts
@@ -47,7 +47,8 @@ async function fetchGraphQL(operationsDoc: string, operationName: string, variab
   // return await result.json()
 
   const developmentAPI = process.env.REACT_APP_DEV_GRAPHQL_API || 'https://api-dev.mavryk.finance/v1/graphql'
-  const prodictionAPI = process.env.REACT_APP_GRAPHQL_API || 'https://api.mavryk.finance/v1/graphql'
+  // TODO api-dev for test 
+  const prodictionAPI = process.env.REACT_APP_GRAPHQL_API || 'https://api-dev.mavryk.finance/v1/graphql'
   const gqlAPINetwork = process.env.NODE_ENV === 'development' ? developmentAPI : prodictionAPI
 
   return new Promise<any>((resolve, reject) => {
