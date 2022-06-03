d46ffdae106dae4bc0da72dad180d47d15027028
Tristan Allaire <tristan.allaire@alarch.fr>
2022-06-03 17:26:59 +0200

    Voting power ratio

---
+++ src/contracts/test/04b_test_governance.spec.ts
@@ -0,0 +1,323 @@
+const { TezosToolkit, ContractAbstraction, ContractProvider, Tezos, TezosOperationError } = require("@taquito/taquito")
+const { InMemorySigner, importKey } = require("@taquito/signer");
+import assert, { ok, rejects, strictEqual } from "assert";
+import { MVK, Utils, zeroAddress } from "./helpers/Utils";
+import fs from "fs";
+import { confirmOperation } from "../scripts/confirmation";
+import { BigNumber } from "bignumber.js";
+
+const chai = require("chai");
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);   
+chai.should();
+
+import env from "../env";
+import { bob, alice, eve, mallory, trudy, oscar } from "../scripts/sandbox/accounts";
+
+import doormanAddress from '../deployments/doormanAddress.json';
+import delegationAddress from '../deployments/delegationAddress.json';
+import councilAddress from '../deployments/councilAddress.json'
+import mvkTokenAddress from '../deployments/mvkTokenAddress.json';
+import governanceAddress from '../deployments/governanceAddress.json';
+import governanceProxyAddress from '../deployments/governanceProxyAddress.json';
+import emergencyGovernanceAddress from '../deployments/emergencyGovernanceAddress.json';
+import breakGlassAddress from '../deployments/breakGlassAddress.json';
+
+// import governanceLambdaParamBytes from "../build/lambdas/governanceLambdaParametersBytes.json";
+import { config } from "yargs";
+import { MichelsonMap } from "@taquito/taquito";
+
+describe("Governance tests", async () => {
+    var utils: Utils;
+
+    let doormanInstance;
+    let delegationInstance;
+    let mvkTokenInstance;
+    let governanceInstance;
+    let governanceProxyInstance;
+    let emergencyGovernanceInstance;
+    let breakGlassInstance;
+    let councilInstance;
+
+    let doormanStorage;
+    let delegationStorage;
+    let mvkTokenStorage;
+    let governanceStorage;
+    let governanceProxyStorage;
+    let emergencyGovernanceStorage;
+    let breakGlassStorage;
+    let councilStorage;
+    
+    const signerFactory = async (pk) => {
+        await utils.tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(pk) });
+        return utils.tezos;
+    };
+
+    before("setup", async () => {
+
+        utils = new Utils();
+        await utils.init(bob.sk);
+        
+        doormanInstance    = await utils.tezos.contract.at(doormanAddress.address);
+        delegationInstance = await utils.tezos.contract.at(delegationAddress.address);
+        mvkTokenInstance   = await utils.tezos.contract.at(mvkTokenAddress.address);
+        governanceInstance = await utils.tezos.contract.at(governanceAddress.address);
+        governanceProxyInstance = await utils.tezos.contract.at(governanceProxyAddress.address);
+        emergencyGovernanceInstance = await utils.tezos.contract.at(emergencyGovernanceAddress.address);
+        breakGlassInstance = await utils.tezos.contract.at(breakGlassAddress.address);
+        councilInstance = await utils.tezos.contract.at(councilAddress.address);
+            
+        doormanStorage    = await doormanInstance.storage();
+        delegationStorage = await delegationInstance.storage();
+        mvkTokenStorage   = await mvkTokenInstance.storage();
+        governanceStorage = await governanceInstance.storage();
+        governanceProxyStorage  = await governanceProxyInstance.storage();
+        emergencyGovernanceStorage = await emergencyGovernanceInstance.storage();
+        breakGlassStorage = await breakGlassInstance.storage();
+        councilStorage  = await councilInstance.storage();
+
+        console.log('-- -- -- -- -- Governance Tests -- -- -- --')
+        console.log('Doorman Contract deployed at:', doormanInstance.address);
+        console.log('Delegation Contract deployed at:', delegationInstance.address);
+        console.log('MVK Token Contract deployed at:', mvkTokenInstance.address);
+        console.log('Governance Contract deployed at:', governanceInstance.address);
+        console.log('Emergency Governance Contract deployed at:', emergencyGovernanceInstance.address);
+        console.log('Bob address: ' + bob.pkh);
+        console.log('Alice address: ' + alice.pkh);
+        console.log('Eve address: ' + eve.pkh);
+
+        // Init multiple satellites with multiple delegates
+        delegationStorage = await delegationInstance.storage();
+        const satelliteMap = await delegationStorage.satelliteLedger;
+        if(satelliteMap.get(eve.pkh) === undefined){
+
+            // ALICE WITH BOB AND TRUDY
+            await signerFactory(alice.sk)
+            updateOperators = await mvkTokenInstance.methods
+                .update_operators([
+                {
+                    add_operator: {
+                        owner: alice.pkh,
+                        operator: doormanAddress.address,
+                        token_id: 0,
+                    },
+                },
+                ])
+                .send()
+            await updateOperators.confirmation();
+            var stakeOperation = await doormanInstance.methods.stake(MVK(1)).send();
+            await stakeOperation.confirmation();
+            var registerAsSatellite = await delegationInstance.methods
+            .registerAsSatellite(
+                "Alice Satellite", 
+                "Test description", 
+                "Test image", 
+                "Test website", 
+                700
+            ).send();
+            await registerAsSatellite.confirmation();
+
+            await signerFactory(bob.sk)
+            var updateOperators = await mvkTokenInstance.methods
+                .update_operators([
+                {
+                    add_operator: {
+                        owner: bob.pkh,
+                        operator: doormanAddress.address,
+                        token_id: 0,
+                    },
+                },
+                ])
+                .send()
+            await updateOperators.confirmation();
+            stakeOperation = await doormanInstance.methods.stake(MVK(10000)).send();
+            await stakeOperation.confirmation();
+            var delegateOperation   = await delegationInstance.methods.delegateToSatellite(bob.pkh, alice.pkh).send()
+            await delegateOperation.confirmation()
+
+            await signerFactory(trudy.sk)
+            var updateOperators = await mvkTokenInstance.methods
+                .update_operators([
+                {
+                    add_operator: {
+                        owner: trudy.pkh,
+                        operator: doormanAddress.address,
+                        token_id: 0,
+                    },
+                },
+                ])
+                .send()
+            await updateOperators.confirmation();
+            stakeOperation = await doormanInstance.methods.stake(MVK(1234)).send();
+            await stakeOperation.confirmation();
+            var delegateOperation   = await delegationInstance.methods.delegateToSatellite(trudy.pkh, alice.pkh).send()
+            await delegateOperation.confirmation()
+            
+            // EVE WITH MALLORY AND OSCAR
+            await signerFactory(eve.sk)
+            updateOperators = await mvkTokenInstance.methods
+                .update_operators([
+                {
+                    add_operator: {
+                        owner: eve.pkh,
+                        operator: doormanAddress.address,
+                        token_id: 0,
+                    },
+                },
+                ])
+                .send()
+            await updateOperators.confirmation();
+            stakeOperation = await doormanInstance.methods.stake(MVK(20000)).send();
+            await stakeOperation.confirmation();
+            registerAsSatellite = await delegationInstance.methods
+            .registerAsSatellite(
+                "Eve Satellite", 
+                "Test description", 
+                "Test image", 
+                "Test website", 
+                700
+            ).send();
+            await registerAsSatellite.confirmation();
+
+            await signerFactory(mallory.sk)
+            var updateOperators = await mvkTokenInstance.methods
+                .update_operators([
+                {
+                    add_operator: {
+                        owner: mallory.pkh,
+                        operator: doormanAddress.address,
+                        token_id: 0,
+                    },
+                },
+                ])
+                .send()
+            await updateOperators.confirmation();
+            stakeOperation = await doormanInstance.methods.stake(MVK(200)).send();
+            await stakeOperation.confirmation();
+            var delegateOperation   = await delegationInstance.methods.delegateToSatellite(mallory.pkh, eve.pkh).send()
+            await delegateOperation.confirmation()
+
+            await signerFactory(oscar.sk)
+            var updateOperators = await mvkTokenInstance.methods
+                .update_operators([
+                {
+                    add_operator: {
+                        owner: oscar.pkh,
+                        operator: doormanAddress.address,
+                        token_id: 0,
+                    },
+                },
+                ])
+                .send()
+            await updateOperators.confirmation();
+            stakeOperation = await doormanInstance.methods.stake(MVK(800)).send();
+            await stakeOperation.confirmation();
+            var delegateOperation   = await delegationInstance.methods.delegateToSatellite(oscar.pkh, alice.pkh).send()
+            await delegateOperation.confirmation()
+        }
+
+        // Reset signer
+        await signerFactory(bob.sk)
+
+        // Set council contract admin to governance proxy for later tests
+        const setAdminOperation = await councilInstance.methods.setAdmin(governanceProxyAddress.address).send();
+        await setAdminOperation.confirmation()
+    });
+
+    describe("%updateConfig", async () => {
+        beforeEach("Set signer to admin", async () => {
+            await signerFactory(bob.sk)
+        });
+        it('Admin should be able to call the entrypoint and configure the voting power ratio', async () => {
+            try{
+                // Initial Values
+                governanceStorage = await governanceInstance.storage();
+                const newConfigValue = 5000;
+
+                // Operation
+                const updateConfigOperation = await governanceInstance.methods.updateConfig(newConfigValue,"configVotingPowerRatio").send();
+                await updateConfigOperation.confirmation();
+
+                // Final values
+                governanceStorage = await governanceInstance.storage();
+                const updateConfigValue = governanceStorage.config.votingPowerRatio;
+
+                // Assertions
+                assert.equal(updateConfigValue, newConfigValue);
+            } catch(e){
+                console.dir(e, {depth: 5})
+            }
+        });
+    });
+
+    describe("%startNextRound", async () => {
+        beforeEach("Set signer to standard user", async () => {
+            await signerFactory(eve.sk)
+        });
+
+        it('User should be able to start the proposal round if no round has been initiated yet', async () => {
+            try{
+                // Initial Values
+                governanceStorage = await governanceInstance.storage();
+                const currentCycleInfoRound                       = governanceStorage.currentCycleInfo.round
+                const currentCycleInfoRoundString                 = Object.keys(currentCycleInfoRound)[0]
+                const currentCycleInfoBlocksPerProposalRound      = governanceStorage.currentCycleInfo.blocksPerProposalRound
+                const currentCycleInfoBlocksPerVotingRound        = governanceStorage.currentCycleInfo.blocksPerVotingRound
+                const currentCycleInfoBlocksPerTimelockRound      = governanceStorage.currentCycleInfo.blocksPerTimelockRound
+                const currentCycleInfoRoundStartLevel             = governanceStorage.currentCycleInfo.roundStartLevel
+                const currentCycleInfoRoundEndLevel               = governanceStorage.currentCycleInfo.roundEndLevel
+                const currentCycleInfoCycleEndLevel               = governanceStorage.currentCycleInfo.cycleEndLevel
+                const cycleHighestVotedProposalId = governanceStorage.cycleHighestVotedProposalId
+
+                // Operation
+                const startNextRoundOperation = await governanceInstance.methods.startNextRound(true).send();
+                await startNextRoundOperation.confirmation();
+
+                // Final values
+                governanceStorage = await governanceInstance.storage();
+                const finalRound                       = governanceStorage.currentCycleInfo.round
+                const finalRoundString                 = Object.keys(finalRound)[0]
+                const finalBlocksPerProposalRound      = governanceStorage.currentCycleInfo.blocksPerProposalRound
+                const finalBlocksPerVotingRound        = governanceStorage.currentCycleInfo.blocksPerVotingRound
+                const finalBlocksPerTimelockRound      = governanceStorage.currentCycleInfo.blocksPerTimelockRound
+                const finalRoundStartLevel             = governanceStorage.currentCycleInfo.roundStartLevel
+                const finalRoundEndLevel               = governanceStorage.currentCycleInfo.roundEndLevel
+                const finalCycleEndLevel               = governanceStorage.currentCycleInfo.cycleEndLevel
+                const finalRoundHighestVotedProposalId = governanceStorage.cycleHighestVotedProposalId
+
+                // Assertions
+                assert.equal(currentCycleInfoRoundString, "proposal");
+                assert.equal(currentCycleInfoBlocksPerProposalRound, 0);
+                assert.equal(currentCycleInfoBlocksPerVotingRound, 0);
+                assert.equal(currentCycleInfoBlocksPerTimelockRound, 0);
+                assert.equal(currentCycleInfoRoundStartLevel, 0);
+                assert.equal(currentCycleInfoRoundEndLevel, 0);
+                assert.equal(currentCycleInfoCycleEndLevel, 0);
+                assert.equal(cycleHighestVotedProposalId, 0);
+
+                assert.equal(finalRoundString, "proposal");
+                assert.notEqual(finalBlocksPerProposalRound, currentCycleInfoBlocksPerProposalRound);
+                assert.notEqual(finalBlocksPerVotingRound, currentCycleInfoBlocksPerVotingRound);
+                assert.notEqual(finalBlocksPerTimelockRound, currentCycleInfoBlocksPerTimelockRound);
+                assert.notEqual(finalRoundStartLevel, currentCycleInfoRoundStartLevel);
+                assert.notEqual(finalRoundEndLevel, currentCycleInfoRoundEndLevel);
+                assert.notEqual(finalCycleEndLevel, currentCycleInfoCycleEndLevel);
+                assert.notEqual(finalRoundHighestVotedProposalId, cycleHighestVotedProposalId);
+
+                // Check storage
+                delegationStorage       = await delegationInstance.storage()
+                governanceStorage       = await governanceInstance.storage()
+                const snapshotLedger    = governanceStorage.snapshotLedger
+                console.dir(snapshotLedger, {depth: 5})
+
+                const firstSatellite    = delegationStorage.satelliteLedger.get(alice.pkh);
+                console.dir(firstSatellite, {depth: 5})
+
+                const secondSatellite   = delegationStorage.satelliteLedger.get(eve.pkh);
+                console.dir(secondSatellite, {depth: 5})
+            } catch(e){
+                console.dir(e, {depth: 5})
+            }
+        })
+    })
+});
