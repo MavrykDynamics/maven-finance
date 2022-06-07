e33ded25df70b23e07df363f4edae2927f2ae8e1
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-06-07 20:15:52 +0300

    update SATELLITE_RECORDS_QUERY

--- src/frontend/src/gql/queries/getSatelliteRecords.ts
+++ src/frontend/src/gql/queries/getSatelliteRecords.ts
@@ -15,38 +15,10 @@ export const SATELLITE_RECORDS_QUERY = `
       user {
         smvk_balance
         mvk_balance
-      }
-      governance_financial_requests_votes {
-        governance_financial_request_id
-        id
-        timestamp
-        vote
-        voter_id
-        voting_power
-        governance_financial_request {
-          approve_vote_total
-          disapprove_vote_total
-          executed
-          expiration_datetime
-          id
-          ready
-          request_purpose
-          requested_datetime
-          request_type
-          requester_id
-          smvk_percentage_for_approval
-          smvk_required_for_approval
-          snapshot_smvk_total_supply
-          status
-          token_amount
-          token_contract_address
-          token_id
-          token_name
-          treasury_id
-        }
+      }     
       }
     }
-  }
+  
 `
 
 export const SATELLITE_RECORDS_QUERY_OLD = `
