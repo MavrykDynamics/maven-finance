cebcc40b0b2a42d719796216d45cdaaa347ee3da
Tim <tyong.espoir@gmail.com>
2022-06-09 16:56:04 +0800

    Minor bug fix: aggregator storage name | Update accounts for testnet (oracleMaintainer, oracle0, oracle1, oracle2)

--- src/contracts/test/types/aggregatorStorageType.ts
+++ src/contracts/test/types/aggregatorStorageType.ts
@@ -6,6 +6,7 @@ export type aggregatorStorageType = {
   
   admin                     : string;
   metadata                  : MichelsonMap<MichelsonMapKey, unknown>;
+  name                      : string;
   
   config                    : {
     nameMaxLength                       : BigNumber;
