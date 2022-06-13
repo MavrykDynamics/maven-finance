91cdf1856cacdfa85a1d3f39803606829287dfd4
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-06-13 16:32:06 +0300

    hide voting

--- src/frontend/src/pages/Governance/Governance.view.tsx
+++ src/frontend/src/pages/Governance/Governance.view.tsx
@@ -292,7 +292,7 @@ export const GovernanceView = ({
           ) : null}
 
           <div className="voting-proposal">
-            <VotingArea
+            {/*<VotingArea
               ready={ready}
               loading={loading}
               accountPkh={accountPkh}
@@ -300,7 +300,7 @@ export const GovernanceView = ({
               handleVotingRoundVote={handleVotingRoundVote}
               selectedProposal={rightSideContent}
               voteStatistics={voteStatistics}
-            />
+            />*/}
             {isExecuteProposal ? (
               <Button
                 className="execute-proposal"
