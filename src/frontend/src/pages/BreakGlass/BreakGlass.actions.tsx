4f5b9704d1e67a23c72d5559b30389d3a9d48585
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-06-14 22:37:03 +0300

    Add Treasury Factory

--- src/frontend/src/pages/BreakGlass/BreakGlass.actions.tsx
+++ src/frontend/src/pages/BreakGlass/BreakGlass.actions.tsx
@@ -15,19 +15,6 @@ export const SET_GLASS_BROKEN = 'SET_GLASS_BROKEN'
 export const getBreakGlassStorage = (accountPkh?: string) => async (dispatch: any, getState: any) => {
   const state: State = getState()
 
-  // if (!accountPkh) {
-  //   dispatch(showToaster(ERROR, 'Public address not found', 'Make sure your wallet is connected'))
-  //   return
-  // }
-  // const contract = accountPkh
-  //   ? await state.wallet.tezos?.wallet.at(breakGlassAddress.address)
-  //   : await new TezosToolkit(
-  //       (process.env.REACT_APP_RPC_PROVIDER as any) || 'https://hangzhounet.api.tez.ie/',
-  //     ).contract.at(breakGlassAddress.address)
-  //
-  // const storage = await (contract as any).storage()
-  // console.log('Printing out Break Glass storage:\n', storage)
-
   const storage = await fetchFromIndexer(
     BREAK_GLASS_STORAGE_QUERY,
     BREAK_GLASS_STORAGE_QUERY_NAME,
