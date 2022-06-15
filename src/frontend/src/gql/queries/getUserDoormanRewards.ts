4bfe269831de3b817b02e1c0e97afcf80fe4cb68
Sam Morrow <smorrow32@gmail.com>
2022-06-15 15:26:59 +0300

    1. Added gql queries for a user's getting Doorman, Satellite, and Farm rewards 2. Updated User state to have fields for the user rewards stated above along with the data needed to calculate them 3. Readied functions for the reward calculations 4. Updated Doorman page and compound button functionality to handle changes + only show the option to compound when a user has enough rewards 5. Fixed condition for showing Not Staking flag

---
+++ src/frontend/src/gql/queries/getUserDoormanRewards.ts
@@ -0,0 +1,21 @@
+export const USER_DOORMAN_REWARDS_QUERY = `
+  query GetUserDoormanRewards ($_eq: String = "") {
+    doorman {
+      unclaimed_rewards
+      accumulated_fees_per_share
+      stake_accounts(where: {address: {_eq: $_eq}}) {
+        address
+        participation_fees_per_share
+        smvk_balance
+        mvk_balance
+      }
+    }
+  }
+`
+
+export const USER_DOORMAN_REWARDS_QUERY_NAME = 'GetUserDoormanRewards'
+
+export function USER_DOORMAN_REWARDS_QUERY_VARIABLES(address: string): Record<string, any> {
+  /* prettier-ignore */
+  return { _eq: address }
+}
