c19ebd1f2f295cc3c735bf724c672fcf0b14d37b
Tristan Allaire <tristan.allaire@alarch.fr>
2022-06-16 16:13:37 +0200

    Aggregator indexing continued

--- src/indexer/mavryk/handlers/on_aggregator_toggle_pause_set_observation_commit.py
+++ src/indexer/mavryk/handlers/on_aggregator_toggle_pause_set_observation_commit.py
@@ -3,9 +3,28 @@ from mavryk.types.aggregator.storage import AggregatorStorage
 from dipdup.models import Transaction
 from mavryk.types.aggregator.parameter.toggle_pause_set_observation_commit import TogglePauseSetObservationCommitParameter
 from dipdup.context import HandlerContext
+import mavryk.models as models
 
 async def on_aggregator_toggle_pause_set_observation_commit(
     ctx: HandlerContext,
     toggle_pause_set_observation_commit: Transaction[TogglePauseSetObservationCommitParameter, AggregatorStorage],
 ) -> None:
-    ...
+
+    # Get operation info
+    aggregator_address                                  = toggle_pause_set_observation_commit.data.target_address
+    request_rate_update_paused                          = toggle_pause_set_observation_commit.storage.breakGlassConfig.requestRateUpdateIsPaused
+    request_rate_update_deviation_paused                = toggle_pause_set_observation_commit.storage.breakGlassConfig.requestRateUpdateDeviationIsPaused
+    set_observation_commit_paused                       = toggle_pause_set_observation_commit.storage.breakGlassConfig.setObservationCommitIsPaused
+    set_observation_reveal_paused                       = toggle_pause_set_observation_commit.storage.breakGlassConfig.setObservationRevealIsPaused
+    withdraw_reward_xtz_paused                          = toggle_pause_set_observation_commit.storage.breakGlassConfig.withdrawRewardXtzIsPaused
+    withdraw_reward_smvk_paused                         = toggle_pause_set_observation_commit.storage.breakGlassConfig.withdrawRewardStakedMvkIsPaused
+
+    # Update record
+    aggregator                                          = await models.Aggregator.get(address    = aggregator_address)
+    aggregator.request_rate_update_paused               = request_rate_update_paused
+    aggregator.request_rate_update_deviation_paused     = request_rate_update_deviation_paused
+    aggregator.set_observation_commit_paused            = set_observation_commit_paused
+    aggregator.set_observation_reveal_paused            = set_observation_reveal_paused
+    aggregator.withdraw_reward_xtz_paused               = withdraw_reward_xtz_paused
+    aggregator.withdraw_reward_smvk_paused              = withdraw_reward_smvk_paused
+    await aggregator.save()
