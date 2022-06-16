0e365c6a8ff17a7c2861aa18d40545dec0e296da
Tristan Allaire <tristan.allaire@alarch.fr>
2022-06-16 16:13:37 +0200

    Aggregator indexing continued

--- src/indexer/mavryk/handlers/on_aggregator_set_governance.py
+++ src/indexer/mavryk/handlers/on_aggregator_set_governance.py
@@ -1,11 +1,19 @@
 
+from mavryk.utils.persisters import persist_governance
 from mavryk.types.aggregator.storage import AggregatorStorage
 from dipdup.models import Transaction
 from mavryk.types.aggregator.parameter.set_governance import SetGovernanceParameter
 from dipdup.context import HandlerContext
+import mavryk.models as models
 
 async def on_aggregator_set_governance(
     ctx: HandlerContext,
     set_governance: Transaction[SetGovernanceParameter, AggregatorStorage],
 ) -> None:
-    ...
+    
+    # Get operation info
+    target_contract = set_governance.data.target_address
+    contract        = await models.AggregatorFactory.get(address = target_contract)
+
+    # Persist new admin
+    await persist_governance(set_governance, contract)
--- src/indexer/mavryk/handlers/on_aggregator_update_general_contracts.py
+++ src/indexer/mavryk/handlers/on_aggregator_update_general_contracts.py
@@ -1,4 +1,5 @@
 
+from mavryk.utils.persisters import persist_general_contract
 from mavryk.types.aggregator.parameter.update_general_contracts import UpdateGeneralContractsParameter
 from mavryk.types.aggregator.storage import AggregatorStorage
 from dipdup.models import Transaction
@@ -8,4 +9,6 @@ async def on_aggregator_update_general_contracts(
     ctx: HandlerContext,
     update_general_contracts: Transaction[UpdateGeneralContractsParameter, AggregatorStorage],
 ) -> None:
-    ...
+
+    # Perists general contract
+    await persist_general_contract(update_general_contracts)
