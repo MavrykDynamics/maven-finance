0dd344866fde62fbad698c8500fefafabcb0b221
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-06-21 21:54:19 +0300

    add style My Past Council Actions

--- src/frontend/src/pages/Council/CouncilPastAction/CouncilPastAction.style.tsx
+++ src/frontend/src/pages/Council/CouncilPastAction/CouncilPastAction.style.tsx
@@ -1,6 +1,42 @@
 import styled from 'styled-components/macro'
-import { Card, downColor, upColor, skyColor, headerColor, royalPurpleColor } from 'styles'
+import { Card, downColor, upColor, skyColor, headerColor, cyanColor } from 'styles'
 
 export const CouncilPastActionStyled = styled(Card)`
   margin: 0;
+  margin-bottom: 10px;
+  display: grid;
+  grid-template-columns: 160px 220px auto 70px;
+  align-items: center;
+  padding-top: 17px;
+  padding-block: 17px;
+
+  p {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 21px;
+    color: ${skyColor};
+    margin-top: 0;
+    margin-bottom: 4px;
+  }
+
+  h4 {
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 14px;
+    color: ${cyanColor};
+  }
+
+  figure {
+    margin: 0;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    svg {
+      fill: none;
+      stroke: ${headerColor};
+      width: 16px;
+      height: 16px;
+    }
+  }
 `
--- src/frontend/src/pages/Council/CouncilPastAction/CouncilPastAction.view.tsx
+++ src/frontend/src/pages/Council/CouncilPastAction/CouncilPastAction.view.tsx
@@ -1,6 +1,30 @@
+import { Link } from 'react-router-dom'
+
+import Icon from '../../../app/App.components/Icon/Icon.view'
+
 // style
 import { CouncilPastActionStyled } from './CouncilPastAction.style'
 
 export const CouncilPastActionView = () => {
-  return <CouncilPastActionStyled>CouncilPastActionStyled</CouncilPastActionStyled>
+  return (
+    <CouncilPastActionStyled>
+      <div>
+        <p>Date</p>
+        <h4>Nov 11th, 2022</h4>
+      </div>
+      <div>
+        <p>Purpose</p>
+        <h4>Change Council Member</h4>
+      </div>
+      <div>
+        <p>Approved / vetoed</p>
+        <h4>5/9</h4>
+      </div>
+      <figure>
+        <Link to="/">
+          <Icon id="send" />
+        </Link>
+      </figure>
+    </CouncilPastActionStyled>
+  )
 }
