2e70298890012ae96a95e9b5b484b152aed50e09
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-06-21 20:34:42 +0300

    add style pending-items

--- src/frontend/src/pages/Council/CouncilPending/CouncilPending.view.tsx
+++ src/frontend/src/pages/Council/CouncilPending/CouncilPending.view.tsx
@@ -1,5 +1,6 @@
 // components
 import { TzAddress } from '../../../app/App.components/TzAddress/TzAddress.view'
+import { Button } from '../../../app/App.components/Button/Button.controller'
 
 // style
 import { CouncilPendingStyled } from './CouncilPending.style'
@@ -8,14 +9,13 @@ export const CouncilPendingView = () => {
   return (
     <CouncilPendingStyled>
       <h3>Add Vestee</h3>
-      <div>
+      <div className="parameters">
         <p>Parameters</p>
         <p>Signed</p>
-      </div>
-      <div>
         <TzAddress tzAddress={'tz1ezDb77a9jaFMHDWs8QXrKEDkpgGdgsjPD'} hasIcon={false} />
-        <p>3/8</p>
+        <div>3/8</div>
       </div>
+      <Button text="Sign" className="sign-btn" kind={'actionPrimary'} icon="sign" onClick={() => null} />
     </CouncilPendingStyled>
   )
 }
