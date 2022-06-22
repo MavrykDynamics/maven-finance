25aa9dec8c5587d3c1081073896191a425755500
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-06-22 17:52:29 +0300

    Fix z index

--- src/frontend/src/gql/queries/getCouncilStorage.ts
+++ src/frontend/src/gql/queries/getCouncilStorage.ts
@@ -4,19 +4,38 @@ export const COUNCIL_STORAGE_QUERY = `
       action_counter
       action_expiry_days
       address
+      admin
+      council_member_image_max_length
+      council_member_name_max_length
+      governance_id
+      request_purpose_max_length
+      request_token_name_max_length
       threshold
       council_action_records {
         action_type
-        executed
         council_id
+        executed
         executed_datetime
+        executed_level
         expiration_datetime
         id
-        initiator_id
-        start_datetime
         status
+        start_datetime
+        signers_count
+        initiator_id
+        signers {
+          id
+          signer_id
+        }
       }
-      governance_id
+      council_member_website_max_length
+      council_council_members {
+        id
+        name
+        image
+        user_id
+        website
+      }
     }
   }
 `
