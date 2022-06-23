790c8fd2309e5319c63d0412dc7ee411901ae96f
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-06-23 23:20:53 +0300

    Add CouncilFormAddCouncilMember

--- src/frontend/src/pages/Council/CouncilForms/CouncilFormAddCouncilMember.view.tsx
+++ src/frontend/src/pages/Council/CouncilForms/CouncilFormAddCouncilMember.view.tsx
@@ -3,45 +3,58 @@ import { TzAddress } from '../../../app/App.components/TzAddress/TzAddress.view'
 import { Input } from '../../../app/App.components/Input/Input.controller'
 import { Button } from '../../../app/App.components/Button/Button.controller'
 import Icon from '../../../app/App.components/Icon/Icon.view'
+import { IPFSUploader } from '../../../app/App.components/IPFSUploader/IPFSUploader.controller'
 
 // style
 import { CouncilFormStyled } from './CouncilForms.style'
 
-export const CouncilFormAddVestee = () => {
+export const CouncilFormAddCouncilMember = () => {
+  const disabled = false
+
   return (
     <CouncilFormStyled>
       <a className="info-link" href="https://mavryk.finance/litepaper#mavryk-council" target="_blank" rel="noreferrer">
         <Icon id="question" />
       </a>
-      <h1 className="form-h1">Add Vestee</h1>
-      <p>Please enter valid function parameters for adding a vestee</p>
+      <h1 className="form-h1">Add Council Member</h1>
+      <p>Please enter valid function parameters for adding a council member</p>
       <div className="form-grid">
         <div>
-          <label>Vestee Address</label>
+          <label>Council Member Address</label>
           <Input type="text" value={''} onChange={() => null} onBlur={() => {}} />
         </div>
 
         <div>
-          <label>Total Allocated Amount</label>
+          <label>Council Member Name</label>
           <Input type="text" value={''} onChange={() => null} onBlur={() => {}} />
         </div>
 
         <div>
-          <label>
-            Cliff Period <small>(in months)</small>
-          </label>
-          <Input type="text" value={''} onChange={() => null} onBlur={() => {}} />
-        </div>
-
-        <div>
-          <label>
-            Vesting Period <small>(in months)</small>
-          </label>
+          <label>Council Member Website URL</label>
           <Input type="text" value={''} onChange={() => null} onBlur={() => {}} />
         </div>
       </div>
+      <IPFSUploader
+        disabled={disabled}
+        typeFile="image"
+        imageIpfsUrl={''}
+        className="form-ipfs"
+        setIpfsImageUrl={(e: any) => {
+          // setForm({ ...form, image: e })
+          // setValidForm({ ...validForm, image: Boolean(e) })
+          // setFormInputStatus({ ...formInputStatus, image: Boolean(e) ? 'success' : 'error' })
+        }}
+        title={'Upload Profile Pic'}
+        //listNumber={6}
+      />
       <div className="btn-group">
-        <Button text="Add Vestee" className="plus-btn" kind={'actionPrimary'} icon="plus" onClick={() => null} />
+        <Button
+          text="Add Council Member"
+          className="plus-btn"
+          kind={'actionPrimary'}
+          icon="plus"
+          onClick={() => null}
+        />
       </div>
     </CouncilFormStyled>
   )
