733f10ef79af30fb8e2ed4205a4f6888fecd87a0
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-07-05 16:38:20 +0300

    Total Active Satellites

--- src/frontend/src/pages/SatelliteGovernance/SatelliteGovernance.controller.tsx
+++ src/frontend/src/pages/SatelliteGovernance/SatelliteGovernance.controller.tsx
@@ -23,6 +23,10 @@ import { DropdownWrap, DropdownCard } from '../../app/App.components/DropDown/Dr
 
 export const SatelliteGovernance = () => {
   const { accountPkh } = useSelector((state: State) => state.wallet)
+  const { delegationStorage } = useSelector((state: State) => state.delegation)
+  const satelliteLedger = delegationStorage?.satelliteLedger
+
+  const satelliteLedgerActive = satelliteLedger.filter((item) => item.active)
   const itemsForDropDown = [
     { text: 'Chose action', value: '' },
     { text: 'Suspend Satellite', value: 'suspendSatellite' },
@@ -61,7 +65,7 @@ export const SatelliteGovernance = () => {
           <div className="satellite-governance-info">
             <h3>Total Active Satellites</h3>
             <p>
-              350{' '}
+              {satelliteLedgerActive?.length}{' '}
               <a
                 className="info-link"
                 href="https://mavryk.finance/litepaper#satellites-governance-and-the-decentralized-oracle"
