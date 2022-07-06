3bcbf532c8333e761f87af7e01c01ff63b91df95
Tristan Allaire <tristan.allaire@alarch.fr>
2022-07-06 17:38:31 +0200

    Indexer connected to Mainnet and Testnet at the same time + Liquidity Baking contract indexed + Droplet module updated with ssh keys support + ansible playbook started for TimescaleDB

--- src/indexer/mavryk/models.py
+++ src/indexer/mavryk/models.py
@@ -1,6 +1,46 @@
 from tortoise import Model, fields
 from enum import IntEnum
 
+###
+# Tezos Ecosystem
+###
+class DexType(IntEnum):
+    ADD_LIQUIDITY     = 0
+    REMOVE_LIQUIDITY  = 1
+    XTZ_TO_TOKEN      = 2
+    TOKEN_TO_XTZ      = 3
+    TOKEN_TO_TOKEN    = 4
+
+class LiquidityBaking(Model):
+    address                                 = fields.CharField(pk=True, max_length=36)
+    token_pool                              = fields.BigIntField(default=0)
+    xtz_pool                                = fields.BigIntField(default=0)
+    lqt_total                               = fields.BigIntField(default=0)
+    token_address                           = fields.CharField(max_length=36, default="")
+    lqt_address                             = fields.CharField(max_length=36, default="")
+    xtz_decimals                            = fields.SmallIntField(default=6)
+    token_decimals                          = fields.SmallIntField(default=8)
+
+    class Meta:
+        table = 'liquidity_baking'
+
+class LiquidityBakingHistoryData(Model):
+    id                                      = fields.BigIntField(pk=True)
+    liquidity_baking                        = fields.ForeignKeyField('models.LiquidityBaking', related_name='history_data')
+    timestamp                               = fields.DatetimeField()
+    type                                    = fields.IntEnumField(enum_type=DexType)
+    token_pool                              = fields.BigIntField(default=0)
+    xtz_pool                                = fields.BigIntField(default=0)
+    lqt_total                               = fields.BigIntField(default=0)
+    xtz_token_price                         = fields.FloatField(default=0.0)
+    token_xtz_price                         = fields.FloatField(default=0.0)
+
+    class Meta:
+        table = 'liquidity_baking_history_data'
+
+###
+# Mavryk Ecosystem
+###
 class StakeType(IntEnum):
     STAKE               = 0
     UNSTAKE             = 1
