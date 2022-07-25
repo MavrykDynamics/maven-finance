fbcd04dbd48d141991d866abe1fca539df4ddd82
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-07-25 15:47:49 +0300

    The main image on the header of the Proposal Submission page is smaller

--- src/frontend/src/app/App.components/PageHeader/PageHeader.style.tsx
+++ src/frontend/src/app/App.components/PageHeader/PageHeader.style.tsx
@@ -79,6 +79,7 @@ export const PageHeaderForegroundImage = styled.img<{ page: string; src: string 
     switch (page) {
       case 'governance':
       case 'council':
+      case 'financial requests':
       case 'break glass':
       case 'emergency governance':
       case 'proposal submission':
@@ -91,6 +92,7 @@ export const PageHeaderForegroundImage = styled.img<{ page: string; src: string 
     switch (page) {
       case 'governance':
       case 'council':
+      case 'financial requests':
       case 'dashboard':
       case 'staking':
         return 'fit-content'
@@ -110,8 +112,9 @@ export const PageHeaderForegroundImage = styled.img<{ page: string; src: string 
         return '130px'
       case 'governance':
       case 'council':
+      case 'financial requests':
       case 'proposal submission':
-        return '156px'
+        return '158px'
       case 'satellite-governance':
         return '192px'
       default:
@@ -124,8 +127,9 @@ export const PageHeaderForegroundImage = styled.img<{ page: string; src: string 
         return '-20px'
       case 'governance':
       case 'council':
+      case 'financial requests':
       case 'proposal submission':
-        return '6px'
+        return '7px'
       case 'emergency governance':
         return '-2px'
       default:
