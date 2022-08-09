c33015b8fb700b3e82a72b2b9d57e622e2d96856
Tim <tyong.espoir@gmail.com>
2022-08-09 20:38:35 +0800

    1) Token Pool LP Token setup 2) Vault / Lending Controller minor fixes (types and errors) 3) Lending Controller bug fixes 4) Lending Controller tests with setLoanTokens 5) Refactor scripts in package.json to accept multiple contracts for compiling

---
+++ src/contracts/storage/tokenPoolLpTokenStorage.ts
@@ -0,0 +1,72 @@
+import { MichelsonMap } from '@taquito/michelson-encoder'
+
+import { BigNumber } from 'bignumber.js'
+import { Buffer } from 'buffer'
+import { array } from 'yargs'
+
+const { alice, bob, eve, mallory } = require('../scripts/sandbox/accounts')
+
+
+import { tokenPoolLpTokenStorageType } from '../test/types/tokenPoolLpTokenStorageType'
+
+export const tokenPoolLpTokenDecimals = 6
+
+const totalSupply      = 0                        // 500 LP Tokens
+const initialSupply    = new BigNumber(totalSupply)       // 500 LP Tokens
+
+const metadata = MichelsonMap.fromLiteral({
+  '': Buffer.from('tezos-storage:data', 'ascii').toString('hex'),
+  data: Buffer.from(
+    JSON.stringify({
+      version: 'v1.0.0',
+      description: 'LP Token TEST',
+      authors: ['MAVRYK Dev Team <contact@mavryk.finance>'],
+      source: {
+        tools: ['Ligo', 'Flextesa'],
+        location: 'https://ligolang.org/',
+      },
+      interfaces: ['TZIP-7', 'TZIP-12', 'TZIP-16', 'TZIP-21'],
+      errors: [],
+      views: [],
+      assets: [
+        {
+          symbol: Buffer.from('LP1-LP2').toString('hex'),
+          name: Buffer.from('LP Token TEST').toString('hex'),
+          decimals: Buffer.from(tokenPoolLpTokenDecimals.toString()).toString('hex'),
+          icon: Buffer.from('https://mavryk.finance/logo192.png').toString('hex'),
+          shouldPreferSymbol: true,
+          thumbnailUri: 'https://mavryk.finance/logo192.png',
+        },
+      ],
+    }),
+    'ascii',
+  ).toString('hex'),
+})
+
+const ledger = MichelsonMap.fromLiteral({})
+
+const token_metadata = MichelsonMap.fromLiteral({
+  0: {
+    token_id: '0',
+    token_info: MichelsonMap.fromLiteral({
+      symbol: Buffer.from('LP1-LP2').toString('hex'),
+      name: Buffer.from('LP Token TEST').toString('hex'),
+      decimals: Buffer.from(tokenPoolLpTokenDecimals.toString()).toString('hex'),
+      icon: Buffer.from('https://mavryk.finance/logo192.png').toString('hex'),
+      shouldPreferSymbol: Buffer.from(new Uint8Array([1])).toString('hex'),
+      thumbnailUri: Buffer.from('https://mavryk.finance/logo192.png').toString('hex'),
+    }),
+  },
+})
+
+export const tokenPoolLpTokenStorage: tokenPoolLpTokenStorageType = {
+  admin               : alice.pkh,
+  metadata            : metadata,
+  token_metadata      : token_metadata,
+  totalSupply         : initialSupply,
+
+  whitelistContracts  : MichelsonMap.fromLiteral({}),
+
+  ledger              : ledger,
+  operators           : MichelsonMap.fromLiteral({}),
+}
