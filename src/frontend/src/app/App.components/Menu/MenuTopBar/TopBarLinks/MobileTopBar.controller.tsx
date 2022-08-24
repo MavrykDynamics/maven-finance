0ecd7b53957b03641551c1336271c88ebdb5d590
Kucher Maxym <max.kucher98@gmail.com>
2022-08-24 16:17:10 +0300

    added some links for topBar menu

--- src/frontend/src/app/App.components/Menu/MenuTopBar/TopBarLinks/MobileTopBar.controller.tsx
+++ src/frontend/src/app/App.components/Menu/MenuTopBar/TopBarLinks/MobileTopBar.controller.tsx
@@ -1,6 +1,6 @@
 import { ConnectWallet } from 'app/App.components/ConnectWallet/ConnectWallet.controller'
 import React, { useState } from 'react'
-import { SocialIcons } from '../MenuTopBar.controller'
+import { ABOUT_LINKS, BLOG_LINKS, DOCS_LINKS, PRODUCTS_LINKS, SocialIcons } from '../MenuTopBar.controller'
 import { MobileTopBarStyled } from '../MenuTopBar.style'
 import { TopBarLinks } from './TopBarLinks.controller'
 
@@ -13,12 +13,7 @@ export const MobileTopBar = ({ show }: { show: boolean }) => {
       <div className="container">
         <TopBarLinks
           groupName={'Products'}
-          groupLinks={[
-            { name: 'Dapp', href: '/' },
-            { name: 'Liquidity Baking', href: '/' },
-            { name: 'Mavryk Bakery', href: '/' },
-            { name: 'DAO Bakery', href: '/' },
-          ]}
+          groupLinks={PRODUCTS_LINKS}
           useClickOpening
           selectedLinksBlock={selectedLinksBlock}
           setSelectedLinksBlock={() => {
@@ -27,27 +22,17 @@ export const MobileTopBar = ({ show }: { show: boolean }) => {
         />
         <TopBarLinks
           groupName={'About'}
-          groupLinks={[
-            { name: 'Who we are', href: 'https://mavryk.finance/' },
-            { name: 'MVK Token', href: '/' },
-            { name: 'Team', href: '/' },
-            { name: 'Roadmap', href: '/' },
-          ]}
+          groupLinks={ABOUT_LINKS}
           useClickOpening
           selectedLinksBlock={selectedLinksBlock}
           setSelectedLinksBlock={() => {
             setSelectedLinksBlock(selectedLinksBlock === 'About' ? null : 'About')
           }}
         />
-        <TopBarLinks groupName={'Blog 🔥'} groupLinks={[]} useClickOpening />
+        <TopBarLinks groupName={'Blog 🔥'} groupLinks={BLOG_LINKS} useClickOpening />
         <TopBarLinks
           groupName={'Docs'}
-          groupLinks={[
-            { name: 'Litepaper', href: 'https://mavryk.finance/litepaper' },
-            { name: 'DAO docs', href: '/' },
-            { name: 'Security Audits', href: '/' },
-            { name: 'Github', href: 'https://github.com/mavrykfinance/' },
-          ]}
+          groupLinks={DOCS_LINKS}
           useClickOpening
           selectedLinksBlock={selectedLinksBlock}
           setSelectedLinksBlock={() => {
