f49d9323e34427c6ba580ff3b9c818e200af200e
Kucher Maxym <max.kucher98@gmail.com>
2022-08-24 16:27:51 +0300

    slamm fix

--- src/frontend/src/app/App.components/Menu/Menu.style.tsx
+++ src/frontend/src/app/App.components/Menu/Menu.style.tsx
@@ -101,10 +101,13 @@ export const MenuSidebarContent = styled.div`
   text-align: center;
   width: 100%;
   max-width: 232px;
-  min-height: 100vh;
   height: 100%;
   background-color: ${({ theme }) => theme.containerColor};
   padding-top: 80px;
+  min-height: 650px;
+  transition: 0.6s all;
+  height: fit-content;
+  overflow-y: scroll;
 
   &::-webkit-scrollbar {
     display: none;
