951649e7820f77b3e6ccb17329d98af560864ff7
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-08-30 23:22:14 +0300

    Update types

--- src/frontend/src/pages/Doorman/ExitFeeModal/ExitFeeModal.actions.tsx
+++ src/frontend/src/pages/Doorman/ExitFeeModal/ExitFeeModal.actions.tsx
@@ -1,9 +1,10 @@
 import { showToaster } from 'app/App.components/Toaster/Toaster.actions'
 import { ERROR } from 'app/App.components/Toaster/Toaster.constants'
 import { State } from 'reducers'
+import type { AppDispatch, GetState } from 'app/App.controller'
 
 export const SHOW_EXIT_FEE_MODAL = 'SHOW_EXIT_FEE_MODAL'
-export const showExitFeeModal = (amount: number) => (dispatch: any, getState: any) => {
+export const showExitFeeModal = (amount: number) => (dispatch: AppDispatch, getState: GetState) => {
   const state: State = getState()
 
   if (!state.wallet.ready) {
@@ -23,14 +24,14 @@ export const showExitFeeModal = (amount: number) => (dispatch: any, getState: an
 }
 
 export const HIDE_EXIT_FEE_MODAL = 'HIDE_EXIT_FEE_MODAL'
-export const hideExitFeeModal = () => (dispatch: any) => {
+export const hideExitFeeModal = () => (dispatch: AppDispatch) => {
   dispatch({
     type: HIDE_EXIT_FEE_MODAL,
   })
 }
 
 export const SET_EXIT_FEE_AMOUNT = 'SET_EXIT_FEE_AMOUNT'
-export const setExitFeeAmount = (amount: number) => (dispatch: any) => {
+export const setExitFeeAmount = (amount: number) => (dispatch: AppDispatch) => {
   dispatch({
     type: SET_EXIT_FEE_AMOUNT,
     amount,
--- src/frontend/src/pages/EmergencyGovernance/EmergencyGovProposalModal/EmergencyGovProposalModal.actions.tsx
+++ src/frontend/src/pages/EmergencyGovernance/EmergencyGovProposalModal/EmergencyGovProposalModal.actions.tsx
@@ -1,9 +1,10 @@
 import { showToaster } from 'app/App.components/Toaster/Toaster.actions'
 import { ERROR } from 'app/App.components/Toaster/Toaster.constants'
 import { State } from 'reducers'
+import type { AppDispatch, GetState } from 'app/App.controller'
 
 export const SHOW_EXIT_FEE_MODAL = 'SHOW_EXIT_FEE_MODAL'
-export const showExitFeeModal = () => (dispatch: any, getState: any) => {
+export const showExitFeeModal = () => (dispatch: AppDispatch, getState: GetState) => {
   const state: State = getState()
 
   if (!state.wallet.ready) {
@@ -17,7 +18,7 @@ export const showExitFeeModal = () => (dispatch: any, getState: any) => {
 }
 
 export const HIDE_EXIT_FEE_MODAL = 'HIDE_EXIT_FEE_MODAL'
-export const hideExitFeeModal = () => (dispatch: any) => {
+export const hideExitFeeModal = () => (dispatch: AppDispatch) => {
   dispatch({
     type: HIDE_EXIT_FEE_MODAL,
   })
