c03b532609341858505da60e162c44dceb94be91
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-08-31 08:11:03 +0300

    update utils types

--- src/frontend/src/utils/api.ts
+++ src/frontend/src/utils/api.ts
@@ -3,39 +3,32 @@ const axios = require('axios').default
 // const network = process.env.REACT_APP_API_NETWORK
 const network = 'ghostnet'
 
-
-export async function getContractBigmapKeys(contractAddress: string, name: string): Promise<any> {
+export async function getContractBigmapKeys(contractAddress: string, name: string) {
   return await axios
-    .get(
-      `https://api.${network}.tzkt.io/v1/contracts/${contractAddress}/bigmaps/${name}/keys`,
-    )
-    .then((response: any) => {
+    .get(`https://api.${network}.tzkt.io/v1/contracts/${contractAddress}/bigmaps/${name}/keys`)
+    .then((response: { data: object }) => {
       return response.data
     })
 }
 
-export async function getContractStorage(contractAddress: string): Promise<any> {
+export async function getContractStorage(contractAddress: string) {
   return await axios
     .get(`https://api.${network}.tzkt.io/v1/contracts/${contractAddress}/storage`)
-    .then((response: any) => {
+    .then((response: { data: object }) => {
       return response.data
     })
 }
 
-export async function getChainInfo(): Promise<any> {
-  return await axios.get(`https://api.${network}.tzkt.io/v1/head`).then((response: any) => {
+export async function getChainInfo() {
+  return await axios.get(`https://api.${network}.tzkt.io/v1/head`).then((response: { data: object }) => {
     return response.data
   })
 }
 
-export async function getTreasuryDataByAddress(treasuryAddress: string): Promise<any> {
+export async function getTreasuryDataByAddress(treasuryAddress: string) {
   return await axios
-    .get(
-      `https://api.better-call.dev/v1/account/${
-        network
-      }/${treasuryAddress}/token_balances`,
-    )
-    .then((response: any) => {
+    .get(`https://api.better-call.dev/v1/account/${network}/${treasuryAddress}/token_balances`)
+    .then((response: { data: object }) => {
       return response.data
     })
 }
