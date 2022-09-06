1231adc4b7e5b23c898402d200477517defcdb9a
Kucher Maxym <max.kucher98@gmail.com>
2022-09-06 15:52:10 +0300

    added mvk stats block

--- src/frontend/src/pages/Dashboard/Dashboard.controller.tsx
+++ src/frontend/src/pages/Dashboard/Dashboard.controller.tsx
@@ -1,24 +1,44 @@
-import * as React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { State } from '../../reducers'
 import { useEffect } from 'react'
-import { DashboardStyled } from './Dashboard.style'
 import { getVestingStorage } from '../Treasury/Treasury.actions'
 import { Page } from 'styles'
 import { PageHeader } from '../../app/App.components/PageHeader/PageHeader.controller'
 import { DashboardView } from './Dashboard.view'
 
+export type mvkStatsType = {
+  marketCap: number
+  stakedMvk: number
+  circuatingSupply: number
+  maxSupply: number
+  livePrice: number
+  prevPrice: number
+}
+
 export const Dashboard = () => {
   const dispatch = useDispatch()
+  const {
+    exchangeRate,
+    mvkTokenStorage: { totalSupply, maximumTotalSupply },
+  } = useSelector((state: State) => state.mvkToken)
 
   useEffect(() => {
     dispatch(getVestingStorage())
   }, [dispatch])
 
+  const mvkStatsBlock: mvkStatsType = {
+    marketCap: 0,
+    stakedMvk: 0,
+    circuatingSupply: totalSupply,
+    maxSupply: maximumTotalSupply,
+    livePrice: exchangeRate,
+    prevPrice: exchangeRate - 0.1,
+  }
+
   return (
     <Page>
       <PageHeader page={'dashboard'} />
-      <DashboardView tvl={38545844} />
+      <DashboardView tvl={38545844} mvkStatsBlock={mvkStatsBlock} />
     </Page>
   )
 }
--- src/frontend/src/pages/Dashboard/Dashboard.style.tsx
+++ src/frontend/src/pages/Dashboard/Dashboard.style.tsx
@@ -8,30 +8,86 @@ export const DashboardStyled = styled.div<{ theme: MavrykTheme }>`
     display: flex;
     justify-content: space-between;
     height: 240px;
+    column-gap: 20px;
 
-    .tvlBlock {
+    > div {
       width: 50%;
       height: 100%;
-      padding: 25px 0 0 30px;
-      background: url('images/dashboardTVLbg.png'), ${({ theme }) => theme.dashboardTvlGradient};
-      background-size: cover;
-      background-repeat: no-repeat;
       border: 1px solid #503eaa;
+      padding: 25px 0 0 30px;
       border-radius: 10px;
 
       h1 {
-        color: ${silverColor};
+        color: ${({ theme }) => theme.blockNameTitleColor};
 
         &:after {
-          background-color: ${silverColor};
+          background-color: ${({ theme }) => theme.blockNameTitleColor};
         }
       }
+    }
+
+    .tvlBlock {
+      background: url('images/dashboardTVLbg.png'), ${({ theme }) => theme.dashboardTvlGradient};
+      background-size: cover;
+      background-repeat: no-repeat;
 
       > div {
         font-weight: 600;
         font-size: 32px;
         color: ${({ theme }) => theme.dataColor};
         margin-top: 30px;
+      }
+    }
+
+    .mvkStats {
+      background-color: ${({ theme }) => theme.containerColor};
+
+      .statsWrapper {
+        margin-top: 15px;
+        display: grid;
+        grid-template-columns: repeat(3, auto);
+        column-gap: 30px;
+        row-gap: 20px;
+
+        .stat {
+          display: flex;
+          flex-direction: column;
+          row-gap: 4px;
+
+          .name {
+            font-weight: 600;
+            font-size: 14px;
+            color: ${({ theme }) => theme.blockNameTitleColor};
+          }
+
+          .value {
+            display: flex;
+            color: ${({ theme }) => theme.dataColor};
+            font-weight: 600;
+            font-size: 16px;
+            column-gap: 4px;
+            height: 36px;
+            align-items: center;
+
+            .impact {
+              border-radius: 5px;
+              font-weight: 400;
+              font-size: 12px;
+              padding: 2px 3px;
+              height: fit-content;
+
+              &.up {
+                color: #4bcf83;
+                background: rgba(39, 174, 96, 0.4);
+              }
+
+              &.down {
+                color: '#FF4343';
+                background: rgba(174, 48, 39, 0.4);
+              }
+            }
+          }
+        }
       }
     }
   }
