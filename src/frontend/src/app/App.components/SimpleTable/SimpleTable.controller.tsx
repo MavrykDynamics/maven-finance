f3ac1c127eca3452a0660dbb7f236ac20d4be8f4
Kucher Maxym <max.kucher98@gmail.com>
2022-09-08 12:33:25 +0300

    extraceted table for dashboard, started vaults tab

---
+++ src/frontend/src/app/App.components/SimpleTable/SimpleTable.controller.tsx
@@ -0,0 +1,61 @@
+import { CommaNumber } from '../CommaNumber/CommaNumber.controller'
+import { TzAddress } from '../TzAddress/TzAddress.view'
+import { SimpletableStyled } from './SimpleTable.style'
+
+type TableProps = {
+  className?: string
+  colunmNames: Array<string>
+  data: Array<Record<string, string | number>>
+  fieldsMapper: Array<{
+    fieldName: string
+    needCommaNumber?: boolean
+    needTzAddress?: boolean
+    propsToComponents?: Record<string, unknown>
+  }>
+}
+
+export const SimpleTable = ({ colunmNames, data, fieldsMapper, className = '' }: TableProps) => {
+  return (
+    <SimpletableStyled className={`simple-table ${className}`}>
+      <div className="row column-names">
+        {colunmNames.map((name) => (
+          <div className="row-item">{name}</div>
+        ))}
+      </div>
+
+      <div className="table-content scroll-block">
+        {data.map((item) => {
+          return (
+            <div className="row" key={item.id}>
+              {fieldsMapper.map(({ fieldName, needCommaNumber, needTzAddress, propsToComponents = {} }) => {
+                if (!item?.[fieldName]) return null
+
+                if (needCommaNumber) {
+                  return (
+                    <div className="row-item" key={item[fieldName]}>
+                      <CommaNumber {...propsToComponents} value={Number(item[fieldName])} />
+                    </div>
+                  )
+                }
+
+                if (needTzAddress) {
+                  return (
+                    <div className="row-item" key={item[fieldName]}>
+                      <TzAddress hasIcon {...propsToComponents} tzAddress={String(item[fieldName])} />
+                    </div>
+                  )
+                }
+
+                return (
+                  <div className="row-item" key={item[fieldName]}>
+                    {item[fieldName]}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </SimpletableStyled>
+  )
+}
