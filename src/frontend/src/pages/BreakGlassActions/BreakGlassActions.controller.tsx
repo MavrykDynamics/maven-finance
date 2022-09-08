aee78df460257a3a481dc855a9c68fa3a734b19d
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-09-08 11:50:18 +0300

    added break glass actions page to navigation

---
+++ src/frontend/src/pages/BreakGlassActions/BreakGlassActions.controller.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+
+// view
+import { PageHeader } from '../../app/App.components/PageHeader/PageHeader.controller'
+
+// styles
+import { Page } from 'styles'
+
+
+export const BreakGlassActions: FC = () => {
+  return (
+    <Page>
+      <PageHeader page={'break-glass-actions'} />
+      Working...
+    </Page>
+  )
+}
