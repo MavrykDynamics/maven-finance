14d451282db3bf76c331ff60bba7db64afac4cad
Kucher Maxym <max.kucher98@gmail.com>
2022-09-17 01:34:44 +0300

    created loader for wert io

--- src/frontend/src/reducers/preferences.ts
+++ src/frontend/src/reducers/preferences.ts
@@ -1,8 +1,10 @@
+import { TOGGLE_LOADER } from 'app/App.components/Loader/Loader.action'
 import {
   TOGGLE_RPC_NODE_POPUP,
   SELECT_NEW_RPC_APP_NODE,
   SET_RPC_NODES,
 } from 'app/App.components/SettingsPopup/SettingsPopup.actions'
+import { ROCKET_LOADER, WERT_IO_LOADER } from 'utils/constants'
 import { ThemeType, TOGGLE_DARK_THEME } from '../app/App.components/DarkThemeProvider/DarkThemeProvider.actions'
 import { GET_HEAD_DATA, TOGGLE_SIDEBAR } from '../app/App.components/Menu/Menu.actions'
 import { getItemFromStorage } from '../utils/storage'
@@ -26,6 +28,7 @@ export interface PreferencesState {
   RPC_NODES: Array<RPCNodeType>
   REACT_APP_RPC_PROVIDER: string
   sidebarOpened: boolean
+  loader: null | typeof ROCKET_LOADER | typeof WERT_IO_LOADER
 }
 
 const preferencesDefaultState: PreferencesState = {
@@ -37,12 +40,15 @@ const preferencesDefaultState: PreferencesState = {
     { title: 'ECADLABS', url: 'https://jakartanet.ecadinfra.com', nodeLogoUrl: 'ECAD_logo.png' },
   ],
   REACT_APP_RPC_PROVIDER: 'https://jakartanet.tezos.marigold.dev/',
+  loader: null,
 }
 
 export function preferences(state = preferencesDefaultState, action: Action) {
   switch (action.type) {
     case TOGGLE_DARK_THEME:
       return { ...state, themeSelected: action.newThemeSelected }
+    case TOGGLE_LOADER:
+      return { ...state, loader: action.newLoader }
     case GET_HEAD_DATA:
       return { ...state, headData: action.headData }
     case TOGGLE_RPC_NODE_POPUP:
