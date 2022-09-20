1749849a3de6f98f31ac99e20e9ffd7c0bfc2004
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-09-20 14:53:51 +0300

    added pending signature card

--- src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncil.style.tsx
+++ src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncil.style.tsx
@@ -1,4 +1,26 @@
-import styled from "styled-components/macro";
-import { Card, royalPurpleColor, textsColor } from "styles";
+import styled from "styled-components/macro"
+import { Card, royalPurpleColor, textsColor, headerColor } from "styles"
 
 import { MavrykTheme } from "../../styles/interfaces";
+
+export const BreakGlassCouncilStyled = styled.div<{ theme: MavrykTheme }>`
+  display: flex;
+  justify-content: space-between;
+
+  .left-block {
+    width: 751px;
+  }
+
+  .right-block {
+    width: 309px;
+  }
+
+  .pending-signature {
+    display: flex;
+    justify-content: space-between;
+  }
+`
+
+export const ReviewPastCouncilActionsCard = styled(Card)<{ theme: MavrykTheme }>`
+  
+`
