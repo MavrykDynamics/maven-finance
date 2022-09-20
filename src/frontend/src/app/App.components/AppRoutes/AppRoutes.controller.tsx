b5c636a15b35e212ca13fe1f8106ae4c6fd71ec9
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-09-20 12:01:53 +0300

    added break glass council page

--- src/frontend/src/app/App.components/AppRoutes/AppRoutes.controller.tsx
+++ src/frontend/src/app/App.components/AppRoutes/AppRoutes.controller.tsx
@@ -14,6 +14,7 @@ import { BecomeSatellite } from '../../../pages/BecomeSatellite/BecomeSatellite.
 import { BreakGlass } from '../../../pages/BreakGlass/BreakGlass.controller'
 import { Council } from '../../../pages/Council/Council.controller'
 import { BreakGlassActions } from 'pages/BreakGlassActions/BreakGlassActions.controller'
+import { BreakGlassCouncil } from 'pages/BreakGlassCouncil/BreakGlassCouncil.controller'
 import { Dashboard } from '../../../pages/Dashboard/Dashboard.controller'
 import { Doorman } from '../../../pages/Doorman/Doorman.controller'
 import { EmergencyGovernance } from '../../../pages/EmergencyGovernance/EmergencyGovernance.controller'
@@ -82,6 +83,9 @@ export const AppRoutes = () => {
       </Route>
       <Route exact path="/mavryk-council">
         <Council />
+      </Route>
+      <Route exact path="/break-glass-council">
+        <BreakGlassCouncil />
       </Route>
       <Route exact path="/break-glass-actions">
         <BreakGlassActions />
---
+++ src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncil.style.tsx
@@ -0,0 +1,4 @@
+import styled from "styled-components/macro";
+import { Card, royalPurpleColor, textsColor } from "styles";
+
+import { MavrykTheme } from "../../styles/interfaces";
