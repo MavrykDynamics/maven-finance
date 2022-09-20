cb2e70173c468b4cfedae2308170fdd85df6eb06
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-09-20 21:11:46 +0300

    refactored pending signature cards

--- src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncil.controller.tsx
+++ src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncil.controller.tsx
@@ -3,8 +3,9 @@ import React, { FC } from 'react'
 // components
 import { PageHeader } from '../../app/App.components/PageHeader/PageHeader.controller'
 import { Button } from 'app/App.components/Button/Button.controller'
-import { PendingSignatureCard } from './PendingSignatureCard/PendingSignatureCard.controller'
 import { CouncilPastActionView } from 'pages/Council/CouncilPastAction/CouncilPastAction.view'
+import Carousel from '../../app/App.components/Carousel/Carousel.view'
+import { CouncilPendingView } from '../Council/CouncilPending/CouncilPending.view'
 
 // helpers
 import { ACTION_SECONDARY } from '../../app/App.components/Button/Button.constants'
@@ -15,45 +16,55 @@ import { Page, BreakGlassCouncilStyled, ReviewPastCouncilActionsCard } from './B
 // TODO: change mock to valid data
 const mockCards = [
   {
+    execution_datetime: `${new Date()}`,
     id: 1,
-    title: 'Sign Action',
-    addressName: 'Vestee address',
-    address: 'tz1Vdsjfdjksjfoda;apgjsdiojcjb',
-    signed: '3/8',
-    onClick: () => {},
+    action_type: 'Sign Action',
+    signers_count: 233,
+    initiator_id: '3',
+    num_council_members: 3423,
+    councilPendingActionsLength: 3255,
+    parameters: [],
   },
   {
+    execution_datetime: `${new Date()}`,
     id: 2,
-    title: 'Add Council Member',
-    addressName: 'Vestee address',
-    address: 'tz1Vdsjfdjksjfoda;apgjsdiojcjb',
-    signed: '3/8',
-    onClick: () => {},
+    action_type: 'Add Council Member',
+    signers_count: 233,
+    initiator_id: '3',
+    num_council_members: 3423,
+    councilPendingActionsLength: 3255,
+    parameters: [],
   },
   {
+    execution_datetime: `${new Date()}`,
     id: 3,
-    title: 'Change Council Member',
-    addressName: 'Vestee address',
-    address: 'tz1Vdsjfdjksjfoda;apgjsdiojcjb',
-    signed: '3/8',
-    onClick: () => {},
+    action_type: 'Change Council Member',
+    signers_count: 233,
+    initiator_id: '3',
+    num_council_members: 3423,
+    councilPendingActionsLength: 3255,
+    parameters: [],
   },
-  // {
-  //   id: 4,
-  //   title: 'Update Council Member',
-  //   addressName: 'Vestee address',
-  //   address: 'tz1Vdsjfdjksjfoda;apgjsdiojcjb',
-  //   signed: '3/8',
-  //   onClick: () => {},
-  // },
-  // {
-  //   id: 5,
-  //   title: 'Remove Council Member',
-  //   addressName: 'Vestee address',
-  //   address: 'tz1Vdsjfdjksjfoda;apgjsdiojcjb',
-  //   signed: '3/8',
-  //   onClick: () => {},
-  // },
+  {
+    execution_datetime: `${new Date()}`,
+    id: 4,
+    action_type: 'Update Council Member',
+    signers_count: 233,
+    initiator_id: '3',
+    num_council_members: 3423,
+    councilPendingActionsLength: 3255,
+    parameters: [],
+  },
+  {
+    execution_datetime: `${new Date()}`,
+    id: 5,
+    action_type: 'Remove Council Member',
+    signers_count: 233,
+    initiator_id: 'rlejjtewjiorweiojtiowrjieojrtiow',
+    num_council_members: 3423,
+    councilPendingActionsLength: 3255,
+    parameters: [],
+  },
 ]
 // TODO: change mock to valid data
 const mockHistory = [
@@ -91,11 +102,25 @@ export const BreakGlassCouncil: FC = () => {
 
       <BreakGlassCouncilStyled>
         <div className='left-block'>
-          <div className='pending-signature'>
-            {mockCards.map((item) => (
-              <PendingSignatureCard key={item.id} {...item} />
-            ))}
-          </div>
+          <article className="pending">
+            <div className="pending-items">
+              <Carousel itemLength={mockCards.length} key={1} >
+                {mockCards.map((item) => (
+                  <CouncilPendingView
+                    execution_datetime={item.execution_datetime}
+                    key={item.id}
+                    id={item.id}
+                    action_type={item.action_type}
+                    signers_count={item.signers_count}
+                    initiator_id={item.initiator_id}
+                    num_council_members={item.num_council_members}
+                    councilPendingActionsLength={item.councilPendingActionsLength}
+                    parameters={item.parameters || []}
+                  />
+                ))}
+              </Carousel>
+            </div>
+          </article>
 
           <h1>My Past Council Actions</h1>
 
