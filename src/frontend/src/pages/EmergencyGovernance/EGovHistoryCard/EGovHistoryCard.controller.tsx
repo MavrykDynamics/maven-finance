8dfe8f6644cc09ed56a168a07ecab67e78524a71
Kucher Maxym <max.kucher98@gmail.com>
2022-09-21 18:39:11 +0300

    removed logs

--- src/frontend/src/pages/EmergencyGovernance/EGovHistoryCard/EGovHistoryCard.controller.tsx
+++ src/frontend/src/pages/EmergencyGovernance/EGovHistoryCard/EGovHistoryCard.controller.tsx
@@ -43,8 +43,6 @@ export const EGovHistoryCard = ({ emergencyGovernance }: EGovHistoryCardProps) =
 
   const currentData = emergencyGovernance.startTimestamp
 
-  console.log('emergencyGovernance', emergencyGovernance)
-
   const votingStatistic = useMemo(
     () => ({
       forVotesMVKTotal: emergencyGovernance.totalsMvkVotes,
