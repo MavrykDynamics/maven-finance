e1ae624f7e662b8c23f5d63a4a8e47b29d764bcc
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-09-22 17:28:25 +0300

    added past break glass council action query

--- src/frontend/src/gql/queries/getBreakGlassCouncilStorage.ts
+++ src/frontend/src/gql/queries/getBreakGlassCouncilStorage.ts
@@ -12,3 +12,29 @@ export const BREAK_GLASS_COUNCIL_MEMBER_QUERY = `
     }
   }
 `
+
+export const PAST_BREAK_GLASS_COUNCIL_ACTION_QUERY_NAME = 'GetPastBreakGlassCouncilActions'
+export const PAST_BREAK_GLASS_COUNCIL_ACTION_QUERY_VARIABLE = {}
+
+export const PAST_BREAK_GLASS_COUNCIL_ACTION_QUERY = `
+  query GetPastBreakGlassCouncilActions($_lt: timestamptz = "") {
+    break_glass_action(where: {expiration_datetime: {_lt: $_lt}, _or: {executed: {_eq: true}}}) {
+      action_type
+      break_glass_id
+      executed
+      execution_datetime
+      execution_level
+      expiration_datetime
+      id
+      initiator_id
+      signers_count
+      status
+      start_datetime
+      signers {
+        signer_id
+        id
+        break_glass_action_id
+      }
+    }
+  }
+`
