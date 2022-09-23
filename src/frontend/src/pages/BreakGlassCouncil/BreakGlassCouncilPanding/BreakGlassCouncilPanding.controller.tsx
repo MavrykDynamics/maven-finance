5776351600f93b4a7dec640a7e9b863c93fa5b09
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-09-23 18:09:51 +0300

    fixed pending signature

---
+++ src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncilPanding/BreakGlassCouncilPanding.controller.tsx
@@ -0,0 +1,63 @@
+import { useDispatch } from 'react-redux'
+
+// components
+import { Button } from '../../../app/App.components/Button/Button.controller'
+
+// types
+import { BreakGlassAction } from 'utils/TypesAndInterfaces/BreakGlass'
+
+// helpers
+import { getSeparateCamelCase } from '../../../utils/parse'
+import { getShortTzAddress } from '../../../utils/tzAdress'
+
+// actions
+import { signAction } from '../BreakGlassCouncil.actions'
+
+// style
+import { BreakGlassCouncilPendingStyled } from './BreakGlassCouncilPanding.style'
+
+type Props = BreakGlassAction[0] & {
+  numCouncilMembers: number
+  councilPendingActionsLength: number
+}
+
+export const BreakGlassCouncilPanding = (props: Props) => {
+  const dispatch = useDispatch()
+
+  const {
+    id,
+    actionType,
+    signersCount,
+    initiatorId,
+    numCouncilMembers,
+    councilPendingActionsLength,
+  } = props
+
+  const handleSign = () => {
+    if (id) {
+      dispatch(signAction(id))
+    }
+  }
+
+  return (
+    <BreakGlassCouncilPendingStyled className={`${actionType} ${councilPendingActionsLength > 1 ? 'more' : ''}`}>
+      <h3>{getSeparateCamelCase(actionType)}</h3>
+        <div className='parameters'>
+          <div>
+            <p>Address</p>
+            <span className="parameters-value">
+              {getShortTzAddress(initiatorId)}
+            </span>
+          </div>
+
+          <div>
+            <p>Signed</p>
+            <span className="parameters-value">
+              {signersCount}/{numCouncilMembers}
+            </span>
+          </div>
+        </div>
+      <Button text="Sign" className="sign-btn" kind={'actionPrimary'} icon="sign" onClick={handleSign} />
+    </BreakGlassCouncilPendingStyled>
+  )
+}
