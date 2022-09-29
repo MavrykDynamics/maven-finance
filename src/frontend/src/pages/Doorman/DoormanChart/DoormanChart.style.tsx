808a981a0294e58b5ab7821c0f0e99cb162f0937
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-09-29 19:27:47 +0300

    changed font-size in buttons

--- src/frontend/src/pages/Doorman/DoormanChart/DoormanChart.style.tsx
+++ src/frontend/src/pages/Doorman/DoormanChart/DoormanChart.style.tsx
@@ -3,23 +3,27 @@ import { MavrykTheme } from '../../../styles/interfaces'
 import { Card } from 'styles'
 
 export const ChartCard = styled(Card)<{ theme: MavrykTheme }>`
-padding: 30px 20px 20px;
+  padding: 30px 20px 20px;
 `
 
 export const ChartSlidingTabButtons = styled.div`
-display: flex;
-width: 300px;
-height: 40px;
+  display: flex;
+  width: 300px;
+  height: 40px;
 
-> div {
-  width: 100%;
-  justify-content: flex-end;
-}
+  > div {
+    width: 100%;
+    justify-content: flex-end;
+  }
 
-button {
-  width: 100%;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 14px;
-}
+  div {
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 14px;
+  }
+
+  button {
+    padding: 0 12px;
+    width: 100%;
+  }
 `
