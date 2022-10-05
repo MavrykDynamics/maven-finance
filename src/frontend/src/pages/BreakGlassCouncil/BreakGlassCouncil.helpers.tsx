edb5bb96f9b3cb9d1e06964561425d107633841a
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-10-05 15:13:36 +0300

    added timestamptz to query as variable

--- src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncil.helpers.tsx
+++ src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncil.helpers.tsx
@@ -51,16 +51,18 @@ type BreakGlassCouncilMemberProps = {
 export function normalizeBreakGlassCouncilMember(storage: BreakGlassCouncilMemberProps) {
   const { break_glass_council_member } = storage
 
-  return break_glass_council_member?.map((item) => {
-    return {
-      breakGlassId: item.break_glass_id,
-      breakGlass: item.break_glass,
-      id: item.id,
-      image: item.image,
-      name: item.name,
-      user: item.user,
-      userId: item.user_id,
-      website: item.website,
-    }
-  })
+  return break_glass_council_member?.length
+  ? break_glass_council_member.map((item) => {
+      return {
+        breakGlassId: item.break_glass_id,
+        breakGlass: item.break_glass,
+        id: item.id,
+        image: item.image,
+        name: item.name,
+        user: item.user,
+        userId: item.user_id,
+        website: item.website,
+      }
+    })
+  : []
 }
