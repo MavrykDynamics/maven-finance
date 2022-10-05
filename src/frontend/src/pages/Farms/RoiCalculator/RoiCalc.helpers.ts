c70a7e880c9c875f0e570716f25e85e681395f34
Kucher Maxym <max.kucher98@gmail.com>
2022-10-05 14:27:43 +0300

    added question for sam

--- src/frontend/src/pages/Farms/RoiCalculator/RoiCalc.helpers.ts
+++ src/frontend/src/pages/Farms/RoiCalculator/RoiCalc.helpers.ts
@@ -17,7 +17,7 @@ export const COMPOUNDING_ITEMS = [
   { text: '30D', id: 4, active: false, actualValue: 30 },
 ]
 
-export const LP_EXCHANGE_RATE = 0.5
+export const LP_EXCHANGE_RATE = 0.001
 export const TOP_INPUT = 'amount'
 export const BOTTOM_INPUT = 'backwardAmount'
 
@@ -35,8 +35,30 @@ export const calcRoi = ({
   farm: FarmStorage[number]
 }) => {
   const blocksAmount = 2 * 60 * 24 * stakedDays // blocks amount depends on staked time amount
-  const apy = calculateAPR(farm.currentRewardPerBlock, blocksAmount, farm.lpBalance)
-  return Math.pow((startUSDAmount + 1) / startUSDAmount, 1 / (stakedDays / 365))
+  // const farmDepositedAmount = farm.farmAccounts.reduce(
+  //   (acc, { deposited_amount }) => (acc += deposited_amount * LP_EXCHANGE_RATE),
+  //   0,
+  // ) // deposited LP Tokens amount of farm, converted to USD
+  if (useCompound && compoundFrequency) {
+    // 5 blocks per day  => 5$ => 100$ => 5$ => 100 105$ =. 5.50 => 110.5$ - 100$
+    return Math.pow((startUSDAmount + 1) / startUSDAmount, 1 / (stakedDays / 365))
+  } else {
+    // const persentOfDepositedAmount = startUSDAmount * 100 / (farmDepositedAmount + startUSDAmount) // persent of deposit if invest input amount in farm
+
+    // 1. how user revenue grow due to staked amount revenue for 1$ and revenue for 100$
+    // formula from https://www.youtube.com/watch?v=wPVZBPWYYXY
+
+    const revenue = blocksAmount * farm.currentRewardPerBlock * LP_EXCHANGE_RATE // getting how many revenue for this period
+    const annualizedROI = revenue / startUSDAmount * 100
+    const roiPerStakedPeriod = Math.pow(1 + annualizedROI, stakedDays / 365) - 1
+
+    console.log('usd value to stake: ', startUSDAmount)
+    console.log('annualizedROI: ', `${annualizedROI}%`, 'revenue: ', revenue)
+    console.log('roiPerStakedPeriod: ', `${roiPerStakedPeriod}%`, 'years staked:', stakedDays / 365)
+    console.log('------------------')
+
+    return roiPerStakedPeriod
+  }
 }
 
 export const getOppositeROIvalue = (
