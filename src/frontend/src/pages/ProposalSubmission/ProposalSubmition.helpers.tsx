5f6fba1899148fcff920bfe6221546a79b0ea8a2
Kucher Maxym <max.kucher98@gmail.com>
2022-10-11 16:58:18 +0300

    types

--- src/frontend/src/pages/ProposalSubmission/ProposalSubmition.helpers.tsx
+++ src/frontend/src/pages/ProposalSubmission/ProposalSubmition.helpers.tsx
@@ -1,3 +1,4 @@
+import { InputStatusType, INPUT_STATUS_ERROR, INPUT_STATUS_SUCCESS } from 'app/App.components/Input/Input.constants'
 import { ProposalUpdateFormProposalBytes } from 'utils/TypesAndInterfaces/Forms'
 import { ProposalDataType } from 'utils/TypesAndInterfaces/Governance'
 
@@ -10,12 +11,24 @@ export const getBytesPairValidationStatus = (
   fieldToValidate: 'validTitle' | 'validBytes',
   currentByteId: number,
   proposalData?: Array<ProposalDataType>,
-): 'success' | 'error' => {
+): typeof INPUT_STATUS_SUCCESS | typeof INPUT_STATUS_ERROR => {
   const isExistTitleInServer = proposalData?.some(({ id }) => id === currentByteId)
 
   if (fieldToValidate === 'validTitle') {
-    return Boolean(newText) && !isExistTitleInServer ? 'success' : 'error'
+    return Boolean(newText) && !isExistTitleInServer ? INPUT_STATUS_SUCCESS : INPUT_STATUS_ERROR
   } else {
-    return Boolean(newText) ? 'success' : 'error'
+    return Boolean(newText) ? INPUT_STATUS_SUCCESS : INPUT_STATUS_ERROR
   }
+}
+
+export const PROPOSAL_BYTE = {
+  bytes: '',
+  governance_proposal_record_id: 0,
+  id: 1,
+  record_internal_id: 0,
+  title: '',
+  validTitle: '' as InputStatusType,
+  validBytes: '' as InputStatusType,
+  order: 1,
+  isUnderTheDrop: false,
 }
