729d9eaf8a4c204fccb74fe9e01f885ab4667b57
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-10-19 12:06:31 +0300

    added correct display of dropdown when not full screen

--- src/frontend/src/utils/scrollToBottomOfElement.ts
+++ src/frontend/src/utils/scrollToBottomOfElement.ts
@@ -1,5 +1,7 @@
-export const scrollToBottomOfElement = (element: HTMLElement | null) => {
+type VerticalPositionTypes = 'start' | 'center' | 'end' | 'nearest'
+
+export const scrollToBottomOfElement = (element: HTMLElement | null, block: VerticalPositionTypes = 'start') => {
   if (!element) return
 
-  element.scrollIntoView({ block: "start", inline: "nearest", behavior: "smooth"})
+  element.scrollIntoView({ block, inline: "nearest", behavior: "smooth"})
 }
