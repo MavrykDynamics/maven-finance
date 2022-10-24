ec68fd40f848f8c35ecdd0354e8a72e850b39425
Tristan Allaire <tristan.allaire@alarch.fr>
2022-10-24 13:46:13 +0200

    MetricsServer installed on k8s + Mavryk Prod2 DB installed, configured and used on a new indexer + Ansible script fixed + Mavryk helm chart updated

--- src/infrastructure/modules/droplet/main.tf
+++ src/infrastructure/modules/droplet/main.tf
@@ -5,5 +5,5 @@ resource "digitalocean_droplet" "this" {
   size      = var.size
   vpc_uuid  = var.vpc_id
   tags      = var.tags
-  ssh_keys  = var.import_ssh_keys ? data.digitalocean_ssh_keys.this.ssh_keys[*].id : []
+  ssh_keys  = data.digitalocean_ssh_keys.this.ssh_keys[*].id
 }
