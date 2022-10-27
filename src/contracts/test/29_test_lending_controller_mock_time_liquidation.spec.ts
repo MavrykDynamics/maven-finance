61b82af0a81c66efaa340efb1cb1e91e72044480
Tim <>
2022-10-28 00:39:45 +0800

    1) Refactor Lending Controller Mock Time %liquidateVault entrypoint to be smaller (so it can compile) 2) Bugfix Lending Controller tests 3) mToken basic tests setup

--- src/contracts/test/29_test_lending_controller_mock_time_liquidation.spec.ts
+++ src/contracts/test/29_test_lending_controller_mock_time_liquidation.spec.ts
@@ -1905,7 +1905,7 @@ describe("Lending Controller (Mock Time - Liquidation) tests", async () => {
             vaultRecord                 = await lendingControllerStorage.vaults.get(vaultHandle);
             lastUpdatedBlockLevel       = vaultRecord.lastUpdatedBlockLevel;
 
-            const yearsPassed  = 7; 
+            const yearsPassed  = 8; 
             mockLevelChange = yearsPassed * oneYearLevelBlocks;
             newMockLevel = parseInt(lastUpdatedBlockLevel) + mockLevelChange;
 
