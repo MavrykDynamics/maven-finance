64cafa386676a2a10367b0b14ad40d0b86a7720b
Tristan Allaire <tristan.allaire@alarch.fr>
2022-10-28 09:48:34 +0200

    Lending Controller Mock Time indexed

---
+++ src/indexer/mavryk/handlers/on_lending_controller_mock_time_set_collateral_token.py
@@ -0,0 +1,49 @@
+
+from mavryk.utils.persisters import persist_token_metadata
+from mavryk.types.lending_controller_mock_time.storage import LendingControllerMockTimeStorage
+from dipdup.models import Transaction
+from dipdup.context import HandlerContext
+from mavryk.types.lending_controller_mock_time.parameter.set_collateral_token import SetCollateralTokenParameter, ActionItem as createCollateralToken
+import mavryk.models as models
+
+async def on_lending_controller_mock_time_set_collateral_token(
+    ctx: HandlerContext,
+    set_collateral_token: Transaction[SetCollateralTokenParameter, LendingControllerMockTimeStorage],
+) -> None:
+
+    # Get operation info
+    action_class                    = type(set_collateral_token.parameter.action)
+    if action_class == createCollateralToken:
+        collateral_token_name       = set_collateral_token.parameter.action.createCollateralToken.tokenName
+    else:
+        collateral_token_name       = set_collateral_token.parameter.action.updateCollateralToken.tokenName
+
+    lending_controller_address      = set_collateral_token.data.target_address
+    if collateral_token_name in set_collateral_token.storage.collateralTokenLedger:
+        collateral_token_storage        = set_collateral_token.storage.collateralTokenLedger[collateral_token_name]
+        collateral_token_oracle_address = collateral_token_storage.oracleAddress
+        collateral_token_address        = collateral_token_storage.tokenContractAddress
+        collateral_token_id             = 0
+
+        # Persist collateral Token Metadata
+        await persist_token_metadata(
+            ctx=ctx,
+            token_address=collateral_token_address,
+            token_id=str(collateral_token_id)
+        )
+
+        # Create / Update record
+        lending_controller          = await models.LendingController.get(
+            address         = lending_controller_address,
+            mock_time       = True
+        )
+        oracle, _                   = await models.MavrykUser.get_or_create(
+            address = collateral_token_oracle_address
+        )
+        await oracle.save()
+        lending_controller_collateral_token, _  = await models.LendingControllerCollateralToken.get_or_create(
+            lending_controller  = lending_controller,
+            token_address       = collateral_token_address,
+            oracle              = oracle
+        )
+        await lending_controller_collateral_token.save()
