b99295d49f9eaf050677b0ec324c544a1e8a0829
Evgeniy Polishchuk <evhenii.polishchuk@gmail.com>
2022-11-10 15:27:20 +0200

    refactored right card on contract status page

--- src/frontend/src/pages/BreakGlass/ContractCard/ContractCard.controller.tsx
+++ src/frontend/src/pages/BreakGlass/ContractCard/ContractCard.controller.tsx
@@ -16,7 +16,6 @@ export const ContractCard = ({ contract, isActive, onClick, isExpanded, handleEx
   const title = (contract.title as string).replace(/([a-z0-9])([A-Z])/g, '$1 $2')
   const address = contract.address as string
   const lastUpdated = (contract.lastUpdated || '') as string
-  console.log("🚀 ~ file: ContractCard.controller.tsx ~ line 19 ~ ContractCard ~ lastUpdated", lastUpdated)
   const admin = contract.admin as string
   const methods = contract.methods as Record<string, boolean>
   const isStatusPaused = methods ? Object.keys(methods).some((method) => methods[method]) : false
