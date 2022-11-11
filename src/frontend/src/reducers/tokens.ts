45e9fdcfdad44b15a55fe019333e4325353e505d
Kucher Maxym <max.kucher98@gmail.com>
2022-11-11 21:36:12 +0200

    added fetching for whitelist tokens

--- src/frontend/src/reducers/tokens.ts
+++ src/frontend/src/reducers/tokens.ts
@@ -1,17 +1,27 @@
+import { Governance_Financial_Whitelist_Token_Contract } from 'utils/generated/graphqlTypes'
 import { DipDupTokensGraphQl } from 'utils/TypesAndInterfaces/DipDupTokens'
 import type { Action } from '../utils/TypesAndInterfaces/ReduxTypes'
-import { GET_DIP_DUP_TOKENS } from './actions/dipDupActions.actions'
+import { GET_DIP_DUP_TOKENS, GET_WHITELIST_TOKENS } from './actions/dipDupActions.actions'
 
 export interface ContractAddressesState {
   [key: string]: { address: string }
 }
-export type TokensType = { dipDupTokens: Array<DipDupTokensGraphQl> }
-const dipDupTokensDefaultState: Array<DipDupTokensGraphQl> = []
+export type TokensType = {
+  dipDupTokens: Array<DipDupTokensGraphQl>
+  whitelistTokens: Array<Governance_Financial_Whitelist_Token_Contract>
+}
 
-export function tokens(state = dipDupTokensDefaultState, action: Action) {
+const defaultTokensInfoState: TokensType = {
+  dipDupTokens: [],
+  whitelistTokens: [],
+}
+
+export function tokens(state = defaultTokensInfoState, action: Action) {
   switch (action.type) {
     case GET_DIP_DUP_TOKENS:
       return { ...state, dipDupTokens: action.dipDupTokens }
+    case GET_WHITELIST_TOKENS:
+      return { ...state, whitelistTokens: action.whitelistTokens }
     default:
       return state
   }
