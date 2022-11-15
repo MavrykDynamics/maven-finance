d8d79914433d93edb303a6352bbefc551f8ab675
Tristan Allaire <tristan.allaire@alarch.fr>
2022-11-15 18:01:34 +0100

    Network basenet cluster created with three nodes on mavryk.network + tzkt api on mavryk.io + external-dns/nginx/sealedSecrets installed

---
+++ src/infrastructure/helm-charts/tezos-chain/scripts/upgrade-storage.sh
@@ -0,0 +1,8 @@
+set -ex
+
+if [ ! -e /var/tezos/node/data/context ]
+then
+  printf "No context in data dir found, probably initial start, doing nothing."
+  exit 0
+fi
+tezos-node upgrade storage --data-dir /var/tezos/node/data
