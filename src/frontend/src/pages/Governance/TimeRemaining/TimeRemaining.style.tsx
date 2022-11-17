fce31685b81acca9c8b44b112301cd65adeda164
Kucher Maxym <max.kucher98@gmail.com>
2022-11-17 17:40:02 +0200

    fixed timer for proposal top bar

--- src/frontend/src/pages/Governance/TimeRemaining/TimeRemaining.style.tsx
+++ src/frontend/src/pages/Governance/TimeRemaining/TimeRemaining.style.tsx
@@ -1,21 +1,17 @@
-import styled from 'styled-components/macro'
+import styled, { css } from 'styled-components/macro'
 import { Card, cyanColor, headerColor, skyColor, darkPurpleColor } from 'styles'
 
-export const TimeLeftAreaWrap = styled.div`
-  border-left: 2px solid ${darkPurpleColor};
-  display: flex;
-  height: 38px;
-  flex-shrink: 0;
-  align-items: center;
-  min-width: 189px;
-  text-align: right;
-  justify-content: flex-end;
-`
+export const TimeLeftAreaWrap = styled.div<{ showBorder: boolean }>`
+  ${({ showBorder }) =>
+    showBorder
+      ? css`
+          border-left: 2px solid ${darkPurpleColor};
+        `
+      : ''}
+  padding-left: 15px;
+  margin-left: auto;
 
-export const TimeLeftArea = styled.div`
-  color: ${skyColor};
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 16px;
-  margin-left: 32px;
+  > div {
+    font-size: 18px;
+  }
 `
