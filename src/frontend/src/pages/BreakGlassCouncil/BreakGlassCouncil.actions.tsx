33f4dcc04a227a9c6d3071a424b396fa3644fc20
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2022-11-21 18:22:19 +0200

    added drop button action to break glass council

--- src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncil.actions.tsx
+++ src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncil.actions.tsx
@@ -507,3 +507,48 @@ export const propagateBreakGlass = () => async (dispatch: AppDispatch, getState:
     })
   }
 }
+
+// Drop Action
+export const DROP_BREAK_GLASS_REQUEST = 'DROP_BREAK_GLASS_REQUEST'
+export const DROP_BREAK_GLASS_RESULT = 'DROP_BREAK_GLASS_RESULT'
+export const DROP_BREAK_GLASS_ERROR = 'DROP_BREAK_GLASS_ERROR'
+export const dropBreakGlass = (breakGlassActionID: number) => async (dispatch: AppDispatch, getState: GetState) => {
+  const state: State = getState()
+
+  if (!state.wallet.ready) {
+    dispatch(showToaster(ERROR, 'Please connect your wallet', 'Click Connect in the left menu'))
+    return
+  }
+
+  if (state.loading) {
+    dispatch(showToaster(ERROR, 'Cannot send transaction', 'Previous transaction still pending...'))
+    return
+  }
+
+  try {
+    dispatch({
+      type: DROP_BREAK_GLASS_REQUEST,
+    })
+    const contract = await state.wallet.tezos?.wallet.at(state.contractAddresses.breakGlassAddress.address)
+    console.log('contract', contract)
+    const transaction = await contract?.methods.flushAction(breakGlassActionID).send()
+    console.log('transaction', transaction)
+
+    dispatch(showToaster(INFO, 'Propagate Break Glass...', 'Please wait 30s'))
+
+    const done = await transaction?.confirmation()
+    console.log('done', done)
+    dispatch(showToaster(SUCCESS, 'Propagate Break Glass done', 'All good :)'))
+    dispatch(toggleLoader())
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('dropBreakGlass - ERROR ', error)
+      dispatch(showToaster(ERROR, 'Error', error.message))
+    }
+    dispatch({
+      type: PROPOGATE_BREAK_GLASS_ERROR,
+      error,
+    })
+  }
+}
+
