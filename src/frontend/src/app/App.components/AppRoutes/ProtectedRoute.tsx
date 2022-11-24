ba054daf1e9bb64a64ccb9926cce41c62e66b57b
Kucher Maxym <max.kucher98@gmail.com>
2022-11-24 11:27:37 +0200

    set upped routing for sashboard personal

--- src/frontend/src/app/App.components/AppRoutes/ProtectedRoute.tsx
+++ src/frontend/src/app/App.components/AppRoutes/ProtectedRoute.tsx
@@ -1,47 +1,15 @@
-import * as React from 'react'
-import { Redirect, Route, RouteProps, useLocation } from 'react-router'
-import { SatelliteRecord } from '../../../utils/TypesAndInterfaces/Delegation'
+import { Redirect, Route, RouteProps } from 'react-router'
 
 export type ProtectedRouteProps = {
-  authenticationPath: string
   redirectPath: string
-  arrayToFilterThrough: SatelliteRecord[] | undefined
-  accountPkh: string | undefined
+  isAuthorized: boolean
+  hasAccess: boolean
 } & RouteProps
 
-export default function ProtectedRoute({
-  authenticationPath,
-  redirectPath,
-  accountPkh,
-  arrayToFilterThrough,
-  ...routeProps
-}: ProtectedRouteProps) {
-  const currentLocation = useLocation()
-  const isAuthorized = isAllowedToRoute(accountPkh, redirectPath, arrayToFilterThrough)
-
-  if (isAuthorized && redirectPath === currentLocation.pathname) {
+export default function ProtectedRoute({ redirectPath, hasAccess, isAuthorized, ...routeProps }: ProtectedRouteProps) {
+  if (hasAccess) {
     return <Route {...routeProps} />
   } else {
-    return <Redirect to={{ pathname: isAuthorized ? redirectPath : authenticationPath }} />
+    return <Redirect to={{ pathname: redirectPath }} />
   }
-}
-
-const isAllowedToRoute = (
-  accountPkh: string | undefined,
-  path: string,
-  arrayToFilterThrough: SatelliteRecord[] | undefined,
-) => {
-  let isAllowed
-  switch (path) {
-    case '/submit-proposal':
-      const accountPkhIsSatellite = arrayToFilterThrough?.filter(
-        (satellite: SatelliteRecord) => satellite.address === accountPkh,
-      )[0]
-      isAllowed = accountPkhIsSatellite
-      break
-    default:
-      isAllowed = false
-  }
-
-  return isAllowed
 }
---
+++ src/frontend/src/pages/DashboardPersonal/DashboardPersonal.style.tsx
@@ -0,0 +1,3 @@
+import styled from 'styled-components'
+
+export const DashboardPersonalStyled = styled.div``
