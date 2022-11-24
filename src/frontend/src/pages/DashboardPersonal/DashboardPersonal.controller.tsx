4267bce1863e20b855dc6d687faf04e4ae8efd0b
Kucher Maxym <max.kucher98@gmail.com>
2022-11-24 15:18:54 +0200

    finidhed markup for top of the page, provided static data for it

--- src/frontend/src/pages/DashboardPersonal/DashboardPersonal.controller.tsx
+++ src/frontend/src/pages/DashboardPersonal/DashboardPersonal.controller.tsx
@@ -1,5 +1,5 @@
 import { PageHeader } from 'app/App.components/PageHeader/PageHeader.controller'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 import { State } from 'reducers'
@@ -8,6 +8,8 @@ import { isValidId, PORTFOLIO_TAB_ID } from './DashboardPersonal.utils'
 import DashboardPersonalView from './DashboardPersonal.view'
 
 const DashboardPersonal = () => {
+  const { tabId } = useParams<{ tabId: string }>()
+
   const {
     myDoormanRewardsData,
     myFarmRewardsData,
@@ -17,13 +19,33 @@ const DashboardPersonal = () => {
     myXTZTokenBalance,
     isSatellite,
   } = useSelector((state: State) => state.user)
+  const {
+    tokensPrices: { tezos },
+  } = useSelector((state: State) => state.tokens)
+  const { exchangeRate: mvkRate } = useSelector((state: State) => state.mvkToken)
 
-  const { tabId } = useParams<{ tabId: string }>()
+  const claimRewards = useCallback(() => {
+    console.log('claim rewards in DashboardPersonal')
+  }, [])
 
   return (
     <Page>
       <PageHeader page={'dashboard'} />
-      <DashboardPersonalView isUserSatellite={isSatellite} activeTab={isValidId(tabId) ? tabId : PORTFOLIO_TAB_ID} />
+      <DashboardPersonalView
+        isUserSatellite={isSatellite}
+        activeTab={isValidId(tabId) ? tabId : PORTFOLIO_TAB_ID}
+        claimRewardsHandler={claimRewards}
+        earnings={{
+          mvkRate,
+          xtzRate: tezos?.usd ?? 1,
+          satelliteRewards: 1231,
+          governanceRewards: 313,
+          farmsRewards: 3131.31,
+          exitRewards: 131,
+          maxSupply: 31.13123,
+          lendingIncome: 3131.31,
+        }}
+      />
     </Page>
   )
 }
--- src/frontend/src/pages/DashboardPersonal/DashboardPersonal.view.tsx
+++ src/frontend/src/pages/DashboardPersonal/DashboardPersonal.view.tsx
@@ -2,20 +2,33 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { DashboardPersonalStyled } from './DashboardPersonal.style'
 import { DELEGATION_TAB_ID, PORTFOLIO_TAB_ID, SATELLITE_TAB_ID, TabId } from './DashboardPersonal.utils'
-import DashboardPersonalEarningsHistory from './DashboardPersonalComponents/DashboardPersonalEarningsHistory'
+import DashboardPersonalEarningsHistory, {
+  DashboardPersonalEarningsHistoryProps,
+} from './DashboardPersonalComponents/DashboardPersonalEarningsHistory'
 import DashboardPersonalMyRewards from './DashboardPersonalComponents/DashboardPersonalMyRewards'
 
 type DashboardPersonalProps = {
   activeTab: TabId
   isUserSatellite: boolean
+  claimRewardsHandler: () => void
+  earnings: DashboardPersonalEarningsHistoryProps
 }
 
-const DashboardPersonalView = ({ activeTab, isUserSatellite }: DashboardPersonalProps) => {
+const DashboardPersonalView = ({
+  activeTab,
+  isUserSatellite,
+  claimRewardsHandler,
+  earnings,
+}: DashboardPersonalProps) => {
   return (
     <DashboardPersonalStyled>
       <div className="top">
-        <DashboardPersonalMyRewards />
-        <DashboardPersonalEarningsHistory />
+        <DashboardPersonalMyRewards
+          earnedRewards={121212.12}
+          rewardsToClaim={121212.1212}
+          claimRewardsHandler={claimRewardsHandler}
+        />
+        <DashboardPersonalEarningsHistory {...earnings} />
       </div>
       <div className="tabs-switchers">
         <Link
