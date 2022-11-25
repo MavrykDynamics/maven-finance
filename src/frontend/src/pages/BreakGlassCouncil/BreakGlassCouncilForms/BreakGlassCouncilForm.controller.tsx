a921b9ba6fc9816745f9bce6b717133e443edca8
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2022-11-25 13:34:23 +0200

    removed update action from dropdown

--- src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncilForms/BreakGlassCouncilForm.controller.tsx
+++ src/frontend/src/pages/BreakGlassCouncil/BreakGlassCouncilForms/BreakGlassCouncilForm.controller.tsx
@@ -3,7 +3,6 @@ import { FormSetAllContractsAdminView } from './FormSetAllContractsAdmin.view'
 import { FormSetSingleContractAdminView } from './FormSetSingleContractAdmin.view'
 import { FormSignActionView } from './FormSignAction.view'
 import { FormAddCouncilMemberView } from './FormAddCouncilMember.view'
-import { FormUpdateCouncilMemberView } from './FormUpdateCouncilMember.view'
 import { FormChangeCouncilMemberView } from './FormChangeCouncilMember.view'
 import { FormRemoveCouncilMemberView } from './FormRemoveCouncilMember.view'
 
@@ -20,7 +19,6 @@ export const actions = {
   SET_SINGLE_CONTRACT_ADMIN: 'SET_SINGLE_CONTRACT_ADMIN',
   SIGN_ACTION: 'SIGN_ACTION',
   ADD_COUNCIL_MEMBER: 'ADD_COUNCIL_MEMBER',
-  UPDATE_COUNCIL_MEMBER: 'UPDATE_COUNCIL_MEMBER',
   CHANGE_COUNCIL_MEMBER: 'CHANGE_COUNCIL_MEMBER',
   REMOVE_COUNCIL_MEMBER: 'REMOVE_COUNCIL_MEMBER',
 }
@@ -32,7 +30,6 @@ export function BreakGlassCouncilForm ({ action, councilMemberMaxLength }: Props
       {actions.SET_SINGLE_CONTRACT_ADMIN === action ? <FormSetSingleContractAdminView /> : null}
       {actions.SIGN_ACTION === action ? <FormSignActionView /> : null}
       {actions.ADD_COUNCIL_MEMBER === action ? <FormAddCouncilMemberView councilMemberMaxLength={councilMemberMaxLength} /> : null}
-      {actions.UPDATE_COUNCIL_MEMBER === action ? <FormUpdateCouncilMemberView councilMemberMaxLength={councilMemberMaxLength} /> : null}
       {actions.CHANGE_COUNCIL_MEMBER === action ? <FormChangeCouncilMemberView councilMemberMaxLength={councilMemberMaxLength} /> : null}
       {actions.REMOVE_COUNCIL_MEMBER === action ? <FormRemoveCouncilMemberView /> : null}{' '}
     </>
