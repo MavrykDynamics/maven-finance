1135315014722fcd44b32489fb776c28c152cd38
Kucher Maxym <max.kucher98@gmail.com>
2022-11-28 16:18:30 +0200

    added initial page fetchif for dashboard personal

--- src/frontend/src/pages/DashboardPersonal/DashboardPersonal.controller.tsx
+++ src/frontend/src/pages/DashboardPersonal/DashboardPersonal.controller.tsx
@@ -1,12 +1,18 @@
-import { PageHeader } from 'app/App.components/PageHeader/PageHeader.controller'
-import React, { useCallback } from 'react'
-import { useSelector } from 'react-redux'
 import { useParams } from 'react-router'
-import { State } from 'reducers'
-import { Page } from 'styles/components'
+import { useCallback, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
 import { calcWithoutPrecision } from 'utils/calcFunctions'
+import { getGovernanceStorage } from 'pages/Governance/Governance.actions'
+import { getDelegationStorage, getOracleStorage } from 'pages/Satellites/Satellites.actions'
+import { getEmergencyGovernanceStorage } from 'pages/EmergencyGovernance/EmergencyGovernance.actions'
 import { isValidId, PORTFOLIO_TAB_ID } from './DashboardPersonal.utils'
+
+import { PageHeader } from 'app/App.components/PageHeader/PageHeader.controller'
+import { Page } from 'styles/components'
 import DashboardPersonalView from './DashboardPersonal.view'
+
+import { State } from 'reducers'
 
 const DashboardPersonal = () => {
   const { tabId } = useParams<{ tabId: string }>()
@@ -30,6 +36,13 @@ const DashboardPersonal = () => {
 
   const claimRewards = useCallback(() => {
     console.log('claim rewards in DashboardPersonal')
+  }, [])
+
+  useEffect(() => {
+    getGovernanceStorage()
+    getOracleStorage()
+    getDelegationStorage()
+    getEmergencyGovernanceStorage()
   }, [])
 
   return (
