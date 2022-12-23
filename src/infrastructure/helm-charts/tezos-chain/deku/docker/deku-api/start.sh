0dddb366d65c63b098854adf1944089d84d71f9d
Tristan Allaire <tristan.allaire@alarch.fr>
2022-12-23 10:07:31 +0100

    Deku images updated + Tezos address updated

--- src/infrastructure/helm-charts/tezos-chain/deku/docker/deku-api/start.sh
+++ src/infrastructure/helm-charts/tezos-chain/deku/docker/deku-api/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 nginx &
-/nix/store/pm3xyqyp4ml15vidrm8b4nl2cm1ghf1i-ocaml5.0.0+alpha1-deku-0.0.0-dev/bin/deku-api
+/nix/store/hkbvm7iig39h2qj01s4ll102kcqd3scw-ocaml5.0.0+alpha1-deku-0.0.0-dev/bin/deku-api
