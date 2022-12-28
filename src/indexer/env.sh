9db44a50faf84263187cdc289e0c18b43769e508
Sam Morrow <smorrow32@gmail.com>
2022-12-28 11:53:56 +0200

    Adding rewards_earned to m_token_account

--- src/indexer/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# TZKT URL
-export TZKT_GHOSTNET_URL=https://api.ghostnet.tzkt.io
-export TZKT_MAINNET_URL=https://api.tzkt.io
-export POSTGRES_PASSWORD=dipdup12345
-export ADMIN_SECRET=hasura12345
