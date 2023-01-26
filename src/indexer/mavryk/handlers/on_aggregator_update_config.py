40468dc372b14651cd34f2a43ac2be9737251f44
Tristan Allaire <tristan.allaire@alarch.fr>
2023-01-26 20:17:58 +0900

    upda teConfig for aggregators fixed

--- src/indexer/mavryk/handlers/on_aggregator_update_config.py
+++ src/indexer/mavryk/handlers/on_aggregator_update_config.py
@@ -1,5 +1,5 @@
 
-from mavryk.types.aggregator.parameter.update_config import UpdateConfigParameter, UpdateConfigActionItem as configDecimals, UpdateConfigActionItem1 as configDevTriggerBanDuration, UpdateConfigActionItem2 as configHeartBeatSeconds, UpdateConfigActionItem3 as configPercentOracleThreshold, UpdateConfigActionItem4 as configRewardAmountStakedMvk, UpdateConfigActionItem5 as configRewardAmountXtz
+from mavryk.types.aggregator.parameter.update_config import UpdateConfigParameter, UpdateConfigActionItem as configAlphaPercentPerThousand, UpdateConfigActionItem1 as configDecimals, UpdateConfigActionItem2 as configHeartBeatSeconds, UpdateConfigActionItem3 as configPercentOracleThreshold, UpdateConfigActionItem4 as configRewardAmountStakedMvk, UpdateConfigActionItem5 as configRewardAmountXtz
 from mavryk.types.aggregator.storage import AggregatorStorage
 from dipdup.models import Transaction
 from dipdup.context import HandlerContext
@@ -23,12 +23,12 @@ async def on_aggregator_update_config(
     aggregator.last_updated_at  = timestamp
     if updated_config_action == configDecimals:
         aggregator.decimals                             = updated_value
-    elif updated_config_action == configDevTriggerBanDuration:
-        aggregator.deviation_trigger_ban_duration       = updated_value
+    elif updated_config_action == configAlphaPercentPerThousand:
+        aggregator.alpha_pct_per_thousand               = updated_value
     elif updated_config_action == configHeartBeatSeconds:
         aggregator.heart_beat_seconds                   = updated_value
     elif updated_config_action == configPercentOracleThreshold:
-        aggregator.percent_oracle_threshold             = updated_value
+        aggregator.pct_oracle_threshold                 = updated_value
     elif updated_config_action == configRewardAmountStakedMvk:
         aggregator.reward_amount_smvk                   = updated_value
     elif updated_config_action == configRewardAmountXtz:
