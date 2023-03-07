6c76c8af2e1169b55044a854638f49d1017c3e56
Tim <>
2023-03-08 00:04:17 +0800

    1) Vault contract fixes to updateDepositors and delegate tez / mvk 2) Fix all lending controller, mToken, and vault tests

--- src/contracts/test/types/vaultStorageType.ts
+++ src/contracts/test/types/vaultStorageType.ts
@@ -2,9 +2,11 @@ import { MichelsonMap, MichelsonMapKey } from '@taquito/michelson-encoder'
 import { BigNumber } from 'bignumber.js'
 import { alice } from '../../scripts/sandbox/accounts'
 
+export type depositorsType = string | { whitelist : Array<string>};
+
 export type vaultStorageType = {
     admin                       : string;
     handle                      : {};
     name                        : string;
-    depositors                  : MichelsonMap<MichelsonMapKey, unknown>;
+    depositors                  : depositorsType;
 }
