9951b2e738f5addc9c959abcee2027155b26c833
Tim <>
2023-03-20 15:28:39 +0800

    Fix Doorman exit fee calculation

--- src/contracts/test/02_test_doorman.spec.ts
+++ src/contracts/test/02_test_doorman.spec.ts
@@ -232,7 +232,7 @@
 
 //                 // Test values
 //                 const mli = Math.trunc((doormanSMVKTotalSupply * 100 * 10**36) / mvkTotalSupply);
-//                 const exitFee = (((300_000 * 10**36) - (5_250 * mli)) * 10**36 + 25*mli*mli) / (10_000 * 10**36)
+//                 const exitFee = (((300_000 * 10**36) - (5_250 * mli)) * 10**36 + 25 * mli * mli) / (10_000 * 10**36)
 //                 const paidFee = Math.trunc(userUnstake * (exitFee/100));
 //                 const expectedFinalAmount = userUnstake - (paidFee/10**36);
 
