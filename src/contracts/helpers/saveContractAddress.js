8dbe950c8de91edd961b5d0f09aa9ec130b97eae
Tim <>
2023-03-24 17:20:33 +0800

    1) Deployment contract test helpers refactor to general contract helper 2) Format to 4 spaces instead of 2 spaces

--- src/contracts/helpers/saveContractAddress.js
+++ src/contracts/helpers/saveContractAddress.js
@@ -1,12 +1,16 @@
 const { outputFile } = require('fs-extra');
 
 module.exports = async (name, address) => {
-    // await outputFile(
-    //     `${process.cwd()}/deployments/${name}.js`,
-    //     `module.exports = "${address}";`
-    // );
+    
+    // console log contract name in Title Case
+    const rawName = name.substring(0, name.length - 7);
+    const addSpaces = rawName.replace(/([A-Z])/g, " $1");
+    const contractName = addSpaces.charAt(0).toUpperCase() + addSpaces.slice(1);
+    console.log(`${contractName} contract deployed at: ${address}`)
+
     await outputFile(
         `${process.cwd()}/deployments/${name}.json`,
         `{ "address" : "${address}"}`
     );
+    
 };
