0708fb06db3c58efbc8408d255e7a90c150b0ca6
Tim <>
2023-04-04 17:31:00 +0800

    1) Refactor and clean delegation contract tests 2) Basic refactoring of council, farm, farmMToken, farmFactory, mFarmFactory, treasury, and treasuryFactory tests

--- src/contracts/test/05_test_council.spec.ts
+++ src/contracts/test/05_test_council.spec.ts
@@ -23,7 +23,7 @@ import * as helperFunctions from './helpers/helperFunctions'
 // Contract Tests
 // ------------------------------------------------------------------------------
 
-describe("Council tests", async () => {
+describe("Test: Council Contract", async () => {
     
     var utils: Utils;
     let tezos
@@ -60,6 +60,24 @@ describe("Council tests", async () => {
 
     // operations
     let transferOperation
+
+    // housekeeping operations
+    let setAdminOperation
+    let setGovernanceOperation
+    let resetAdminOperation
+    let updateWhitelistContractsOperation
+    let updateGeneralContractsOperation
+    let togglePauseOperation
+    let pauseOperation
+    let pauseAllOperation
+    let unpauseOperation
+    let unpauseAllOperation
+
+    // contract map value
+    let storageMap
+    let contractMapKey
+    let initialContractMapValue
+    let updatedContractMapValue
     
     before("setup", async () => {
 
@@ -3231,7 +3249,7 @@ describe("Council tests", async () => {
                 assert.equal(dataMap.get("tokenAmount"), packedTokenAmount);
 
                 // Operation
-                await signerFactory(trudy.sk);
+                await helperFunctions.signerFactory(tezos, trudy.sk);
                 await chai.expect(councilInstance.methods.signAction(mintActionID).send()).to.be.rejected;
             } catch(e){
                 console.dir(e, {depth: 5});
@@ -3283,4 +3301,170 @@ describe("Council tests", async () => {
             }
         });
     })
+
+
+    describe('Access Control Checks', function () {
+
+        beforeEach("Set signer to non-admin (mallory)", async () => {
+            delegationStorage = await delegationInstance.storage();
+            await helperFunctions.signerFactory(tezos, mallory.sk);
+        });
+
+        it('%setAdmin                 - non-admin (mallory) should not be able to call this entrypoint', async () => {
+            try{
+                // Initial Values
+                delegationStorage        = await delegationInstance.storage();
+                const currentAdmin  = doormanStorage.admin;
+
+                // Operation
+                setAdminOperation = await delegationInstance.methods.setAdmin(mallory.pkh);
+                await chai.expect(setAdminOperation.send()).to.be.rejected;
+
+                // Final values
+                delegationStorage    = await delegationInstance.storage();
+                const newAdmin  = delegationStorage.admin;
+
+                // Assertions
+                assert.strictEqual(newAdmin, currentAdmin);
+
+            } catch(e){
+                console.log(e);
+            }
+        });
+
+        it('%setGovernance            - non-admin (mallory) should not be able to call this entrypoint', async () => {
+            try{
+                // Initial Values
+                delegationStorage        = await delegationInstance.storage();
+                const currentGovernance  = delegationStorage.governanceAddress;
+
+                // Operation
+                setGovernanceOperation = await delegationInstance.methods.setGovernance(mallory.pkh);
+                await chai.expect(setGovernanceOperation.send()).to.be.rejected;
+
+                // Final values
+                delegationStorage        = await delegationInstance.storage();
+                const updatedGovernance  = delegationStorage.governanceAddress;
+
+                // Assertions
+                assert.strictEqual(updatedGovernance, currentGovernance);
+
+            } catch(e){
+                console.log(e);
+            }
+        });
+
+        it('%updateMetadata           - non-admin (mallory) should not be able to update the contract metadata', async () => {
+            try{
+                // Initial values
+                const key   = ''
+                const hash  = Buffer.from('tezos-storage:data fail', 'ascii').toString('hex')
+
+                delegationStorage       = await delegationInstance.storage();   
+                const initialMetadata   = await delegationStorage.metadata.get(key);
+
+                // Operation
+                const updateOperation = await delegationInstance.methods.updateMetadata(key, hash);
+                await chai.expect(updateOperation.send()).to.be.rejected;
+
+                // Final values
+                delegationStorage       = await delegationInstance.storage();            
+                const updatedData       = await delegationStorage.metadata.get(key);
+
+                // check that there is no change in metadata
+                assert.equal(updatedData, initialMetadata);
+                assert.notEqual(updatedData, hash);
+
+            } catch(e){
+                console.dir(e, {depth: 5});
+            } 
+        });
+
+        it('%updateConfig             - non-admin (mallory) should not be able to update contract config', async () => {
+            try{
+                
+                // Initial Values
+                delegationStorage        = await delegationInstance.storage();
+                const initialConfigValue = delegationStorage.config.minMvkAmount;
+                const newMinMvkAmount = MVK(10);
+
+                // Operation
+                const updateConfigOperation = await delegationInstance.methods.updateConfig(newMinMvkAmount, "configMinMvkAmount");
+                await chai.expect(updateConfigOperation.send()).to.be.rejected;
+
+                // Final values
+                delegationStorage        = await delegationInstance.storage();
+                const updatedConfigValue = delegationStorage.config.minMvkAmount;
+
+                // check that there is no change in config values
+                assert.equal(updatedConfigValue.toNumber(), initialConfigValue.toNumber());
+                assert.notEqual(updatedConfigValue.toNumber(), newMinMvkAmount);
+                
+            } catch(e){
+                console.dir(e, {depth: 5});
+            }
+        });
+
+        it('%updateWhitelistContracts - non-admin (mallory) should not be able to call this entrypoint', async () => {
+            try {
+
+                // init values
+                contractMapKey  = "mallory";
+                storageMap      = "whitelistContracts";
+
+                initialContractMapValue = await helperFunctions.getStorageMapValue(delegationStorage, storageMap, contractMapKey);
+
+                updateWhitelistContractsOperation = await delegationInstance.methods.updateWhitelistContracts(contractMapKey, alice.pkh)
+                await chai.expect(updateWhitelistContractsOperation.send()).to.be.rejected;
+
+                delegationStorage       = await delegationInstance.storage()
+                updatedContractMapValue = await helperFunctions.getStorageMapValue(delegationStorage, storageMap, contractMapKey);
+
+                assert.strictEqual(initialContractMapValue, undefined, 'mallory (key) should not be in the Whitelist Contracts map');
+
+            } catch (e) {
+                console.log(e)
+            }
+        })
+
+        it('%updateGeneralContracts   - non-admin (mallory) should not be able to call this entrypoint', async () => {
+            try {
+
+                // init values
+                contractMapKey  = "mallory";
+                storageMap      = "generalContracts";
+
+                initialContractMapValue = await helperFunctions.getStorageMapValue(delegationStorage, storageMap, contractMapKey);
+
+                updateGeneralContractsOperation = await delegationInstance.methods.updateGeneralContracts(contractMapKey, alice.pkh)
+                await chai.expect(updateGeneralContractsOperation.send()).to.be.rejected;
+
+                delegationStorage       = await delegationInstance.storage()
+                updatedContractMapValue = await helperFunctions.getStorageMapValue(delegationStorage, storageMap, contractMapKey);
+
+                assert.strictEqual(initialContractMapValue, undefined, 'mallory (key) should not be in the General Contracts map');
+
+            } catch (e) {
+                console.log(e)
+            }
+        })
+
+        it("%setLambda                - non-admin (mallory) should not be able to call this entrypoint", async() => {
+            try{
+
+                // random lambda for testing
+                const randomLambdaName  = "delegateToSatellite";
+                const randomLambdaBytes = "050200000cba0743096500000112075e09650000005a036e036e07610368036907650362036c036e036e07600368036e07600368036e09650000000e0359035903590359035903590359000000000761036e09650000000a0362036203620362036200000000036203620760036803690000000009650000000a0362036203620362036e00000000075e09650000006c09650000000a0362036203620362036200000000036e07610368036907650362036c036e036e07600368036e07600368036e09650000000e0359035903590359035903590359000000000761036e09650000000a036203620362036203620000000003620362076003680369000000000362075e07650765036203620362036c075e076507650368036e0362036e036200000000070702000001770743075e076507650368036e0362036e020000004d037a037a0790010000001567657447656e6572616c436f6e74726163744f70740563036e072f020000000b03200743036200a60603270200000012072f020000000203270200000004034c03200342020000010e037a034c037a07430362008e02057000020529000907430368010000000a64656c65676174696f6e0342034205700002034c0326034c07900100000016676574536174656c6c697465526577617264734f7074056309650000008504620000000725756e70616964046200000005257061696404620000001d2570617274696369706174696f6e52657761726473506572536861726504620000002425736174656c6c697465416363756d756c61746564526577617264735065725368617265046e0000001a25736174656c6c6974655265666572656e63654164647265737300000000072f02000000090743036200810303270200000000072f020000000907430362009c0203270200000000070702000000600743036200808080809d8fc0d0bff2f1b26703420200000047037a034c037a0321052900080570000205290015034b031105710002031605700002033a0322072f020000001307430368010000000844495620627920300327020000000003160707020000001a037a037a03190332072c0200000002032002000000020327034f0707020000004d037a037a0790010000001567657447656e6572616c436f6e74726163744f70740563036e072f020000000b03200743036200a60603270200000012072f020000000203270200000004034c032000808080809d8fc0d0bff2f1b2670342020000092d037a057a000505700005037a034c07430362008f03052100020529000f0529000307430359030a034c03190325072c0200000002032702000000020320053d036d05700002072e02000008a4072e020000007c057000030570000405700005057000060570000705200005072e020000002c072e0200000010072e02000000020320020000000203200200000010072e0200000002032002000000020320020000002c072e0200000010072e02000000020320020000000203200200000010072e0200000002032002000000020320020000081c072e0200000044057000030570000405700005057000060570000705200005072e0200000010072e02000000020320020000000203200200000010072e020000000203200200000002032002000007cc072e0200000028057000030570000405700005057000060570000705200005072e02000000020320020000000203200200000798072e0200000774034c032003480521000305210003034c052900050316034c03190328072c020000000002000000090743036200880303270570000205210002034c0321052100030521000205290011034c0329072f020000002005290015074303620000074303620000074303620000074303620000054200050200000004034c03200743036200000521000203160319032a072c020000021c052100020521000407430362008e02057000020529000907430368010000000a64656c65676174696f6e034203420521000b034c0326034c07900100000016676574536174656c6c697465526577617264734f7074056309650000008504620000000725756e70616964046200000005257061696404620000001d2570617274696369706174696f6e52657761726473506572536861726504620000002425736174656c6c697465416363756d756c61746564526577617264735065725368617265046e0000001a25736174656c6c6974655265666572656e63654164647265737300000000072f0200000009074303620081030327020000001a072f02000000060743035903030200000008032007430359030a074303620000034c072c020000007303200521000205210004034205210007034c0326052100030521000205290008034205700007034c03260521000205290005034c05290007034b0311052100030316033a0521000b034c0322072f02000000130743036801000000084449562062792030032702000000000316034c0316031202000000060570000603200521000305210003034205210008034c0326052100030521000205700004052900030312055000030571000205210003052100030570000405290005031205500005057100020521000305700002052100030570000403160312031205500001034c05210003034c0570000305290013034b031105500013034c02000000060570000503200521000205290015055000080521000205700002052900110570000205700003034c0346034c0350055000110571000205210003052900070743036200000790010000000c746f74616c5f737570706c790362072f020000000907430362008a01032702000000000521000405290007074303620000037703420790010000000b6765745f62616c616e63650362072f02000000090743036200890103270200000000034c052100090743036200a40105210004033a033a0322072f0200000013074303680100000008444956206279203003270200000000031605210009074303620002033a0312052100090521000a07430362008803033a033a0322072f020000001307430368010000000844495620627920300327020000000003160743036200a401034c0322072f0200000013074303680100000008444956206279203003270200000000031605210004033a05210009052100020322072f0200000013074303680100000008444956206279203003270200000000031605210005034b0311052100060570000a052100040322072f0200000013074303680100000008444956206279203003270200000000031605700007052900130312055000130571000507430362008c0305210004052100070342034205210009034c0326032005700005057000030342052100050570000305700002037a034c0570000305700002034b0311074303620000052100020319032a072c020000003b05210002034c057000030322072f02000000130743036801000000084449562062792030032702000000000316057000020529001503120550001502000000080570000205200002057100030521000405210003034c05290011034c0329072f0200000009074303620089030327020000000003210521000507430362008b03057000020316057000020342034205700007034c03260320032105700004057000020316034b031105500001052100040529000707430362000005700003034205210004037705700002037a057000040655055f0765046e000000062566726f6d5f065f096500000026046e0000000425746f5f04620000000925746f6b656e5f696404620000000725616d6f756e7400000000000000042574787300000009257472616e73666572072f0200000008074303620027032702000000000743036a0000053d0765036e055f096500000006036e0362036200000000053d096500000006036e036203620000000005700004057000050570000705420003031b057000040342031b034d0743036200000521000303160319032a072c02000000440521000405210003034205700005034c032605210003052100020570000403160312055000010571000205210005034c0570000505290013034b031105500013057100030200000006057000040320034c052100040529001505500008034c0521000405700004052900110570000305210005034c0346034c03500550001105710002052100030570000207430362008e02057000020529000907430368010000000a64656c65676174696f6e0342034205700004034c03260655036e0000000e256f6e5374616b654368616e6765072f02000000090743036200b702032702000000000743036a000005700002034d053d036d034c031b034c031b02000000180570000305700004057000050570000605700007052000060200000036057000030570000405700005057000060570000705200005072e0200000010072e0200000002032002000000020320020000000203200342";
+
+                const setLambdaOperation = delegationInstance.methods.setLambda(randomLambdaName, randomLambdaBytes); 
+                await chai.expect(setLambdaOperation.send()).to.be.rejected;
+
+            } catch(e) {
+                console.dir(e, {depth: 5})
+            }
+        })
+
+    })
+
+
 });
