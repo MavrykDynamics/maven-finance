ea8580783c2fcb8a1ab53e8143687c423aa9f5b9
Tim <>
2023-04-07 17:33:34 +0800

    1) Major Delegation tests refactoring 2) Minor council and farm tests refactoring 3) Mock Sample Data satellites for Ivan

--- src/contracts/test/helpers/mockSampleData.ts
+++ src/contracts/test/helpers/mockSampleData.ts
@@ -287,7 +287,7 @@ export const mockSatelliteData = {
 
     "eve" : {
         name            : "Buzz Lightyear",
-        desc            : "Buzz is a fabled part of our childhood. He was created by Disney and Pixar mainly voiced by Tim Allen. He is a Superhero toy action figure based on the in-universe media franchise Toy Story, consisting of a blockbuster feature film and animated series, a Space Ranger. While Buzz Lightyear's sole mission used to be defeating the evil Emperor Zurg, what he now cares about most is keeping Andy's toy family together. After he feature-film Lightyear starring Chris Evans, Buzz has decided to operate a satellite of the Mavryk Finance network and sign oracle price feeds to further grow and secure the future of financial independence.", 
+        desc            : "Buzz is a fabled part of our childhood. He was created by Disney and Pixar mainly voiced by Tim Allen. He is a Superhero toy action figure based on the in-universe media franchise Toy Story, consisting of a blockbuster feature film and animated series, a Space Ranger.", 
         image           : "https://infura-ipfs.io/ipfs/QmcbigzB5PVfawr1jhctTWDgGTmLBZFbHPNfosDfq9zckQ",
         website         : "https://toystory.disney.com/buzz-lightyear", 
         satelliteFee    : 350,
@@ -297,7 +297,7 @@ export const mockSatelliteData = {
 
     "mallory" : {
         name            : "Captain Kirk",
-        desc            : "James Tiberius \"Jim\" Kirk is a legendary Starfleet officer who lived during the 23rd century. His time in Starfleet, made Kirk arguably one of the most famous and sometimes infamous starship captains in Starfleet history. The highly decorated Kirk served as the commanding officer of the Constitution-class starships USS Enterprise and USS Enterprise-A, where he served Federation interests as an explorer, soldier, diplomat, and time traveler. He currently spends his time as a Mavryk Satellite and signs Oracle price feeds for the Mavryk Finance network.",
+        desc            : "James Tiberius \"Jim\" Kirk is a legendary Starfleet officer who lived during the 23rd century. His time in Starfleet, made Kirk arguably one of the most famous and sometimes infamous starship captains in Starfleet history.",
         image           : "https://infura-ipfs.io/ipfs/QmT5aHNdawngnruJ2QtKxGd38H642fYjV7xqZ7HX5CuwRn",
         website         : "https://intl.startrek.com/",
         satelliteFee    : 700,
@@ -307,12 +307,23 @@ export const mockSatelliteData = {
 
     "oscar" : {
         name            : "Oscar Wilde",
-        desc            : "Oscar Fingal O'Fflahertie Wills Wilde was an Irish poet and playwright. After writing in different forms throughout the 1880s, he became one of the most popular playwrights in London in the early 1890s",
+        desc            : "Oscar Fingal O'Fflahertie Wills Wilde was an Irish poet and playwright. After writing in different forms throughout the 1880s, he became one of the most popular playwrights in London in the early 1890s.",
+        image           : "https://infura-ipfs.io/ipfs/QmT5aHNdawngnruJ2QtKxGd38H642fYjV7xqZ7HX5CuwRn",
+        website         : "https://intl.startrek.com/",
+        satelliteFee    : 700,
+        oraclePublicKey : oscar.pk,
+        oraclePeerId    : oscar.peerId
+    },
+
+    "ivan" : {
+        name            : "Ivan Pavlov",
+        desc            : "Ivan Petrovich Pavlov, was a Russian and Soviet experimental neurologist, psychologist and physiologist known for his discovery of classical conditioning through his experiments with dogs.",
         image           : "https://infura-ipfs.io/ipfs/QmT5aHNdawngnruJ2QtKxGd38H642fYjV7xqZ7HX5CuwRn",
         website         : "https://intl.startrek.com/",
         satelliteFee    : 700,
         oraclePublicKey : oscar.pk,
         oraclePeerId    : oscar.peerId
     }
+    
     
 }
