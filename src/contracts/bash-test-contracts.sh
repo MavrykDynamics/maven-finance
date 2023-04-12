f95d875021ad4bf99ae8996cef70423104a16f3d
Tim <tyong.espoir@gmail.com>
2023-04-12 16:56:14 +0800

    1) Delegation tests bugfix 2) Minor edits in doorman test, mock sample data, and bash test contracts script

--- src/contracts/bash-test-contracts.sh
+++ src/contracts/bash-test-contracts.sh
@@ -37,6 +37,12 @@ for contract_test in "${CONTRACTS_TEST_ARRAY[@]}"; do
             COMMANDS+=("yarn ts-mocha --paths test/03_test_mavryk_fa2_tokens.spec.ts --bail --timeout 9000000")
             COMMANDS+=("yarn ts-mocha --paths test/04_test_doorman.spec.ts --bail --timeout 9000000")
             ;;
+        delegationTest)
+            echo "Running tests for delegation"
+            COMMANDS+=("yarn ts-mocha --paths test/03_test_doorman.spec.ts --bail --timeout 9000000")
+            COMMANDS+=("yarn ts-mocha --paths test/05_test_delegation.spec.ts --bail --timeout 9000000")
+            # COMMANDS+=("yarn ts-mocha --paths test/34_test_delegation.spec.ts --bail --timeout 9000000")
+            ;;
         mvkToken)
             echo "Running tests for mvkToken"
             COMMANDS+=("yarn ts-mocha --paths test/01_test_mvk_token.spec.ts --bail --timeout 9000000")
