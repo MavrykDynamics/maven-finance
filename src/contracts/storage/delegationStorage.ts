47bfb2d0eb6d4d4546c41b7c05dea94169086d2a
Tim <>
2023-04-13 16:36:03 +0800

    Delegation contract test fix

--- src/contracts/storage/delegationStorage.ts
+++ src/contracts/storage/delegationStorage.ts
@@ -5,7 +5,7 @@ import { MVK } from "../test/helpers/Utils"
 import { delegationStorageType } from "./storageTypes/delegationStorageType"
 
 const config = {
-    minimumStakedMvkBalance             : MVK(2), 
+    minimumStakedMvkBalance             : MVK(1), 
     delegationRatio                     : 1000,
     maxSatellites                       : 100,
     satelliteNameMaxLength              : 400,
