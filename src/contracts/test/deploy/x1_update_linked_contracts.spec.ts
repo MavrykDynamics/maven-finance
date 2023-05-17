f57d8fe4e7a873e1229e5af37d99d8e6fc11124a
Tim <>
2023-05-17 23:56:23 +0800

    x1_update_linked_contracts tests remove funds setup

--- src/contracts/test/deploy/x1_update_linked_contracts.spec.ts
+++ src/contracts/test/deploy/x1_update_linked_contracts.spec.ts
@@ -119,41 +119,7 @@ describe('Linked contracts updates for Tests', async () => {
         
             console.log('MVK Token Contract - set whitelist contract addresses [doorman, vesting, treasury]')
             
-            // Send MVK to treasury contract and council (TODO: keep?)
-            if (utils.production !== "true"){
-                const transferToTreasury = await mvkTokenInstance.methods
-                .transfer([
-                    {
-                    from_: bob.pkh,
-                    txs: [
-                        {
-                            to_: contractDeployments.treasury.address,
-                            token_id: 0,
-                            amount: MVK(6000),
-                        },
-                        {
-                            to_: contractDeployments.council.address,
-                            token_id: 0,
-                            amount: MVK(15),
-                        }
-                    ],
-                    },
-                ])
-                .send()
-                await transferToTreasury.confirmation();
-            }
-            const updateOperatorsTreasury = (await treasuryInstance.methods
-                .updateMvkOperators([
-                {
-                    add_operator: {
-                        owner: contractDeployments.treasury.address,
-                        operator: contractDeployments.doorman.address,
-                        token_id: 0,
-                    },
-                }])
-                .send()) as TransactionOperation
-        
-            await updateOperatorsTreasury.confirmation();
+            
 
             // Farm FA12 Contract - set general contract addresses [doorman]
             // Farm FA12 Contract - set whitelist contract addresses [council] 
