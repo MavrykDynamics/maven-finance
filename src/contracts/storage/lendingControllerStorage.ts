5ea0e244ece965a0b53c683acf2ed6b3ededaaee
Tim <>
2023-05-17 23:57:49 +0800

    Lending Controller storage - metadata change name from Vault Controller to Lending Controller

--- src/contracts/storage/lendingControllerStorage.ts
+++ src/contracts/storage/lendingControllerStorage.ts
@@ -53,7 +53,7 @@ const metadata = MichelsonMap.fromLiteral({
     '': Buffer.from('tezos-storage:data', 'ascii').toString('hex'),
     data: Buffer.from(
         JSON.stringify({
-            name: 'MAVRYK Vault Controller Contract',
+            name: 'MAVRYK Lending Controller Contract',
             version: 'v1.0.0',
             authors: ['MAVRYK Dev Team <contact@mavryk.finance>'],
             source: {
