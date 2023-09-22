46c5cbf10a42ed0c2aae610e9d8ca8d8b8b40ca9
Tristan Allaire <tristan.allaire@alarch.fr>
2023-09-22 11:08:32 +0200

    Mavryk basenet removed prior to merge + Dockerfile updated

--- src/infrastructure/helm-charts/mavryk-basenet/scripts/patch-image-pull-secrets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl patch statefulset mavryk-baking-node -n mavryk-basenet --type='json' -p='[{"op": "add", "path": "/spec/template/spec/imagePullSecrets", "value": [{"name": "docker-pull-credentials"}]}]'
-kubectl patch statefulset rolling-node -n mavryk-basenet --type='json' -p='[{"op": "add", "path": "/spec/template/spec/imagePullSecrets", "value": [{"name": "docker-pull-credentials"}]}]'
