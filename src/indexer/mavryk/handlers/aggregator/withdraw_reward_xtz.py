b541e65ab317179968a075316492139d2ced0954
Tristan Allaire <tristan.allaire@alarch.fr>
2023-11-01 04:50:24 +0100

    Indexer Aggregator bug fix + indexer updated + dipdup types fixed

--- src/indexer/mavryk/handlers/aggregator/withdraw_reward_xtz.py
+++ src/indexer/mavryk/handlers/aggregator/withdraw_reward_xtz.py
@@ -15,21 +15,22 @@ async def withdraw_reward_xtz(
         # Get operation info
         aggregator_address          = withdraw_reward_xtz.data.target_address
         oracle_address              = withdraw_reward_xtz.parameter.__root__
-        oracle_reward_xtz_storage   = withdraw_reward_xtz.storage.oracleRewardXtz[oracle_address]
-    
-        # Update record
-        user                            = await models.mavryk_user_cache.get(network=ctx.datasource.name.replace('tzkt_',''), address=oracle_address)
-        aggregator                      = await models.Aggregator.get(network=ctx.datasource.name.replace('tzkt_',''), address= aggregator_address)
-        oracle                          = await models.AggregatorOracle.get(
-            aggregator  = aggregator,
-            user        = user
-        )
-        oracle_reward_xtz, _        = await models.AggregatorOracleReward.get_or_create(
-            oracle      = oracle,
-            type        = models.RewardType.XTZ
-        )
-        oracle_reward_xtz.xtz       = oracle_reward_xtz_storage
-        await oracle_reward_xtz.save()
+        if oracle_address in withdraw_reward_xtz.storage.oracleRewardXtz:
+            oracle_reward_xtz_storage   = withdraw_reward_xtz.storage.oracleRewardXtz[oracle_address]
+        
+            # Update record
+            user                            = await models.mavryk_user_cache.get(network=ctx.datasource.name.replace('tzkt_',''), address=oracle_address)
+            aggregator                      = await models.Aggregator.get(network=ctx.datasource.name.replace('tzkt_',''), address= aggregator_address)
+            oracle                          = await models.AggregatorOracle.get(
+                aggregator  = aggregator,
+                user        = user
+            )
+            oracle_reward_xtz, _        = await models.AggregatorOracleReward.get_or_create(
+                oracle      = oracle,
+                type        = models.RewardType.XTZ
+            )
+            oracle_reward_xtz.xtz       = oracle_reward_xtz_storage
+            await oracle_reward_xtz.save()
 
     except BaseException as e:
         await save_error_report(e)
