d4399fb65cfcb1bd627a678fd6f7040a36208c85
Tristan Allaire <tristan.allaire@alarch.fr>
2024-01-02 09:41:42 +0100

    Simulator and Frontend folders removed

--- src/frontend/src/pages/Farms/Farms.style.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/macro'
-import { containerColor } from '../../styles'
-
-export const FarmsStyled = styled.div`
-  background-color: ${containerColor};
-`
--- src/frontend/src/pages/Loans/Loans.style.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/macro'
-import { containerColor } from '../../styles'
-
-export const LoansStyled = styled.div`
-  background-color: ${containerColor};
-`
