44d3b96e870c90fb40f0efe1fc2c41a915ef0f1c
Tristan ALLAIRE <tristan.allaire@alarch.fr>
2024-07-22 10:47:17 +0200

    Indexer liquidation fixed

--- src/indexer/maven/handlers/lending_controller_mock_time/liquidate_vault.py
+++ src/indexer/maven/handlers/lending_controller_mock_time/liquidate_vault.py
@@ -85,32 +85,10 @@ async def liquidate_vault(
                 # Save collateral balance ledger
                 for collateral_token_name in vault_collateral_balance_ledger:
                     collateral_token_amount                     = float(vault_collateral_balance_ledger[collateral_token_name])
-                    collateral_token_storage                    = liquidate_vault.storage.collateralTokenLedger[collateral_token_name]
-                    collateral_token_address                    = collateral_token_storage.tokenContractAddress
-
-                    # Get token id
-                    token_id                                    = 0
-                    if type(collateral_token_storage.tokenType) == Fa2:
-                        token_id    = int(collateral_token_storage.tokenType.fa2.tokenId)
-
-                    # Get the token standard
-                    standard = await get_token_standard(
-                        ctx,
-                        collateral_token_address
-                    )
-
-                    # Get the related token
-                    token, _                                = await models.Token.get_or_create(
-                        network             = ctx.datasource.name.replace('mvkt_',''),
-                        token_address       = collateral_token_address,
-                        token_id            = token_id
-                    )
-                    token.token_standard    = standard
-                    await token.save()
 
                     lending_controller_collateral_token         = await models.LendingControllerCollateralToken.get(
                         lending_controller          = lending_controller,
-                        token                       = token
+                        token_name                  = collateral_token_name
                     )
                     lending_controller_collateral_balance, _    = await models.LendingControllerVaultCollateralBalance.get_or_create(
                         lending_controller_vault    = lending_controller_vault,
