d32e2a139ffa724a6dfbd23081f331d47c5a2d6a
Tristan ALLAIRE <tristan.allaire@alarch.fr>
2025-03-21 18:58:22 +0100

    Added VIEWS and MATERIALIZED VIEWS for optimization + addded a cronjob that refresh them every 5min

--- src/indexer/maven/hooks/refresh_views.py
+++ src/indexer/maven/hooks/refresh_views.py
@@ -4,4 +4,7 @@ from dipdup.context import HookContext
 async def refresh_views(
     ctx: HookContext,
 ) -> None:
-    await ctx.execute_sql_script('refresh_views')
+    try:
+        await ctx.execute_sql_script('refresh_views')
+    except:
+        return
