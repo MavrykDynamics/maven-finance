a2289ce48cdc822368313f5063135cb8e6784bf0
Tristan ALLAIRE <tristan.allaire@alarch.fr>
2025-03-24 09:20:44 +0100

    Refreshing views script updated

--- src/indexer/maven/hooks/refresh_views.py
+++ src/indexer/maven/hooks/refresh_views.py
@@ -5,6 +5,9 @@ async def refresh_views(
     ctx: HookContext,
 ) -> None:
     try:
+        print("Refreshing MATERIALIZED VIEWS")
         await ctx.execute_sql_script('refresh_views')
+        print("MATERIALIZED VIEWS refreshed")
     except:
+        print("Error while refreshing MATERIALIZED VIEWS")
         return
