616ba8ab51623285941d6178ee2996946cb46505
Tristan ALLAIRE <tristan.allaire@alarch.fr>
2025-03-24 10:25:45 +0100

    View refresh disabled

--- src/indexer/maven/hooks/refresh_views.py
+++ src/indexer/maven/hooks/refresh_views.py
@@ -6,14 +6,15 @@ async def refresh_views(
 ) -> None:
     try:
         print("Refreshing MATERIALIZED VIEWS")
-        await ctx.execute_sql_script('refresh_materialized_loan_token_view')
-        print("refresh_materialized_loan_token_view")
-        await ctx.execute_sql_script('refresh_materialized_tvl_stats')
-        print("refresh_materialized_tvl_stats")
-        await ctx.execute_sql_script('refresh_materialized_user_dashboard')
-        print("refresh_materialized_user_dashboard")
-        await ctx.execute_sql_script('refresh_materialized_vault_collateral_view')
-        print("MATERIALIrefresh_materialized_vault_collateral_view")
+        print("Refreshing MATERIALIZED VIEWS is currently disabled")
+        # await ctx.execute_sql_script('refresh_materialized_loan_token_view')
+        # print("refresh_materialized_loan_token_view")
+        # await ctx.execute_sql_script('refresh_materialized_tvl_stats')
+        # print("refresh_materialized_tvl_stats")
+        # await ctx.execute_sql_script('refresh_materialized_user_dashboard')
+        # print("refresh_materialized_user_dashboard")
+        # await ctx.execute_sql_script('refresh_materialized_vault_collateral_view')
+        # print("MATERIALIrefresh_materialized_vault_collateral_view")
     except:
         print("Error while refreshing MATERIALIZED VIEWS")
         return
